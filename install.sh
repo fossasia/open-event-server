0491b8d8c644b16397a393e5f4ae075f43d7d53e
Rafał <rafal.arte@gmail.com>
2015-05-13 20:33:09 +0200

    postgress and uwsgi added to provision

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 apt-get update -y
 # Python dev packages
 apt-get install -y build-essential python python-dev python-setuptools python-pip
+apt-get install -y libxml2-dev libxslt1-dev
+apt-get install -y nginx uwsgi uwsgi-plugin-python
+apt-get install -y postgresql postgresql-contrib libpq-dev
 
 #Flask
 pip install flask
