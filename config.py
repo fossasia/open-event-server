b4c3a668abd338af22775c0678c42ab48f3ef5fc
Rafał <rafal.arte@gmail.com>
2015-05-27 20:25:48 +0200

    Sqlite db config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
+# SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] # heroku
 # DATABASE_CONNECT_OPTIONS = {}
 #
 # THREADS_PER_PAGE = 8
--- run.py
+++ run.py
@@ -4,9 +4,4 @@ from open_event.models import db
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-        # from open_event.models import Sponsor
-        # x = Sponsor("test", "test@gmail.com", "aaa")
-        # db.session.add(x)
-        # db.session.commit()
-        # print Sponsor.query.all()
         app.run(debug=True)
