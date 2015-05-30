9c6608a0c8bb4b5cef299c0a98948ec6460e7bf1
Rafał <rafal.arte@gmail.com>
2015-05-31 00:51:41 +0200

    New Api Urls fixed

--- open_event/__init__.py
+++ open_event/__init__.py
@@ -37,14 +37,20 @@ def not_found(error):
     return render_template('404.html'), 404
 
 
-@app.route('/get/api/v1/events', methods=['GET'])
+@app.route('/get/api/v1/event', methods=['GET'])
 @cross_origin()
 def get_events():
     return jsonify({"events":
                     [event.serialize for event in QueryFilter(request.args, Event.query).get_filtered_data()]})
 
 
-@app.route('/get/api/v1/event/<event_id>sessions', methods=['GET'])
+@app.route('/get/api/v1/event/<event_id>', methods=['GET'])
+@cross_origin()
+def get_event_by_id(event_id):
+    return jsonify(Event.query.get(event_id).serialize)
+
+
+@app.route('/get/api/v1/event/<event_id>/sessions', methods=['GET'])
 @cross_origin()
 def get_sessions(event_id):
     sessions = Track.query.filter_by(event_id=event_id)
@@ -68,7 +74,7 @@ def get_speakers(event_id):
                     [speaker.serialize for speaker in QueryFilter(request.args, speakers).get_filtered_data()]})
 
 
-@app.route('/get/api/v1/sponsors', methods=['GET'])
+@app.route('/get/api/v1/event/<event_id>/sponsors', methods=['GET'])
 @cross_origin()
 def get_sponsors(event_id):
     sponsors = Sponsor.query.filter_by(event_id=event_id)
@@ -76,7 +82,7 @@ def get_sponsors(event_id):
                     [sponsor.serialize for sponsor in QueryFilter(request.args, sponsors).get_filtered_data()]})
 
 
-@app.route('/get/api/v1/microlocations', methods=['GET'])
+@app.route('/get/api/v1/event/<event_id>/microlocations', methods=['GET'])
 @cross_origin()
 def get_microlocations(event_id):
     microlocations = Microlocation.query.filter_by(event_id=event_id)
