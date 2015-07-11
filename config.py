2463f95192fdd97a41d6d0da4c132bba547ae937
Rafał <rafal.arte@gmail.com>
2015-07-11 23:05:47 +0200

    config changed to heroku

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
 
 class ProductionConfig(Config):
     DEBUG = False
-    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class StagingConfig(Config):
