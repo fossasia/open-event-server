3093c03c5b7c6bccf4985118753fca0d71722ccb
Rafał <rafal.arte@gmail.com>
2015-07-21 22:12:23 +0200

    Event form refacored

--- open_event/forms/admin/event_form.py
+++ open_event/forms/admin/event_form.py
@@ -1,20 +1,19 @@
 """Copyright 2015 Rafal Kowalski"""
-from flask import flash
 from flask_wtf import Form
-from wtforms import StringField, FloatField, validators, IntegerField, FileField
+from wtforms import StringField, FloatField, validators, FileField
+from wtforms.validators import DataRequired
 from flask.ext.admin.form.fields import DateTimeField
 from ...helpers.validators import CustomDateEventValidate
 
 class EventForm(Form):
-    name = StringField('Name', [validators.DataRequired()])
-    latitude = FloatField('Latitude', [validators.DataRequired()])
-    longitude = FloatField('Longitude', [validators.DataRequired()])
+    name = StringField('Name', [DataRequired()])
+    latitude = FloatField('Latitude', [DataRequired()])
+    longitude = FloatField('Longitude', [DataRequired()])
     location_name = StringField('Location name')
     color = StringField('Color')
-    start_time = DateTimeField('Start Time', [validators.DataRequired(), CustomDateEventValidate()])
-    end_time = DateTimeField('End Time', [validators.DataRequired(), CustomDateEventValidate()])
+    start_time = DateTimeField('Start Time', [DataRequired(), CustomDateEventValidate()])
+    end_time = DateTimeField('End Time', [DataRequired(), CustomDateEventValidate()])
     logo = FileField('Logo')
     email = StringField('Email')
     slogan = StringField('Slogan')
     url = StringField('Url')
-
