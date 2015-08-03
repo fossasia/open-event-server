e5371dc0add41c9829d6aafb364e7f86a0ecea2a
Rafał <rafal.arte@gmail.com>
2015-08-03 20:12:04 +0200

    Procfile fixed

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn run:app
+web: gunicorn run:current_app
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 """Copyright 2015 Rafal Kowalski"""
+from open_event import current_app
+from open_event.models import db
 
 if __name__ == "__main__":
-    from open_event import current_app
-    from open_event.models import db
     with current_app.app_context():
         db.create_all()
     current_app.run(host='0.0.0.0', debug=True)
