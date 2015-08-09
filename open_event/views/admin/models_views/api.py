a25333cd0355c56317505dd369de0b210bfe9f91
Rafał <rafal.arte@gmail.com>
2015-08-09 22:39:33 +0200

    Api all links generated #103 closed

--- open_event/views/admin/models_views/api.py
+++ open_event/views/admin/models_views/api.py
@@ -1,10 +1,11 @@
 """Copyright 2015 Rafal Kowalski"""
 from flask_admin import BaseView, expose
-
-from ....models.event import Event
+from flask.ext import login
+from ....helpers.data_getter import DataGetter
 
 class ApiView(BaseView):
     @expose('/')
     def index(self):
-        events = Event.query.all()
-        return self.render('admin/api/index.html', events=events)
+        events = DataGetter.get_all_events()
+        owner_events = DataGetter.get_all_owner_events(login.current_user.id)
+        return self.render('admin/api/index.html', events=events, owner_events=owner_events)
