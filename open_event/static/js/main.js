088d8d101574778fc3b92d79e4d89cc47a8e1629
Rafał <rafal.arte@gmail.com>
2015-08-09 17:14:11 +0200

    changed width

---
+++ open_event/static/js/main.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+        console.log("test")
+        $('a').click( function() {
+            console.log("test")
+            $(this).addClass('active').siblings().removeClass('active');
+        });
+
+});
