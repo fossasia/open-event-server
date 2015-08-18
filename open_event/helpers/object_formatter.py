db6246f250b961716b9b19fe6f7a68b9a06a58ef
Rafał <rafal.arte@gmail.com>
2015-08-18 21:53:12 +0200

    Numbers of objects displayed in api

--- open_event/helpers/object_formatter.py
+++ open_event/helpers/object_formatter.py
@@ -8,16 +8,21 @@ class ObjectFormatter(object):
 
     @staticmethod
     def get_json(name, query, request, page=None):
+        objects = QueryFilter(request.args, query).get_filtered_data()
+        count = objects.count()
         if not page:
             return jsonify(
                 {name: [
                     table_object.serialize
                     for table_object in
-                    QueryFilter(request.args, query).get_filtered_data()]})
+                    objects],
+                 'count': count})
         else:
             return jsonify(
                 {name: [
                     table_object.serialize
                     for table_object in
                     QueryFilter(request.args, query).get_filtered_data().paginate(page, 50).items
-                    ]})
+                    ],
+                  'count': count
+                 })
