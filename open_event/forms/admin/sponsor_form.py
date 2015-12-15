29e2192beea3502c623bdf6f2dd157cbebbada9a
Aditya <adityavyas17@gmail.com>
2015-12-15 18:47:58 +0530

    Updated sponsor.py

--- open_event/forms/admin/sponsor_form.py
+++ open_event/forms/admin/sponsor_form.py
@@ -1,10 +1,10 @@
 """Copyright 2015 Rafal Kowalski"""
 from flask_wtf import Form
 from wtforms import StringField, validators
-
+from flask_wtf.file import FileField
 
 class SponsorForm(Form):
     """Sponsor Form class"""
     name = StringField('Name', [validators.DataRequired()])
     url = StringField('Url')
-    logo = StringField('Logo')
+    logo = FileField('Logo')
