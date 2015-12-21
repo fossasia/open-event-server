c9f449c36d2b52ffed8513f142a66bcfc9b6b6b4
Aditya <adityavyas17@gmail.com>
2015-12-22 02:43:54 +0530

    Updated views.py
    Added methods to return speaker and session data with a given event id X

--- open_event/views/views.py
+++ open_event/views/views.py
@@ -196,6 +196,18 @@ def get_event_version(event_id):
     return jsonify({"version": []})
 
 
+@app.route('/api/v1/event/<event_id>/sessions/<string:session_name>', methods=['GET'])
+@cross_origin()
+def get_sessions_at_event(event_id, session_name):
+    sessions=Session.query.filter(Session.event_id == event_id,Session.name == session_name)
+    return ObjectFormatter.get_json("sessions", sessions, request)
+    
+@app.route('/api/v1/event/<event_id>/speakers/<string:speaker_name>', methods=['GET'])
+@cross_origin()
+def get_speakers_at_event(event_id, speaker_name):
+    speakers=Speaker.query.filter(Speaker.event_id == event_id,Speaker.name == speaker_name)
+    return ObjectFormatter.get_json("speakers", speakers, request)
+
 @app.route('/pic/<path:filename>')
 def send_pic(filename):
     """Returns image"""
