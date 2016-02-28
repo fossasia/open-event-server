5f033082f48dce782c0984111a8b7a8734c1a177
Rafał Kowalski <rafal.arte@gmail.com>
2016-02-29 00:35:50 +0100

    len of description increased

--- open_event/forms/admin/track_form.py
+++ open_event/forms/admin/track_form.py
@@ -9,6 +9,6 @@ from ...helpers.data_getter import DataGetter
 class TrackForm(Form):
     """Track Form class"""
     name = StringField('Name', [Length(min=6, max=35)])
-    description = StringField('Description', [Length(min=4, max=300)])
+    description = StringField('Description', [Length(min=4, max=1000)])
     track_image_url = StringField('Image')
     sessions = QuerySelectMultipleField(query_factory=DataGetter.get_sessions_by_event_id, allow_blank=True)
