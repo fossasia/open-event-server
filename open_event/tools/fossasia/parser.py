d6376eec273035315b9c30d7632b8bddbb6f1b90
Rafał Kowalski <rafal.arte@gmail.com>
2016-03-12 21:58:49 +0100

    Microlocation added

--- open_event/tools/fossasia/parser.py
+++ open_event/tools/fossasia/parser.py
@@ -14,7 +14,9 @@ from open_event.helpers.data import get_or_create, save_to_db
 from open_event.models.session import Session, Level, Format, Language
 from open_event.models.speaker import Speaker
 from open_event.models.track import Track
+from open_event.models.microlocation import Microlocation
 from dateutil.parser import parse
+import re
 def get_sessions():
     with open('open_event/tools/fossasia/sessions.json', 'r+') as f:
         return json.load(f)['sessions']
@@ -22,7 +24,6 @@ def parse_speakers():
     with current_app.app_context():
         with open('open_event/tools/fossasia/speakers.json', 'r+') as f:
             a =  json.load(f)
-            print a['speakers']
             for spek in a['speakers']:
 
                     sp = get_or_create(Speaker,
@@ -43,19 +44,7 @@ def parse_speakers():
                     sp.linkedin = spek['linkedin']
                     sp.organisation = spek['organisation']
                     save_to_db(sp, "Speaker")
-# {
-#     "py/object": "models.Speaker",
-#     "web": "http://hpdang.com",
-#     "github": "http://github.com/hpdang",
-#     "name": "Hong Phuc Dang",
-#     "country": "",
-#     "twitter": "http://twitter.com/hpdang",
-#     "linkedin": "https://www.linkedin.com/in/hongphucdang",
-#     "id": 1,
-#     "photo": "",
-#     "organisation": "FOSSASIA",
-#     "biography": "Hong Phuc, originally from Vietnam, has lived in the US, Singapore and Berlin. She was a speaker at the Chaos Communication Congress presenting the latest advances in Open Fashion and Technology. She is also the founder of FOSSASIA, an initiative in Asia bringing together an inspiring community across borders and ages to form a better future with Open Technologies and ICT. Hong Phuc organizes events like the annual FOSSASIA summit since 2009 and puts together Science Hackdays across the region in Vietnam, Dubai and China."
-#   },
+
 
 if __name__ == "__main__":
     event_id = sys.argv[1]
@@ -90,6 +79,23 @@ if __name__ == "__main__":
                                         end_time=parse(session_json['end_time']))
                 new_session.speakers = speakers
                 new_session.track = get_or_create(Track, name=session_json['track']['name'], description="", event_id=event_id, track_image_url="")
+                location = session_json["location"]
+                pattern_lvl = re.compile(ur'\d')
+                level = None
+                room = None
+                try:
+                    room = location.split(',')[1]
+                    level = int(re.search(pattern_lvl, location).group(0))
+                except Exception as error:
+                    print error
+                print get_or_create(Microlocation, name=location, floor=level, room=room, event_id=event_id)
+                new_session.microlocation = get_or_create(Microlocation,
+                                                          latitude=1.333194,
+                                                          longitude=103.736132,
+                                                          name=location,
+                                                          floor=level,
+                                                          room=room,
+                                                          event_id=event_id)
                 save_to_db(new_session, "Session Updated")
     except Exception as e:
         print e
