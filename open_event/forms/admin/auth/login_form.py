012b7479cd7e6d83811df21010f490c370958e37
aviaryan <avi.aryan123@gmail.com>
2016-03-19 03:21:44 +0530

    more concrete implementation of login with email

--- open_event/forms/admin/auth/login_form.py
+++ open_event/forms/admin/auth/login_form.py
@@ -10,16 +10,32 @@ class LoginForm(form.Form):
     login = StringField(validators=[validators.required()])
     password = PasswordField(validators=[validators.required()])
 
+    def __init__(self, form):
+        """Init class"""
+        super(LoginForm, self).__init__(form)
+        self.user = None
+
     def validate_login(self, field):
         """Login validation"""
-        user = self.get_user()
+        users = self.get_users()
 
-        if user is None:
+        if users is None:
             raise validators.ValidationError('Invalid user')
 
-        if not check_password_hash(self.password.data.encode("utf-8"), user.password.encode('utf-8'), user.salt):
+        userFound = False
+        for u in users:
+            if check_password_hash(self.password.data.encode("utf-8"), u.password.encode('utf-8'), u.salt):
+                userFound = True
+                break
+        if not userFound:
             raise validators.ValidationError('Invalid password')
+        else:
+            self.user = u
+
+    def get_users(self):
+        """Returns users by login/email"""
+        return db.session.query(User).filter(or_(User.login==self.login.data, User.email==self.login.data))
 
     def get_user(self):
-        """Returns user by login"""
-        return db.session.query(User).filter(or_(User.login==self.login.data, User.email==self.login.data)).first()
+        """Get authenticated user"""
+        return self.user
