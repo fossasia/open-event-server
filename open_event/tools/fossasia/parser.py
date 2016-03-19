43f8b9edfef32d85a25f54836e3d5702b346faf4
Rafał Kowalski <rafal.arte@gmail.com>
2016-03-19 16:25:51 +0800

    biography added to parsers

--- open_event/tools/fossasia/parser.py
+++ open_event/tools/fossasia/parser.py
@@ -48,6 +48,7 @@ def parse_speakers():
                     sp.linkedin = spek['linkedin']
                     sp.github = spek['github']
                     sp.organisation = spek['organisation']
+                    sp.biography = spek['"biography"']
                     save_to_db(sp, "Speaker")
 
 
