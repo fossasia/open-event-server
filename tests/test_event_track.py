5a2829ce1171b5aebea2a1da0a45a6a0da2d203a
Justin Lee <triplez@justinlee.sg>
2016-05-12 11:43:04 +0200

    fix: test fail caused by change in #277

--- tests/test_event_track.py
+++ tests/test_event_track.py
@@ -26,11 +26,11 @@ class TestEvent(OpenEventTestCase):
         self.assertEqual(self.app.get('/api/v1/event/1/tracks').status_code, 200)
 
     def test_admin_track(self):
-        self.assertEqual(self.app.get('/admin/event/1/track').status_code, 200)
+        self.assertEqual(self.app.get('/admin/event/1/track/').status_code, 200)
 
     def test_adding_track_by_owner(self):
         with app.test_request_context():
-            self.app.post(url_for('event.event_track_new',
+            self.app.post(url_for('track.edit_view',
                                   event_id=1),
                                   data=dict(
                                       name='track1',
