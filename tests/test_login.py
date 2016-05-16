edb8bed18678c1b524fc3dd7004d9a0cf7d37947
Avi Aryan <avi.aryan123@gmail.com>
2016-05-16 03:21:17 +0100

    removed duplicate tests/set_up.py, fixes #311 (#312)
    * removed duplicate tests/set_up.py
    
    * fixed the build error

--- tests/test_login.py
+++ tests/test_login.py
@@ -4,7 +4,7 @@ from tests.utils import OpenEventTestCase
 from auth_helper import register
 import unittest
 from flask import url_for
-from tests.set_up import Setup
+from tests.setup_database import Setup
 from open_event import current_app as app
 from open_event.helpers.data import save_to_db
 from tests.object_mother import ObjectMother
