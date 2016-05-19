2ac8ccb0ce95aead45ba027377f4a64df9415983
Aditya Vyas <adityavyas17@gmail.com>
2016-05-19 21:34:06 +0530

    Tests for SessionType Table (#342)

---
+++ tests/test_session_type.py
@@ -0,0 +1,38 @@
+import unittest
+from tests.utils import OpenEventTestCase
+from open_event.models.session_type import SessionType
+from open_event.models.session import Session
+from tests.setup_database import Setup
+from open_event import current_app as app
+from open_event.helpers.data import save_to_db
+from tests.object_mother import ObjectMother
+from datetime import datetime
+
+
+class TestSessionType(OpenEventTestCase):
+    def test_add_session_type_to_db(self):
+        """Checks the one to many relationship between event and session_types and
+                the one to one relationship between session and session_types"""
+        self.app = Setup.create_app()
+        with app.test_request_context():
+            event = ObjectMother.get_event()
+            session1 = ObjectMother.get_session()
+            session2 = Session(title='test2',
+                               description='dsadsd',
+                               start_time=datetime(2003, 8, 4, 12, 30, 45),
+                               end_time=datetime(2003, 8, 4, 12, 30, 45))
+            session_type1 = SessionType(name='Type1', length='30', event_id='1', session_id='1')
+            session_type2 = SessionType(name='Type2', length='30', event_id='1', session_id='2')
+            save_to_db(event, "Event Saved")
+            save_to_db(session1, "Session1 Saved")
+            save_to_db(session2, "Session2 Saved")
+            save_to_db(session_type1, "SessionType1 Saved")
+            save_to_db(session_type2, "SessionType2 Saved")
+            self.assertEqual(session_type1.event_id, 1)
+            self.assertEqual(session_type2.event_id, 1)
+            self.assertEqual(session_type1.session_id, 1)
+            self.assertEqual(session_type2.session_id, 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
