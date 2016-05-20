e1d8704a6a4b10333e5d37a04f6809be39ada00f
Rafal Kowalski <rafal.arte@gmail.com>
2016-05-20 21:05:06 +0200

    bug: fix

--- open_event/static/js/admin.js
+++ open_event/static/js/admin.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
                         "<input type='text' class='form-control' name='session_type[name]' placeholder='Name'>" +
                     "</div>" +
                     "<div class='col-sm-3 input-group'>" +
-                        "<input type='text' class='form-control' name='session_type[name]' placeholder='Length'>" +
+                        "<input type='text' class='form-control' name='session_type[length]' placeholder='Length'>" +
                         "<span class='input-group-btn'>" +
                             "<button type='button' class='btn btn-danger remove-session-type'>-</button>" +
                         "</span>" +
