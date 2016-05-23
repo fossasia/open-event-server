85e2dce00cb145cb7b0fc85bbde71cfb8c904e42
Aditya <adityavyas17@gmail.com>
2016-05-23 22:19:01 +0530

    Added Track View

--- open_event/static/js/admin/event/new.js
+++ open_event/static/js/admin/event/new.js
@@ -39,6 +39,26 @@ $( document ).ready(function() {
         $(this).parent().parent().parent().remove()
     });
 
+    $('.add-tracks').click(function (){
+        var row = "<div class='col-sm-12 row-tracks'>" +
+                    "<div class='col-sm-3'>" +
+                        "<input type='text' class='form-control' name='tracks[name]' placeholder='Name'>" +
+                    "</div>" +
+                    "<div class='col-sm-3 input-group'>" +
+                        "<input class='jscolor' value='ab2567' name='tracks[color]'>" +
+                        "<span class='input-group-btn'>" +
+                            "<button type='button' class='btn btn-danger remove-tracks'>-</button>" +
+                        "</span>" +
+                    "</div>" +
+                "</div>"
+        $('.tracks').append(row)
+
+    })
+
+    $( "body" ).on( "click", ".remove-tracks", function (){
+        $(this).parent().parent().parent().remove()
+    });
+
     // Smart Wizard
     $('#wizard').smartWizard({
         labelFinish:'Save Draft',
