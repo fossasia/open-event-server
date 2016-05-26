5613dea7cb1d3f27aeb7b6b82857d78ae403a4dc
Niranjan Rajendran <niranjan94@yahoo.com>
2016-05-26 16:46:17 +0530

    Added ellipsis library

---
+++ open_event/static/js/jquery/jquery.ellipsis.js
@@ -0,0 +1,42 @@
+// Borrowed from http://stackoverflow.com/a/1022672/1562480
+(function ($) {
+    $.fn.ellipsis = function () {
+        return this.each(function () {
+            var el = $(this);
+
+            var text = el.attr("data-original-text");
+            el.text(text);
+
+            var multiline = true;
+            
+            var t = $(this.cloneNode(true))
+                    .hide()
+                    .css('position', 'absolute')
+                    .css('overflow', 'visible')
+                    .width(multiline ? el.width() : 'auto')
+                    .height(multiline ? 'auto' : el.height())
+                ;
+
+            el.after(t);
+
+            function height() {
+                return t.height() > el.height();
+            }
+
+            function width() {
+                return t.width() > el.width();
+            }
+
+            var func = multiline ? height : width;
+
+
+            while (text.length > 0 && func()) {
+                text = text.substr(0, text.length - 1);
+                t.text(text + "...");
+            }
+
+            el.text(t.text());
+            t.remove();
+        });
+    };
+})(jQuery);
