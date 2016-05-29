d780849a4aac9d2a6017cdff9d2d83e0de468e13
Niranjan Rajendran <niranjan94@yahoo.com>
2016-05-29 19:15:33 +0530

    Moved utility functions to a separate file for easier maintenance

--- open_event/static/js/admin/event/scheduling.js
+++ open_event/static/js/admin/event/scheduling.js
@@ -66,23 +66,6 @@ function fixOverlaps() {
     updateCounterBadge();
 }
 
-function roundOffToMultiple(val, multiple) {
-    if (!multiple) {
-        multiple = 6;
-    }
-    if (val > 0) {
-        var roundUp = Math.ceil(val / multiple) * multiple;
-        var roundDown = Math.floor(val / multiple) * multiple;
-
-        if (val >= roundDown + 12) {
-            return roundUp;
-        } else {
-            return roundDown;
-        }
-
-    } else {
-        return multiple;
-    }
 }
 
 function sessionOverlapTest($track, $session) {
@@ -98,45 +81,12 @@ function sessionOverlapTest($track, $session) {
     return returnVal;
 }
 
-/**
- * @return {boolean}
- */
-function horizontallyBound($parentDiv, $childDiv, offsetAdd) {
-    var pageWidth = $parentDiv.width();
-    var pageHeight = $parentDiv.height();
-    var pageTop = $parentDiv.offset().top;
-    var pageLeft = $parentDiv.offset().left;
-    var elementWidth = $childDiv.width();
-    var elementHeight = $childDiv.height();
-    var elementTop = $childDiv.offset().top;
-    var elementLeft = $childDiv.offset().left;
-    var offset = 50 + offsetAdd;
-    return !!((elementLeft + offset >= pageLeft) && (elementTop + offset >= pageTop) && (elementLeft + elementWidth <= pageLeft + pageWidth) && (elementTop + elementHeight <= pageTop + pageHeight));
-}
-
 function restrictionCheck(x, $sessionElement) {
     return (horizontallyBound($(".tracks"), $sessionElement, 0));
 }
 
 function overDraggable($sessionElement) {
     return collision($(".tracks"), $sessionElement);
-}
-
-// Borrowed from http://stackoverflow.com/a/5541252/1562480. Author: BC.
-function collision($div1, $div2) {
-    var x1 = $div1.offset().left;
-    var y1 = $div1.offset().top;
-    var h1 = $div1.outerHeight(true);
-    var w1 = $div1.outerWidth(true);
-    var b1 = y1 + h1;
-    var r1 = x1 + w1;
-    var x2 = $div2.offset().left;
-    var y2 = $div2.offset().top;
-    var h2 = $div2.outerHeight(true);
-    var w2 = $div2.outerWidth(true);
-    var b2 = y2 + h2;
-    var r2 = x2 + w2;
-    return !(b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2);
 }
 
 function updateColor($element) {
