d5dd25bcc0c0b1b5e04ac2f2af423c9db5ee42da
aviaryan <avi.aryan123@gmail.com>
2016-05-30 15:51:08 +0530

    added tests for POST API

---
+++ tests/api/utils_post_data.py
@@ -0,0 +1,69 @@
+"""
+This file contains sample POST data
+"""
+
+POST_FORMAT_DATA = {
+    "label_en": "TestFormat",
+    "name": "TestFormat"
+}
+
+POST_LANGUAGE_DATA = {
+    "label_de": "TestLanguage",
+    "label_en": "TestLanguage",
+    "name": "TestLanguage"
+}
+
+POST_LEVEL_DATA = {
+    "label_en": "TestLevel",
+    "name": "TestLevel"
+}
+
+POST_MICROLOCATION_DATA = {
+    "floor": 1,
+    "latitude": 1,
+    "longitude": 1,
+    "name": "TestMicrolocation",
+    "room": "TestRoom"
+}
+
+POST_SESSION_DATA = {
+    "abstract": "TestSession",
+    "description": "TestSession",
+    "end_time": "2016-05-30T08:47:37.410Z",
+    "format_id": 1,
+    "language_id": 1,
+    "level_id": 1,
+    "microlocation_id": 1,
+    "speaker_ids": [1],
+    "start_time": "2016-05-25T08:47:37.410Z",
+    "subtitle": "TestSession",
+    "title": "TestSession",
+    "track_id": 1
+}
+
+POST_SPEAKER_DATA = {
+    "biography": "TestSpeaker",
+    "country": "TestSpeaker",
+    "email": "a@b.com",
+    "facebook": "http://facebook.com/user",
+    "github": "http://github.com/user",
+    "linkedin": "http://in.linkedin.com/user",
+    "name": "TestSpeaker",
+    "organisation": "TestSession",
+    "photo": "http://imgur.com/skds.png",
+    "position": "TestSession",
+    "twitter": "http://twitter.com/user",
+    "web": "http://website.com"
+}
+
+POST_SPONSOR_DATA = {
+    "logo": "http://imgur.com/image.png",
+    "name": "TestSponsor",
+    "url": "http://sponsor.com"
+}
+
+POST_TRACK_DATA = {
+    "description": "TestTrack",
+    "name": "TestTrack",
+    "track_image_url": "http://imgur.com/image.png"
+}
