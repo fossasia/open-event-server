235f4ccb2770026f48df2c0ae1d04d40ad953cfe
Shivam Mamgain <shivammamgain47@gmail.com>
2016-05-31 16:30:06 +0530

    Add default values to page parameters

--- open_event/api/utils.py
+++ open_event/api/utils.py
@@ -1,9 +1,20 @@
 from flask_restplus import Model, fields, reqparse
 
+DEFAULT_PAGE_START = 1
+DEFAULT_PAGE_LIMIT = 20
+
 # Parameters for a paginated response
 PAGE_PARAMS = {
-    'start': {'description': 'Serial number to start from', 'type': int},
-    'limit': {'description': 'Limit on the number of results', 'type': int},
+    'start': {
+        'description': 'Serial number to start from',
+        'type': int,
+        'default': DEFAULT_PAGE_START
+    },
+    'limit': {
+        'description': 'Limit on the number of results',
+        'type': int,
+        'default': DEFAULT_PAGE_LIMIT
+    },
 }
 
 # Base Api Model for a paginated response
@@ -23,5 +34,5 @@ class PaginatedResourceBase():
     This includes basic properties used in the class
     """
     parser = reqparse.RequestParser()
-    parser.add_argument('start', type=int, default=1)
-    parser.add_argument('limit', type=int, default=20)
+    parser.add_argument('start', type=int, default=DEFAULT_PAGE_START)
+    parser.add_argument('limit', type=int, default=DEFAULT_PAGE_LIMIT)
