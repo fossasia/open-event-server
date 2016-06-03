9d80c6819f43c74a65379cb27a53f03a15400015
Shivam Mamgain <shivammamgain47@gmail.com>
2016-06-03 21:54:18 +0530

    Fix js lint issues

--- open_event/static/js/admin/event/details.js
+++ open_event/static/js/admin/event/details.js
@@ -1,33 +1,35 @@
-$( document ).ready(function() {
-    $('#wizard').smartWizard({
-        includeFinishButton:false,
-        keyNavigation:false,
-        noForwardJumping: true,
-    });
+$(document).ready(function () {
 
-    $('.date-picker').daterangepicker({
-          singleDatePicker: true,
-          timePicker: true,
-          calender_style: "picker_4",
-          locale: {
-            format: 'MM/DD/YYYY H:mm A'
-          }
-        });
+  $('#wizard').smartWizard({
+    includeFinishButton: false,
+    keyNavigation: false,
+    noForwardJumping: true,
+  });
 
-    $("#go_to_call_for_papers").click(function (){
-        $('#wizard').smartWizard('goToStep', 2);
-//        Send post to change event state to Call for papers
-    })
+  $('.date-picker').daterangepicker({
+    singleDatePicker: true,
+    timePicker: true,
+    calender_style: "picker_4",
+    locale: {
+      format: 'MM/DD/YYYY H:mm A'
+    }
+  });
 
-    $("#back_to_draft").click(function (){
-        $('#wizard').smartWizard('goToStep', 1);
-//        Send post to change event state to Call for papers
-    })
+  $("#go_to_call_for_papers").click(function () {
+    $('#wizard').smartWizard('goToStep', 2);
+    // Send post to change event state to Call for papers
+  });
 
-    $("#start_scheduling").click(function (){
-        $('#wizard').smartWizard('goToStep', 3);
-//        Send post to change event state to Call for papers
-    })
+  $("#back_to_draft").click(function () {
+    $('#wizard').smartWizard('goToStep', 1);
+    // Send post to change event state to Call for papers
+  });
 
-    $(".actionBar").remove()
+  $("#start_scheduling").click(function () {
+    $('#wizard').smartWizard('goToStep', 3);
+    // Send post to change event state to Call for papers
+  });
+
+  $(".actionBar").remove();
+
 });
