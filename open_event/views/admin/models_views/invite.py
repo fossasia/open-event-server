bf61dc4cfc928fce5f4e7348c3eacf2f200982e1
saptaks <saptak013@gmail.com>
2016-06-05 17:19:51 +0530

    Modified invite view to work only with email

--- open_event/views/admin/models_views/invite.py
+++ open_event/views/admin/models_views/invite.py
@@ -18,6 +18,6 @@ class InviteView(ModelView):
                 DataManager.add_invite_to_event(user.id, event_id)
                 hash = DataGetter.get_invite_by_user_id(user.id).hash
                 link = "http://127.0.0.1:5000/admin/events/%s/session/new/%s/%s/" % (str(event_id), str(user.id), str(hash))
-                Helper.send_email_invitation(email, user.login, event.name, link)
+                Helper.send_email_invitation(email, event.name, link)
 
         return redirect(url_for('event.details_view', event_id=event_id))
