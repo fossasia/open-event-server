00a0e2c2ed9a0ddc32fac2bc463af702e6421128
Rafal Kowalski <rafal.arte@gmail.com>
2016-06-08 19:41:33 +0200

    fix: tests not needed removed

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -10,7 +10,6 @@ from open_event import current_app as app
 from open_event.models import db
 from open_event.models.version import Version
 from open_event.helpers.data import DataManager, update_version, save_to_db
-from open_event.forms.admin.microlocation_form import MicrolocationForm
 
 
 class TestDataManager(OpenEventTestCase):
@@ -20,27 +19,9 @@ class TestDataManager(OpenEventTestCase):
             db.session.add(ObjectMother.get_event())
             db.session.commit()
 
-    def _create_microlocation(self):
-        form = MicrolocationForm()
-        form.name.data = 'aaaaaaa'
-        form.latitude.data = 12.00
-        form.longitude.data = 11.00
-        DataManager().create_microlocation(form, 1)
 
     def _delete_object_from_db(self):
         DataManager().remove_microlocation(1)
-
-    @patch.object(open_event.helpers.data, "update_version")
-    def test_update_version_method_called_after_create_microlocation(self, method):
-        with app.test_request_context():
-            self.assertRaises(Exception, self._create_microlocation())
-            self.assertTrue(method.called)
-
-    @patch.object(open_event.helpers.data, "save_to_db")
-    def test_save_to_db_method_called_after_create_microlocation(self, method):
-        with app.test_request_context():
-            self.assertRaises(Exception, self._create_microlocation())
-            self.assertTrue(method.called)
 
     @patch.object(open_event.helpers.data, "delete_from_db")
     def test_delete_from_db_method_called_after_delete_object(self, method):
