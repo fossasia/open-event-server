118834d578bd3f4691b5e7b95209d40c270e1c87
Niranjan Rajendran <niranjan94@yahoo.com>
2016-06-12 23:57:40 +0530

    Add view for individual session in the "My Sessions" Dashboard

--- open_event/views/admin/models_views/my_sessions.py
+++ open_event/views/admin/models_views/my_sessions.py
@@ -1,8 +1,11 @@
+import flask_login
+from flask.ext.restplus import abort
 from flask_admin import BaseView, expose
 from ....helpers.data_getter import DataGetter
 
 class MySessionView(BaseView):
     @expose('/')
+    @flask_login.login_required
     def display_my_sessions_view(self):
         upcoming_events_sessions = DataGetter.get_sessions_of_user(upcoming_events=True)
         past_events_sessions = DataGetter.get_sessions_of_user(upcoming_events=False)
@@ -11,6 +14,9 @@ class MySessionView(BaseView):
                            upcoming_events_sessions=upcoming_events_sessions, past_events_sessions=past_events_sessions)
 
     @expose('/<session_id>')
+    @flask_login.login_required
     def display_session_view(self, session_id):
-        session = DataGetter.get_session(session_id)
+        session = DataGetter.get_sessions_of_user_by_id(session_id)
+        if not session:
+            abort(404)
         return self.render('/gentelella/admin/mysessions/mysession_detail.html', session=session)
