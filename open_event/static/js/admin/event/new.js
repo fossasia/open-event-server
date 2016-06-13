f19f0d91fa3b09fe7e681aa24be51e8d8d996a0a
Rafal Kowalski <rafal.arte@gmail.com>
2016-06-13 18:43:03 +0200

    fix: solve cannot publish event #785

--- open_event/static/js/admin/event/new.js
+++ open_event/static/js/admin/event/new.js
@@ -109,18 +109,49 @@ $(document).ready(function() {
 
   // Smart Wizard
   $("#wizard").smartWizard({
-    labelFinish:'Publish',
-    onFinish: function() { $("#event-create-form").submit(); }
+    labelFinish:'Make your event live',
+    onFinish: function() {
+        var input = $("<input>")
+               .attr("type", "hidden")
+               .attr("name", "state").val("Publish");
+        $('#wizard').append($(input));
+        $("#event-create-form").submit(); },
+    enableAllSteps: true,
   });
 
   $("#wizard-edit").smartWizard({
-    labelFinish:'Update',
-    onFinish: function() { $("#event-edit-form").submit(); }
+    labelFinish:'Make your event live',
+    onFinish: function() {
+        var input = $("<input>")
+               .attr("type", "hidden")
+               .attr("name", "state").val("Publish");
+        $('#wizard-edit').append($(input));
+        $("#event-edit-form").submit(); },
+    enableAllSteps: true,
    });
 
   $('.buttonNext').addClass("btn btn-success");
   $('.buttonPrevious').addClass("btn btn-primary");
-  $('.buttonFinish').addClass("btn btn-default");
-
+  $('.buttonFinish').addClass("btn btn-info");
+  addSaveButton()
 
 });
+
+
+function addSaveButton(){
+    var wizard = $("#wizard")
+    var wizard_edit = $("#wizard-edit")
+    if (wizard.length) {
+        $("#wizard .buttonFinish").after('<a href="#" id="buttonSave" class="btn btn-warning">Save</a>')
+        $("#buttonSave").click(function(){
+            $("#event-create-form").submit();
+        })
+    }
+    else if (wizard_edit.length) {
+        $("#wizard-edit .buttonFinish").after('<a href="#" id="buttonSave" class="btn btn-warning">Update</a>')
+        $("#buttonSave").click(function(){
+            $("#event-edit-form").submit();
+        })
+    }
+
+}
