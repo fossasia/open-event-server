6a24ba42ef0bd24473317ba98d5d667d5821397a
Niranjan Rajendran <niranjan94@yahoo.com>
2016-06-14 11:43:07 +0530

    Add speaker info, sponsors and organizer info

--- open_event/views/public/event_detail.py
+++ open_event/views/public/event_detail.py
@@ -13,9 +13,9 @@ class EventDetailView(BaseView):
     @expose('/<event_id>')
     def display_event_detail_home(self, event_id):
         event = DataGetter.get_event(event_id=event_id)
-
         if not event or event.state != 'Published':
             abort(404)
+        accepted_sessions = DataGetter.get_sessions(event_id)
 
-        return self.render('/gentelella/guest/event/details.html', event=event)
+        return self.render('/gentelella/guest/event/details.html', event=event, accepted_sessions=accepted_sessions)
 
