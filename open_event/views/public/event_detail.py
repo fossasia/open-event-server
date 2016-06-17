9cbec4ccf25a4f3f91164db6c217f51b3056111a
Niranjan Rajendran <niranjan94@yahoo.com>
2016-06-17 23:44:08 +0530

    Base view for anonymous user event->sessions

--- open_event/views/public/event_detail.py
+++ open_event/views/public/event_detail.py
@@ -1,8 +1,15 @@
+from flask import request
 from flask.ext.restplus import abort
 from flask_admin import BaseView, expose
 from werkzeug.utils import redirect
 
 from ...helpers.data_getter import DataGetter
+
+def get_published_event_or_abort(event_id):
+    event = DataGetter.get_event(event_id=event_id)
+    if not event or (event.state != u'Published' and event.state != 'Published'):
+        abort(404)
+    return event
 
 class EventDetailView(BaseView):
 
@@ -10,12 +17,22 @@ class EventDetailView(BaseView):
     def display_default(self):
         return redirect("/browse")
 
-    @expose('/<int:event_id>')
+    @expose('/<int:event_id>/')
     def display_event_detail_home(self, event_id):
-        event = DataGetter.get_event(event_id=event_id)
-        if not event or event.state != 'Published':
-            abort(404)
+        event = get_published_event_or_abort(event_id)
         accepted_sessions = DataGetter.get_sessions(event_id)
-
         return self.render('/gentelella/guest/event/details.html', event=event, accepted_sessions=accepted_sessions)
 
+    @expose('/<int:event_id>/sessions/')
+    def display_event_sessions(self, event_id):
+        event = get_published_event_or_abort(event_id)
+        sessions = DataGetter.get_sessions(event_id)
+        return self.render('/gentelella/guest/event/sessions.html', event=event, sessions=sessions)
+
+    @expose('/<int:event_id>/<slug>/')
+    def display_event_detail_home_slugged(self, event_id, slug):
+        return self.display_event_detail_home(event_id)
+
+    @expose('/<int:event_id>/<slug>/sessions/')
+    def display_event_sessions_slugged(self, event_id, slug):
+        return self.display_event_sessions(event_id)
