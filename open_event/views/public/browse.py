1dbe74bf718f31e495ffff21d67da19151e1abe8
Rafal Kowalski <rafal.arte@gmail.com>
2016-06-18 12:09:03 +0200

    add search and country geolocation

--- open_event/views/public/browse.py
+++ open_event/views/public/browse.py
@@ -1,10 +1,9 @@
-import flask_login
 from flask_admin import BaseView, expose
 from ...helpers.data_getter import DataGetter
 
 
 class BrowseView(BaseView):
-    @expose('/')
+    @expose('/' , methods=('GET', 'POST'))
     def browse(self, location):
         results = DataGetter.get_all_events()
         return self.render('/gentelella/guest/search/results.html', results=results, location=location)
