ed235c91f4ef07d7c75359f824ada2b983911418
Rafal Kowalski <rafal.arte@gmail.com>
2016-06-18 15:05:53 +0200

    fix: search

--- open_event/views/public/browse.py
+++ open_event/views/public/browse.py
@@ -3,7 +3,7 @@ from ...helpers.data_getter import DataGetter
 
 
 class BrowseView(BaseView):
-    @expose('/' , methods=('GET', 'POST'))
+    @expose('/', methods=('GET', 'POST'))
     def browse(self, location):
         results = DataGetter.get_all_events()
         return self.render('/gentelella/guest/search/results.html', results=results, location=location)
