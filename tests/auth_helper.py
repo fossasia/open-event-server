fb769556e5ab35989a1de9f07413de62b9942042
aviaryan <avi.aryan123@gmail.com>
2016-06-18 08:46:33 +0530

    fixed tests

--- tests/auth_helper.py
+++ tests/auth_helper.py
@@ -21,7 +21,13 @@ def register(app, email, password):
     s = get_serializer()
     data = [email, password]
     data_hash = s.dumps(data)
-    return app.get(url_for('admin.create_account_after_confirmation_view', hash=data_hash), follow_redirects=True)
+    app.post(
+        url_for('admin.register_view'),
+        data=dict(email=email, password=password),
+        follow_redirects=True)
+    return app.get(
+        url_for('admin.create_account_after_confirmation_view', hash=data_hash),
+        follow_redirects=True)
 
 
 def create_user(email, password):
