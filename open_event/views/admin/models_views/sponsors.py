799b21eca756602608ff391185c722cbbaf17c02
Aditya <adityavyas17@gmail.com>
2016-06-22 22:27:16 +0530

    Add sponsor widget to the details page

--- open_event/views/admin/models_views/sponsors.py
+++ open_event/views/admin/models_views/sponsors.py
@@ -6,10 +6,7 @@ from open_event.helpers.permission_decorators import *
 class SponsorsView(BaseView):
     @expose('/', methods=('GET', 'POST'))
     def index_view(self, event_id):
-        sponsors = DataGetter.get_sponsors(event_id)
-        event = DataGetter.get_event(event_id)
-        return self.render('/gentelella/admin/event/sponsors/display.html',
-                           sponsors=sponsors, event_id=event_id, event=event)
+        return ''
 
     @expose('/new/', methods=('GET', 'POST'))
     @can_access
@@ -17,12 +14,12 @@ class SponsorsView(BaseView):
         event = DataGetter.get_event(event_id)
         return self.render('/gentelella/admin/event/sponsors/new.html', event_id=event_id, event=event)
 
-    @expose('/<speaker_id>/delete/', methods=('POST',))
+    @expose('/<sponsor_id>/delete/', methods=('POST',))
     @can_access
     def delete_view(self, event_id,sponsor_id):
         return ''
 
-    @expose('/<speaker_id>/edit/', methods=('POST', 'GET'))
+    @expose('/<sponsor_id>/edit/', methods=('POST', 'GET'))
     @can_access
     def edit_view(self, event_id, sponsor_id):
         event = DataGetter.get_event(event_id)
