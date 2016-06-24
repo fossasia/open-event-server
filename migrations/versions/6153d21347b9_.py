8a5b95bf2d0736adad9803ebf43c1c11a1740255
Niranjan Rajendran <niranjan94@yahoo.com>
2016-06-25 00:17:26 +0530

    Merge migrations

---
+++ migrations/versions/6153d21347b9_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: 6153d21347b9
+Revises: ('d3fa4a1dbb8f', 'e42e322f7ffb')
+Create Date: 2016-06-25 00:17:00.173000
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '6153d21347b9'
+down_revision = ('d3fa4a1dbb8f', 'e42e322f7ffb')
+
+from alembic import op
+import sqlalchemy as sa
+import sqlalchemy_utils
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
