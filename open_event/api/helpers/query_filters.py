2d4f5b9bc0355ef5143390c4ec25a7ff8a3484ac
aviaryan <avi.aryan123@gmail.com>
2016-06-24 09:54:08 +0530

    added query by start_time and end_time in event API

--- open_event/api/helpers/query_filters.py
+++ open_event/api/helpers/query_filters.py
@@ -1,6 +1,7 @@
 from sqlalchemy import or_, func
 
 from open_event.models.event import Event
+from custom_fields import DateTime
 
 
 def extract_special_queries(queries):
@@ -40,11 +41,31 @@ def event_contains(value, query):
     return q
 
 
+def event_start_time_gt(value, query):
+    return query.filter(Event.start_time >= DateTime().from_str(value))
+
+
+def event_start_time_lt(value, query):
+    return query.filter(Event.start_time <= DateTime().from_str(value))
+
+
+def event_end_time_gt(value, query):
+    return query.filter(Event.end_time >= DateTime().from_str(value))
+
+
+def event_end_time_lt(value, query):
+    return query.filter(Event.end_time <= DateTime().from_str(value))
+
+
 #######
 # ADD CUSTOM FILTERS TO LIST
 #######
 
 
 FILTERS_LIST = {
-    '__event_contains': event_contains
+    '__event_contains': event_contains,
+    '__event_start_time_gt': event_start_time_gt,
+    '__event_start_time_lt': event_start_time_lt,
+    '__event_end_time_gt': event_end_time_gt,
+    '__event_end_time_lt': event_end_time_lt,
 }
