f0c2c863b3584beeabe65f6823143135c2e97435
Niranjan Rajendran <niranjan94@yahoo.com>
2016-06-26 16:59:45 +0530

    Add pagination to search results (#1167)

--- open_event/views/public/browse.py
+++ open_event/views/public/browse.py
@@ -1,22 +1,66 @@
+from flask.ext.restplus import abort
 from flask_admin import BaseView, expose
-from ...helpers.data_getter import DataGetter
+from objbrowser import browse
+
+from open_event.api.helpers.helpers import get_paginated_list
+from open_event.models.event import Event
 from flask import request
-import requests
-from urlparse import urlparse
 from flask_restplus import marshal
 from open_event.api.events import DAO as EventDAO, EVENT
 
+RESULTS_PER_PAGE = 2
+
+def get_paginated(**kwargs):
+
+    current_page = request.args.get('page')
+    if current_page:
+        current_page = int(current_page) - 1
+        if current_page < 0:
+            abort(404)
+    else:
+        current_page = 0
+
+    try:
+        return get_paginated_list(Event, request.path, {
+            'start': (current_page * RESULTS_PER_PAGE) + 1,
+            'limit': RESULTS_PER_PAGE,
+        }, **kwargs)
+    except:
+        return {
+            'start': 0,
+            'count': 0,
+            'limit': RESULTS_PER_PAGE,
+            'results': []
+        }
+
+def erase_from_dict(d, k):
+    if isinstance(d, dict):
+        if k in d.keys():
+            d.pop(k)
+            print(d)
 
 class BrowseView(BaseView):
     @expose('/', methods=('GET', 'POST'))
     def browse(self, location):
-        results = DataGetter.get_all_events()
-        return self.render('/gentelella/guest/search/results.html', results=results, location=location)
+        current_page = request.args.get('page')
+        if not current_page:
+            current_page = 1
+        else:
+            current_page = int(current_page)
+        results = get_paginated(privacy='public', state='Published')
+        return self.render('/gentelella/guest/search/results.html', results=results, location=location,
+                           current_page=current_page)
 
     @expose('/s', methods=('GET', 'POST'))
     def browses(self, location):
-        results = marshal(EventDAO.list(location_name=location, privacy='public', state='Published'), EVENT)
+        current_page = request.args.get('page')
+        if not current_page:
+            current_page = 1
+        else:
+            current_page = int(current_page)
+        results = get_paginated(location_name=location, privacy='public', state='Published')
         filters = request.args.items()
+        erase_from_dict(filters, 'page')
         if request.method == "POST":
             word = request.form['word']
             event_type = request.args.get('event_type', '')
@@ -29,11 +73,15 @@ class BrowseView(BaseView):
                                   type=event_type),
                     EVENT)
             if location and word:
-                results = marshal(EventDAO.list(location_name=location, __event_contains=word, privacy='public', state='Published'), EVENT)
+                results = get_paginated(location_name=location, __event_contains=word, privacy='public', state='Published')
             elif location:
-                results = marshal(EventDAO.list(location_name=location, privacy='public', state='Published'), EVENT)
+                results = get_paginated(location_name=location, privacy='public', state='Published')
             elif word:
-                results = marshal(EventDAO.list(__event_contains=word, privacy='public', state='Published'), EVENT)
-            return self.render('/gentelella/guest/search/results.html', results=results, location=location, filters=filters)
-        return self.render('/gentelella/guest/search/results.html', results=results, location=location, filters=filters)
+                results = get_paginated(__event_contains=word, privacy='public', state='Published')
+
+        return self.render('/gentelella/guest/search/results.html',
+                           results=results,
+                           location=location,
+                           filters=filters,
+                           current_page=current_page)
 
