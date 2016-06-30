eb6f25fae49f18d36b162a2bee2621e0241a720e
Shivam Mamgain <shivammamgain47@gmail.com>
2016-07-01 01:01:50 +0530

    Create notifications tab at admin-messages

--- open_event/views/admin/super_admin/messages.py
+++ open_event/views/admin/super_admin/messages.py
@@ -1,6 +1,7 @@
 from flask_admin import expose
 
 from open_event.helpers.system_mails import MAILS
+from open_event.helpers.system_notifications import NOTIFS
 from super_admin_base import SuperAdminBaseView
 
 
@@ -9,5 +10,6 @@ class SuperAdminMessagesView(SuperAdminBaseView):
     def index_view(self):
         return self.render(
             '/gentelella/admin/super_admin/messages/messages.html',
-            mails=MAILS
+            mails=MAILS,
+            notifications=NOTIFS
         )
