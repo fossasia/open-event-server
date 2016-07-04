29e9a6c27556d6ed436af523bede29836551faa4
saptaks <saptak013@gmail.com>
2016-07-05 02:22:06 +0530

    save floor value in microlocation

--- open_event/helpers/data.py
+++ open_event/helpers/data.py
@@ -820,7 +820,7 @@ class DataManager(object):
             track_color = form.getlist('tracks[color]')
 
             room_name = form.getlist('rooms[name]')
-            room_color = form.getlist('rooms[color]')
+            room_floor = form.getlist('rooms[floor]')
 
             sponsor_name = form.getlist('sponsors[name]')
             sponsor_url = form.getlist('sponsors[url]')
@@ -870,7 +870,7 @@ class DataManager(object):
 
             for index, name in enumerate(room_name):
                 if not string_empty(name):
-                    room = Microlocation(name=name, event_id=event.id)
+                    room = Microlocation(name=name, floor=room_floor[index], event_id=event.id)
                     db.session.add(room)
 
             for index, name in enumerate(sponsor_name):
@@ -980,7 +980,7 @@ class DataManager(object):
         track_color = form.getlist('tracks[color]')
 
         room_name = form.getlist('rooms[name]')
-        room_color = form.getlist('rooms[color]')
+        room_floor = form.getlist('rooms[floor]')
 
         sponsor_name = form.getlist('sponsors[name]')
         sponsor_logo_url = []
@@ -1031,7 +1031,7 @@ class DataManager(object):
 
         for index, name in enumerate(room_name):
             if not string_empty(name):
-                room, c = get_or_create(Microlocation, name=name, event_id=event.id)
+                room, c = get_or_create(Microlocation, name=name, floor=room_floor[index], event_id=event.id)
                 db.session.add(room)
 
         for sponsor in sponsors:
