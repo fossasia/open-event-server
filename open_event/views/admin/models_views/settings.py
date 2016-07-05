fb12614f25facd3e79b8639fe476c30e70a1263c
Niranjan Rajendran <niranjan94@yahoo.com>
2016-07-05 20:31:45 +0530

    Removed unused view

--- open_event/views/admin/models_views/settings.py
+++ open_event/views/admin/models_views/settings.py
@@ -23,14 +23,6 @@ class SettingsView(BaseView):
         return self.render('/gentelella/admin/settings/index.html',
                            settings=settings, events=events)
 
-    @expose('/edit/event/<event_id>', methods=('GET', 'POST'))
-    def edit_view(self, event_id):
-        if request.method == 'POST':
-            settings = DataManager.add_email_notification_settings(request.form, login.current_user.id, event_id)
-            return redirect(url_for('settings.index_view'))
-        event = DataGetter.get_event(event_id)
-        settings = DataGetter.get_email_notification_settings_by_event_id(login.current_user.id, event_id)
-        return self.render('/gentelella/admin/settings/edit.html', settings=settings, event=event)
 
     @expose('/toggle', methods=('GET', 'POST'))
     def toggle_view(self):
