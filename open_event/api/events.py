6fb4c0b8aab1b7255485b126434de8dcf8d808e8
Shivam Mamgain <shivammamgain47@gmail.com>
2016-07-05 13:05:39 +0530

    Add Copyright info to Events API

--- open_event/api/events.py
+++ open_event/api/events.py
@@ -4,11 +4,13 @@ from flask import g
 from open_event.models.event import Event as EventModel
 from open_event.models.social_link import SocialLink as SocialLinkModel
 from open_event.models.users_events_roles import UsersEventsRoles
+from open_event.models.event_copyright import EventCopyright
 from open_event.models.role import Role
 from open_event.models.user import ORGANIZER
 from open_event.helpers.data import save_to_db, update_version, record_activity
 
-from .helpers.helpers import get_paginated_list, requires_auth, parse_args
+from .helpers.helpers import get_paginated_list, requires_auth, parse_args, \
+    update_model, get_object_or_404
 from .helpers.utils import PAGINATED_MODEL, PaginatedResourceBase, \
     PAGE_PARAMS, POST_RESPONSES, PUT_RESPONSES, BaseDAO, ServiceDAO
 from .helpers import custom_fields as fields
@@ -20,6 +22,15 @@ api = Namespace('events', description='Events')
 EVENT_CREATOR = api.model('EventCreator', {
     'id': fields.Integer(),
     'email': fields.Email()
+})
+
+EVENT_COPYRIGHT = api.model('EventCopyright', {
+    'holder': fields.String(),
+    'holder_url': fields.Uri(),
+    'licence': fields.String(),
+    'licence_url': fields.Uri(),
+    'year': fields.Integer(),
+    'logo': fields.String()
 })
 
 SOCIAL_LINK = api.model('SocialLink', {
@@ -55,6 +66,7 @@ EVENT = api.model('Event', {
     'privacy': EventPrivacyField(),
     'ticket_url': fields.Uri(),
     'creator': fields.Nested(EVENT_CREATOR, allow_null=True),
+    'copyright': fields.Nested(EVENT_COPYRIGHT, allow_null=True),
     'schedule_published_on': fields.DateTime(),
     'code_of_conduct': fields.String(),
     'social_links': fields.List(fields.Nested(SOCIAL_LINK), attribute='social_link')
@@ -103,9 +115,19 @@ class EventDAO(BaseDAO):
     def create(self, data, url):
         data = self.validate(data)
         payload = self.fix_payload(data)
+        # Extract and save copyright info
+        copyright_data = payload.get('copyright')
+        payload.pop('copyright', None)
+        copyright = EventCopyright(**copyright_data)
+        save_to_db(copyright, 'Copyright info saved')
+
         new_event = self.model(**payload)
-        new_event.creator = g.user
         save_to_db(new_event, "Event saved")
+        # Set event creator
+        new_event.creator = g.user
+        # Set copyright info
+        new_event.copyright = copyright
+
         # set organizer
         role = Role.query.filter_by(name=ORGANIZER).first()
         uer = UsersEventsRoles(g.user, new_event, role)
@@ -123,6 +145,17 @@ class EventDAO(BaseDAO):
     def update(self, event_id, data):
         data = self.validate(data)
         payload = self.fix_payload(data)
+        # Extract and update copyright info
+        copyright_data = payload.get('copyright')
+        event = get_object_or_404(EventModel, event_id)
+        copyright = update_model(
+            EventCopyright,
+            event.copyright.id,
+            copyright_data,
+            event_id
+        )
+        save_to_db(copyright)
+        payload.pop('copyright', None)
         return BaseDAO.update(self, event_id, payload, validate=False)
 
 
