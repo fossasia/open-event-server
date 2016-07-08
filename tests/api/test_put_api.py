3b9e8c0a5d864c03310ee922841984a64622e1c4
aviaryan <avi.aryan123@gmail.com>
2016-07-08 11:58:18 +0530

    shorten code for social links and session type PUT API test

--- tests/api/test_put_api.py
+++ tests/api/test_put_api.py
@@ -39,28 +39,30 @@ class TestPutApi(TestPutApiBase):
     Test PUT APIs against 401 (unauthorized) and
     200 (successful) status codes
     """
-    def _test_model(self, name, data, *args):
+    def _test_model(self, name, data, path=None, *args):
         """
         Tests -
         1. Without login, try to do a PUT request and catch 401 error
         2. Login and match 200 response code and make sure that
             data changed
         """
-        path = get_path(1) if name == 'event' else get_path(1, name + 's', 1)
+        if not path:
+            path = get_path(1) if name == 'event' else get_path(1, name + 's', 1)
         response = self._put(path, data)
         self.assertEqual(401, response.status_code, msg=response.data)
         # login and send the request again
         self._login_user()
         response = self._put(path, data)
         self.assertEqual(200, response.status_code, msg=response.data)
-        # surrounded by quotes for strict checking
-        self.assertIn('"Test%s"' % str(name).title(), response.data)
+        # surrounded by quotes for strict checking.
+        # before PUT, name is TestX_1, after PUT: TestX
+        self.assertIn('"Test%s"' % (name[0].upper() + name[1:]), response.data)
         # check more things
         for _ in args:
             self.assertIn(_, response.data, response.data)
 
     def test_event_api(self):
-        self._test_model('event', POST_EVENT_DATA, 'Test licence')
+        self._test_model('event', POST_EVENT_DATA, None, 'Test licence')
 
     def test_track_api(self):
         self._test_model('track', POST_TRACK_DATA)
@@ -78,34 +80,15 @@ class TestPutApi(TestPutApiBase):
         self._test_model('sponsor', POST_SPONSOR_DATA)
 
     def test_social_link_api(self):
-        self._login_user()
-        # Update the social link
-        data = POST_SOCIAL_LINK_DATA
-        data['name'] = 'UpdatedSocialLink'
-        path = get_path(1, 'links', 1)
-        response = self._put(path, data)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('UpdatedSocialLink', response.data)
-
-        path = get_path(1, 'links')
-        response = self.app.get(path)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('UpdatedSocialLink', response.data)
+        self._test_model(
+            'socialLink', POST_SOCIAL_LINK_DATA, path=get_path(1, 'links', 1)
+        )
 
     def test_session_type_api(self):
-        self._login_user()
-        # Update the session type
-        data = POST_SESSION_TYPE_DATA
-        data['name'] = 'UpdatedSessionType'
-        path = get_path(1, 'sessions', 'types', 1)
-        response = self._put(path, data)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('UpdatedSessionType', response.data)
-
-        path = get_path(1, 'sessions', 'types')
-        response = self.app.get(path)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('UpdatedSessionType', response.data)
+        self._test_model(
+            'sessionType', POST_SESSION_TYPE_DATA,
+            path=get_path(1, 'sessions', 'types', 1)
+        )
 
 
 class TestPutApiMin(TestPutApiBase):
