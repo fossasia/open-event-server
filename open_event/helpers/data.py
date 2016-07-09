b7e3c7c2064fdf59c586622c2bfe0a99c849bcb7
saptaks <saptak013@gmail.com>
2016-07-09 23:07:01 +0530

    floors made non compulsory

--- open_event/helpers/data.py
+++ open_event/helpers/data.py
@@ -908,7 +908,9 @@ class DataManager(object):
 
             for index, name in enumerate(room_name):
                 if not string_empty(name):
-                    room = Microlocation(name=name, floor=room_floor[index], event_id=event.id)
+                    room = Microlocation(name=name,
+                                         floor=room_floor[index] if room_floor[index] != '' else None,
+                                         event_id=event.id)
                     db.session.add(room)
 
             for index, name in enumerate(sponsor_name):
@@ -1004,7 +1006,7 @@ class DataManager(object):
             save_to_db(track_new, "Track copy saved")
 
         for room in rooms_old:
-            room_new = Microlocation(name=room.name, event_id=event.id)
+            room_new = Microlocation(name=room.name, floor=room.floor, event_id=event.id)
             save_to_db(room_new, "Room copy saved")
 
         if call_for_papers_old:
@@ -1169,11 +1171,11 @@ class DataManager(object):
                                             id=room_id[index],
                                             event_id=event.id)
                     room.name = name
-                    room.floor = room_floor[index]
+                    room.floor = room_floor[index] if room_floor[index] != '' else None
                 else:
                     room, c = get_or_create(Microlocation,
                                             name=name,
-                                            floor=room_floor[index],
+                                            floor=room_floor[index] if room_floor[index] != '' else None,
                                             event_id=event.id)
                 db.session.add(room)
 
