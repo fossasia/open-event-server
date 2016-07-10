c41661f3eb52f9017b477424cbea55b74a1648bf
aviaryan <avi.aryan123@gmail.com>
2016-07-10 14:08:14 +0530

    import event delayed task working
    still need to fix issue with thrown exceptions. They are nullified

--- open_event/api/imports.py
+++ open_event/api/imports.py
@@ -1,4 +1,5 @@
 from flask.ext.restplus import Resource, Namespace, marshal
+from flask import jsonify, url_for
 
 from open_event.helpers.data import record_activity
 from helpers.import_helpers import get_file_from_request, import_event_json
@@ -15,6 +16,17 @@ class EventImportJson(Resource):
     @requires_auth
     def post(self):
         file = get_file_from_request(['zip'])
-        new_event = import_event_json(file)
-        record_activity('import_event', event_id=new_event.id)
-        return marshal(new_event, EVENT)
+        from helpers.tasks import import_event_task
+        task = import_event_task.delay(file)
+        # http://stackoverflow.com/questions/26379026/resolving-
+        # task = celery.current_app.send_task('import.event', [file])
+        return jsonify(
+            task_id=task.id,
+            task_url=url_for('api.extras_celery_task', task_id=task.id)
+        )
+
+
+def import_event_task_base(file):
+    new_event = import_event_json(file)
+    record_activity('import_event', event_id=new_event.id)
+    return marshal(new_event, EVENT)
