535b1bdd41822f455b6c38a2e17ef00e320a79aa
saptaks <saptak013@gmail.com>
2016-07-11 22:04:29 +0530

    Modified session form to single column

---
+++ migrations/versions/f1be1db3f6a4_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: f1be1db3f6a4
+Revises: ('dc0f787c88ae', 'c2e5de1111bd')
+Create Date: 2016-07-11 21:27:13.469653
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'f1be1db3f6a4'
+down_revision = ('dc0f787c88ae', 'c2e5de1111bd')
+
+from alembic import op
+import sqlalchemy as sa
+import sqlalchemy_utils
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
