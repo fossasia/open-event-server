77e5a93c3e6f44d6ba00e55b150b5108d5476d43
aviaryan <avi.aryan123@gmail.com>
2016-07-12 20:47:42 +0530

    added test

--- open_event/api/helpers/tasks.py
+++ open_event/api/helpers/tasks.py
@@ -2,6 +2,7 @@
 Define all API v2 celery tasks here
 This is done to resolve circular imports
 """
+import traceback
 from open_event import celery
 from open_event.helpers.request_context_task import RequestContextTask
 from errors import BaseError, ServerError
@@ -16,6 +17,8 @@ def import_event_task(self, file):
         item = import_event_task_base(file)
         return item
     except BaseError as e:
+        print traceback.format_exc()
         return {'__error': True, 'result': e.to_dict()}
     except Exception:
+        print traceback.format_exc()
         return {'__error': True, 'result': ServerError().to_dict()}
