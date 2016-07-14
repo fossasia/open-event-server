478d8687c9ac75c9fa5ebfdf405fe87b9b3e10c0
Rafał Kowalski <rafal.arte@gmail.com>
2016-07-14 18:17:48 +0200

    Unicode in searching by location fixed

--- open_event/api/events.py
+++ open_event/api/events.py
@@ -185,8 +185,8 @@ class EventResource():
     Event Resource Base class
     """
     event_parser = reqparse.RequestParser()
-    event_parser.add_argument('location', type=str, dest='__event_search_location')
-    event_parser.add_argument('contains', type=str, dest='__event_contains')
+    event_parser.add_argument('location', type=unicode, dest='__event_search_location')
+    event_parser.add_argument('contains', type=unicode, dest='__event_contains')
     event_parser.add_argument('state', type=str)
     event_parser.add_argument('privacy', type=str)
     event_parser.add_argument('type', type=str)
