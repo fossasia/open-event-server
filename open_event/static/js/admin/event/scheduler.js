c9b9b843d5d1237f4b6c4e07522538854da53131
Niranjan Rajendran <niranjan94@yahoo.com>
2016-07-14 22:14:45 +0530

    Ellipsize session box content

--- open_event/static/js/admin/event/scheduler.js
+++ open_event/static/js/admin/event/scheduler.js
@@ -251,7 +251,6 @@ function addSessionToTimeline(sessionRef, position, shouldBroadcast) {
     sessionRefObject.$sessionElement.css("height", minutesToPixels(sessionRefObject.session.duration) + "px");
     $microlocationsHolder.find(".microlocation[data-microlocation-id=" + sessionRefObject.session.microlocation.id + "] > .microlocation-inner").append(sessionRefObject.$sessionElement);
 
-    sessionRefObject.$sessionElement.ellipsis().ellipsis();
 
     updateSessionTimeOnTooltip(sessionRefObject.$sessionElement);
     updateColor(sessionRefObject.$sessionElement);
@@ -280,6 +279,8 @@ function addSessionToTimeline(sessionRef, position, shouldBroadcast) {
     _.remove(unscheduledStore, function (sessionTemp) {
         return sessionTemp.id === sessionRefObject.session.id
     });
+
+    sessionRefObject.$sessionElement.ellipsis().ellipsis();
 }
 
 /**
@@ -833,6 +834,7 @@ function initializeTimeline(eventId) {
         loadData(eventId, function () {
             $(".flash-message-holder").hide();
             $(".scheduler-holder").show();
+            $(".session").ellipsis();
             if (!isReadOnly()) {
                 initializeInteractables();
             } else {
