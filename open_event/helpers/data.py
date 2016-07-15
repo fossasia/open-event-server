7fc315f3c9871f1e99bce923db5cb0aaf1bca73c
Rafał Kowalski <rafal.arte@gmail.com>
2016-07-16 01:13:52 +0200

    Position ordering fixed

--- open_event/helpers/data.py
+++ open_event/helpers/data.py
@@ -1355,7 +1355,7 @@ class DataManager(object):
     def create_page(form):
 
         page = Page(name=form.get('name', ''), title=form.get('title', ''), description=form.get('description', ''),
-                    url=form.get('url', ''), place=form.get('place', ''))
+                    url=form.get('url', ''), place=form.get('place', ''), index=form.get('index', 0))
         save_to_db(page, "Page created")
 
     def update_page(self, page, form):
@@ -1364,6 +1364,7 @@ class DataManager(object):
         page.description = form.get('description', '')
         page.url = form.get('url', '')
         page.place = form.get('place', '')
+        page.index = form.get('index', '')
         save_to_db(page, "Page updated")
 
 def save_to_db(item, msg="Saved to db", print_error=True):
