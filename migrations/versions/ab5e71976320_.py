65a1d27dba8072df825a1153955e326e31c60d99
Niranjan Rajendran <niranjan94@yahoo.com>
2016-07-24 21:09:59 +0530

    merged migrations

---
+++ migrations/versions/ab5e71976320_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: ab5e71976320
+Revises: ('6415c74704d9', '673cf7219a71')
+Create Date: 2016-07-24 21:08:46.637000
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'ab5e71976320'
+down_revision = ('6415c74704d9', '673cf7219a71')
+
+from alembic import op
+import sqlalchemy as sa
+import sqlalchemy_utils
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
