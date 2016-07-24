f74c9b92a437b59392a6eb842bced5d97f3d8701
Niranjan Rajendran <niranjan94@yahoo.com>
2016-07-24 16:21:04 +0530

    Hide public pages based on privacy and state

--- app/views/public/event_detail.py
+++ app/views/public/event_detail.py
@@ -1,3 +1,4 @@
+import inspect
 import json
 import logging
 import os
@@ -36,8 +37,8 @@ class EventDetailView(BaseView):
 
     @expose('/<int:event_id>/')
     def display_event_detail_home(self, event_id):
-        call_for_speakers = DataGetter.get_call_for_papers(event_id).first()
         event = get_published_event_or_abort(event_id)
+        call_for_speakers = DataGetter.get_call_for_papers(event_id).first()
         accepted_sessions = DataGetter.get_sessions(event_id)
         if event.copyright:
             licence_details = DataGetter.get_licence_details(event.copyright.licence)
@@ -51,8 +52,10 @@ class EventDetailView(BaseView):
 
     @expose('/<int:event_id>/sessions/')
     def display_event_sessions(self, event_id):
-        call_for_speakers = DataGetter.get_call_for_papers(event_id).first()
         event = get_published_event_or_abort(event_id)
+        if not event.has_session_speakers:
+            abort(404)
+        call_for_speakers = DataGetter.get_call_for_papers(event_id).first()
         tracks = DataGetter.get_tracks(event_id)
         accepted_sessions = DataGetter.get_sessions(event_id)
         if not accepted_sessions:
@@ -62,6 +65,8 @@ class EventDetailView(BaseView):
     @expose('/<int:event_id>/schedule/')
     def display_event_schedule(self, event_id):
         event = get_published_event_or_abort(event_id)
+        if not event.has_session_speakers:
+            abort(404)
         tracks = DataGetter.get_tracks(event_id)
         accepted_sessions = DataGetter.get_sessions(event_id)
         if not accepted_sessions or not event.schedule_published_on:
@@ -71,12 +76,19 @@ class EventDetailView(BaseView):
     @expose('/<int:event_id>/cfs/', methods=('GET',))
     def display_event_cfs(self, event_id):
         event = get_published_event_or_abort(event_id)
+        if not event.has_session_speakers:
+            abort(404)
+
         call_for_speakers = DataGetter.get_call_for_papers(event_id).first()
         accepted_sessions = DataGetter.get_sessions(event_id)
 
         if not call_for_speakers:
             abort(404)
 
+        calling_frame = inspect.getouterframes(inspect.currentframe(), 2)
+        if call_for_speakers.privacy != 'public' and calling_frame[1][3] != 'display_event_cfs_via_hash':
+            abort(404)
+            
         form_elems = DataGetter.get_custom_form_elements(event_id)
         speaker_form = json.loads(form_elems.speaker_form)
         session_form = json.loads(form_elems.session_form)
@@ -101,6 +113,9 @@ class EventDetailView(BaseView):
     @expose('/<int:event_id>/cfs/', methods=('POST',))
     def process_event_cfs(self, event_id):
         email = request.form['email']
+        event = DataGetter.get_event(event_id)
+        if not event.has_session_speakers:
+            abort(404)
         DataManager.add_session_to_event(request, event_id)
         if login.current_user.is_authenticated:
             flash("Your session proposal has been submitted", "success")
