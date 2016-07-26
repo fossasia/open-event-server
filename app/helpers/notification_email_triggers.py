bd0f42ad0c0dd6b03f73a217ff5eb5bdc40b185c
Saptak Sengupta <saptak013@gmail.com>
2016-07-26 18:43:37 +0530

    Fixed Email notification on setting

--- app/helpers/notification_email_triggers.py
+++ app/helpers/notification_email_triggers.py
@@ -2,6 +2,9 @@ from flask import url_for
 from app.helpers.data_getter import DataGetter
 from app.helpers.helpers import send_new_session_organizer, send_notif_new_session_organizer, \
     send_notif_session_accept_reject, send_session_accept_reject, send_schedule_change, send_notif_session_schedule
+from ..models.mail import INVITE_PAPERS, NEW_SESSION, \
+    EVENT_ROLE, SESSION_ACCEPT_REJECT, \
+    SESSION_SCHEDULE, NEXT_EVENT, EVENT_PUBLISH, AFTER_EVENT
 
 
 def trigger_new_session_notifications(session_id, event_id=None, event=None):
@@ -14,10 +17,12 @@ def trigger_new_session_notifications(session_id, event_id=None, event=None):
     link = url_for('event_sessions.session_display_view',
                    event_id=event.id, session_id=session_id, _external=True)
 
+    admin_msg_setting = DataGetter.get_message_setting_by_action(NEW_SESSION)
     organizers = DataGetter.get_user_event_roles_by_role_name(event_id, 'organizer')
     for organizer in organizers:
         email_notification_setting = DataGetter.get_email_notification_settings_by_event_id(organizer.user.id, event_id)
-        if email_notification_setting and email_notification_setting.new_paper == 1:
+        if (email_notification_setting and email_notification_setting.new_paper == 1
+                and admin_msg_setting.user_control_status == 1) or admin_msg_setting.user_control_status == 0:
             send_new_session_organizer(organizer.user.email, event.name, link)
         # Send notification
         send_notif_new_session_organizer(organizer.user, event.name, link)
