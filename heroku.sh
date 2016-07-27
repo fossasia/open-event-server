44f9edd40c32266190b3c77549fcb8243ecba86d
Shivam Mamgain <shivammamgain47@gmail.com>
2016-07-27 20:24:02 +0530

    Update `heroku.sh`

--- heroku.sh
+++ heroku.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 celery worker -A app.celery &
-gunicorn app:app
+gunicorn app:app --worker-class eventlet -w 1
 # if not running on free dyno
 # define a separate worker and scale
 # https://devcenter.heroku.com/articles/celery-heroku
