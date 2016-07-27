d6c9377b5420dfc45492565e0de038c8ceab15fd
Saptak Sengupta <saptak013@gmail.com>
2016-07-27 08:52:44 +0530

    Notification read on clicking notification

--- app/helpers/notification_email_triggers.py
+++ app/helpers/notification_email_triggers.py
@@ -6,7 +6,6 @@ from ..models.mail import NEW_SESSION, SESSION_ACCEPT_REJECT, SESSION_SCHEDULE
 
 
 def trigger_new_session_notifications(session_id, event_id=None, event=None):
-
     if not event and not event_id:
         raise Exception('event or event_id is required')
     if not event:
@@ -16,13 +15,14 @@ def trigger_new_session_notifications(session_id, event_id=None, event=None):
                    event_id=event.id, session_id=session_id, _external=True)
 
     admin_msg_setting = DataGetter.get_message_setting_by_action(NEW_SESSION)
-    organizers = DataGetter.get_user_event_roles_by_role_name(event_id, 'organizer')
+    organizers = DataGetter.get_user_event_roles_by_role_name(event.id, 'organizer')
     for organizer in organizers:
         email_notification_setting = DataGetter.get_email_notification_settings_by_event_id(organizer.user.id, event_id)
         if (email_notification_setting and email_notification_setting.new_paper == 1
                 and admin_msg_setting.user_control_status == 1) or not admin_msg_setting or admin_msg_setting.user_control_status == 0:
             send_new_session_organizer(organizer.user.email, event.name, link)
         # Send notification
+        print " notif Sending"
         send_notif_new_session_organizer(organizer.user, event.name, link)
 
 
@@ -37,7 +37,7 @@ def trigger_session_state_change_notifications(session, event_id, state=None):
                 admin_msg_setting.user_control_status == 1) or not admin_msg_setting or admin_msg_setting.user_control_status == 0:
             send_session_accept_reject(speaker.email, session.title, state, link)
             # Send notification
-            send_notif_session_accept_reject(speaker.user, session.title, state, link)
+        send_notif_session_accept_reject(speaker.user, session.title, state, link)
 
 
 def trigger_session_schedule_change_notifications(session, event_id):
@@ -49,4 +49,4 @@ def trigger_session_schedule_change_notifications(session, event_id):
                 admin_msg_setting.user_control_status == 1) or not admin_msg_setting or admin_msg_setting.user_control_status == 0:
             send_schedule_change(speaker.email, session.title, link)
             # Send notification
-            send_notif_session_schedule(speaker.user, session.title, link)
+        send_notif_session_schedule(speaker.user, session.title, link)
