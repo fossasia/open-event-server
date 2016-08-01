92e91385099299b0c372d016092ee14d0ca815c6
Saptak Sengupta <saptak013@gmail.com>
2016-08-01 08:52:38 +0530

    Images added

--- app/helpers/static.py
+++ app/helpers/static.py
@@ -140,3 +140,28 @@ EVENT_TOPICS = {
         "Canoeing", "Climbing", "Hiking", "Kayaking", "Other", "Rafting", "Travel"
     ]
 }
+
+# Event Images with Event Topics
+
+DEFAULT_EVENT_IMAGES = {
+    'Auto, Boat & Air': '',
+    'Business & Professional': '',
+    'Charity & Causes': '',
+    'Community & Culture': '',
+    'Family & Education': '',
+    'Fashion & Beauty': '',
+    'Film, Media & Entertainment': '',
+    'Food & Drink': '',
+    'Government & Politics': '',
+    'Health & Wellness': '',
+    'Hobbies & Special Interest': '',
+    'Home & Lifestyle': '',
+    'Music': '',
+    'Other': '',
+    'Performing & Visual Arts': '',
+    'Religion & Spirituality': '',
+    'Science & Technology': '',
+    'Seasonal & Holiday': '',
+    'Sports & Fitness': '',
+    'Travel & Outdoor': ''
+}
