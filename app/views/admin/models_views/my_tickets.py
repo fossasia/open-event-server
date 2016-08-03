c0a7835bcfb9a94d40bfb98941369bef0f0fb71a
aviaryan <avi.aryan123@gmail.com>
2016-08-03 10:00:01 +0530

    fixed mytickets page tab headings

--- app/views/admin/models_views/my_tickets.py
+++ app/views/admin/models_views/my_tickets.py
@@ -1,15 +1,15 @@
-
 import flask_login
 from flask_admin import BaseView, expose
 from app.helpers.ticketing import TicketingManager
+
 
 class MyTicketsView(BaseView):
 
     @expose('/')
     @flask_login.login_required
     def display_my_tickets(self):
-        page_content = {"tab_upcoming_events": "Upcoming Sessions",
-                        "tab_past_events": "Past Sessions",
+        page_content = {"tab_upcoming_events": "Upcoming Events",
+                        "tab_past_events": "Past Events",
                         "title": "My Tickets"}
 
         upcoming_events_orders = TicketingManager.get_orders_of_user(upcoming_events=True)
