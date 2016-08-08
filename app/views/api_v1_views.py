24db77e1809ac7894b9b59b472238cdde7107d39
Rafał Kowalski <rafal.arte@gmail.com>
2016-08-08 14:54:49 +0200

    connection with facebook added

--- app/views/api_v1_views.py
+++ app/views/api_v1_views.py
@@ -420,6 +420,7 @@ def facebook_callback():
         response = facebook.get(FbOAuth.get_user_info())
         if response.status_code == 200:
             user_info = response.json()
+            print user_data
             email = user_info['email']
             user_email = DataGetter.get_user_by_email(email, no_flash=True)
             user = create_user_oauth(user_email, user_info, token=token, method='Facebook')
