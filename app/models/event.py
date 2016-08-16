46874e592825cafd740a6a8284ab349b580b3be5
Rafał Kowalski <rafal.arte@gmail.com>
2016-08-16 08:05:17 +0200

    Event model change

--- app/models/event.py
+++ app/models/event.py
@@ -133,7 +133,7 @@ class Event(db.Model):
                  pay_by_paypal=None,
                  pay_by_stripe=None,
                  pay_by_cheque=None,
-                 identifier=get_new_event_identifier(),
+                 identifier=None,
                  pay_by_bank=None,
                  pay_onsite=None):
 
@@ -177,7 +177,7 @@ class Event(db.Model):
         self.pay_by_cheque = pay_by_cheque
         self.pay_by_bank = pay_by_bank
         self.pay_onsite = pay_onsite
-        self.identifier = identifier
+        self.identifier = get_new_event_identifier()
 
     def __repr__(self):
         return '<Event %r>' % self.name
