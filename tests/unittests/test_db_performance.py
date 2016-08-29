1fb96f40fe8167b5d897e8655a67c3a4fdfb68a9
Aditya <adityavyas17@gmail.com>
2016-08-29 23:31:40 +0530

    Updated methods for testing database

--- tests/unittests/test_db_performance.py
+++ tests/unittests/test_db_performance.py
@@ -4,7 +4,7 @@ import time
 from flask import url_for
 from werkzeug.contrib.profiler import ProfilerMiddleware
 
-from tests.unittests.views.view_test_case import OpenEventTestCase
+from tests.unittests.views.view_test_case import OpenEventViewTestCase
 from app.models import db
 from app import current_app as app
 from populate_db import populate
@@ -13,7 +13,7 @@ from flask.ext.sqlalchemy import get_debug_queries
 from config import ProductionConfig
 from tests.unittests.setup_database import Setup
 
-class TestEvents(OpenEventTestCase):
+class TestEvents(OpenEventViewTestCase):
     def setUp(self):
         self.app = Setup.create_app()
         app.config['TESTING'] = True
@@ -26,51 +26,58 @@ class TestEvents(OpenEventTestCase):
 
     def test_db_events(self):
         with app.test_request_context():
-            start = time.clock()
             for i in range(1, 10000):
                 event = ObjectMother.get_event()
                 event.name = 'Event' + str(i)
                 db.session.add(event)
             db.session.commit()
             url = url_for('sadmin_events.index_view')
+            start = time.clock()
             self.app.get(url, follow_redirects=True)
             print time.clock() - start
-            for query in get_debug_queries():
-                if query.duration >= ProductionConfig.DATABASE_QUERY_TIMEOUT:
-                    app.logger.warning("SLOW QUERY: %s\nParameters: %s\nDuration: %fs\nContext: %s\n" % (
-                        query.statement, query.parameters, query.duration, query.context))
+            with open("output_events.txt", "w") as text_file:
+                for query in get_debug_queries():
+                    if query.duration >= ProductionConfig.DATABASE_QUERY_TIMEOUT:
+                        text_file.write("SLOW QUERY: %s\nParameters: %s\nDuration: %fs\nContext: %s\n" % (
+                            query.statement, query.parameters, query.duration, query.context))
+                        text_file.write("\n")
 
     def test_db_sessions(self):
         with app.test_request_context():
-            start = time.clock()
             for i in range(1, 10000):
                 session = ObjectMother.get_session()
                 session.name = 'Session' + str(i)
                 db.session.add(session)
             db.session.commit()
             url = url_for('sadmin_sessions.display_my_sessions_view')
+            start = time.clock()
             self.app.get(url, follow_redirects=True)
             print time.clock() - start
-            for query in get_debug_queries():
-                if query.duration >= ProductionConfig.DATABASE_QUERY_TIMEOUT:
-                    app.logger.warning("SLOW QUERY: %s\nParameters: %s\nDuration: %fs\nContext: %s\n" % (
-                        query.statement, query.parameters, query.duration, query.context))
+            with open("output_session.txt", "w") as text_file:
+                for query in get_debug_queries():
+                    if query.duration >= ProductionConfig.DATABASE_QUERY_TIMEOUT:
+                        text_file.write("SLOW QUERY: %s\nParameters: %s\nDuration: %fs\nContext: %s\n" % (
+                            query.statement, query.parameters, query.duration, query.context))
+                        text_file.write("\n")
 
     def test_db_users(self):
         with app.test_request_context():
-            start = time.clock()
             for i in range(1, 10000):
                 user = ObjectMother.get_user()
                 user.email = 'User' + str(i)
                 db.session.add(user)
             db.session.commit()
             url = url_for('sadmin_users.index_view')
+            start = time.clock()
             self.app.get(url, follow_redirects=True)
             print time.clock() - start
-            for query in get_debug_queries():
-                if query.duration >= ProductionConfig.DATABASE_QUERY_TIMEOUT:
-                    app.logger.warning("SLOW QUERY: %s\nParameters: %s\nDuration: %fs\nContext: %s\n" % (
-                        query.statement, query.parameters, query.duration, query.context))
+            with open("output_users.txt", "w") as text_file:
+                for query in get_debug_queries():
+                    if query.duration >= ProductionConfig.DATABASE_QUERY_TIMEOUT:
+                        text_file.write("SLOW QUERY: %s\nParameters: %s\nDuration: %fs\nContext: %s\n" % (
+                            query.statement, query.parameters, query.duration, query.context))
+                        text_file.write("\n")
+
 
 
     def tearDown(self):
