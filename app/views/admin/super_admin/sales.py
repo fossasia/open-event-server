ac95fdab425b3c81e293876e838e1c32a1ba7342
Niranjan Rajendran <niranjan94@yahoo.com>
2016-10-18 19:56:25 +0530

    Added by marketer and by discount code

--- app/views/admin/super_admin/sales.py
+++ app/views/admin/super_admin/sales.py
@@ -15,6 +15,8 @@ from app.helpers.data import save_to_db, delete_from_db
 from app.helpers.data_getter import DataGetter
 from app.helpers.payment import get_fee
 from app.models.user import User
+from app.models.event import Event
+from app.helpers.cached_getter import CachedGetter
 from app.models.system_role import CustomSysRole, UserSystemRole
 from app.views.admin.super_admin.super_admin_base import SuperAdminBaseView, SALES
 from app.helpers.ticketing import TicketingManager
@@ -71,7 +73,7 @@ class SuperAdminSalesView(SuperAdminBaseView):
             for order_ticket in order.tickets:
                 fee_summary[str(order.event.id)]['tickets_count'] += order_ticket.quantity
                 tickets_total += order_ticket.quantity
-                ticket = TicketingManager.get_ticket(order_ticket.ticket_id)
+                ticket = CachedGetter.get_ticket(order_ticket.ticket_id)
                 if order.paid_via != 'free' and order.amount > 0 and ticket.price > 0:
                     fee = ticket.price * (get_fee(order.event.payment_currency)/100)
                     fee = forex(order.event.payment_currency, self.display_currency, fee)
@@ -114,7 +116,7 @@ class SuperAdminSalesView(SuperAdminBaseView):
                            to_date=to_date)
 
     @expose('/marketer/')
-    def sales_by_marketer_view(self):
+    def sales_by_marketer_view(self, by_discount_code=False):
         from_date = request.args.get('from_date')
         to_date = request.args.get('to_date')
 
@@ -124,34 +126,94 @@ class SuperAdminSalesView(SuperAdminBaseView):
 
             return redirect(url_for('.sales_by_marketer_view'))
 
-        if from_date and to_date:
-            orders = TicketingManager.get_orders(
-                from_date=datetime.strptime(from_date, '%d/%m/%Y'),
-                to_date=datetime.strptime(to_date, '%d/%m/%Y')
-            )
-        else:
-            orders = TicketingManager.get_orders()
-
-        events = DataGetter.get_all_events()
-
-        completed_count = 0
-        completed_amount = 0
-        tickets_count = 0
-        discounts_amount = 0
-
         orders_summary = {
-            'completed': {
-                'class': 'success',
-                'tickets_count': 0,
-                'orders_count': 0,
-                'total_sales': 0,
-                'total_discounts': 0
-            }
+            'tickets_count': 0,
+            'orders_count': 0,
+            'total_sales': 0,
+            'total_discounts': 0
         }
 
         tickets_summary = {}
 
+        if not by_discount_code:
+            user_roles = UserSystemRole.query.filter(CustomSysRole.name == 'Marketer').all()
 
+            active_users_ids = [x.id for x in user_roles]
+
+            marketers = User.query.filter(User.id.in_(active_users_ids)).all()
+
+            for marketer in marketers:
+                tickets_summary[str(marketer.id)] = {
+                    'email': marketer.email,
+                    'name': marketer.user_detail.firstname,
+                    'tickets_count': 0,
+                    'sales': 0,
+                    'discounts': 0
+                }
+        else:
+            discount_codes = InvoicingManager.get_discount_codes()
+            for discount_code in discount_codes:
+                tickets_summary[str(discount_code.id)] = {
+                    'email': discount_code.code,
+                    'name': str(discount_code.value) + '% off for ' + str(discount_code.max_quantity) + ' months',
+                    'tickets_count': 0,
+                    'sales': 0,
+                    'discounts': 0,
+                    'marketer': discount_code.marketer.email
+                }
+
+        events = DataGetter.get_all_events_with_discounts()
+
+        for event in events:
+            temp_month_summary = {}
+            print event
+            discount_coupon = CachedGetter.get_discount_code(event.discount_code_id)
+
+            if not by_discount_code:
+                key = str(discount_coupon.marketer_id)
+            else:
+                key = str(discount_coupon.id)
+
+            if from_date and to_date:
+                orders = TicketingManager.get_orders(
+                    from_date=datetime.strptime(from_date, '%d/%m/%Y'),
+                    to_date=datetime.strptime(to_date, '%d/%m/%Y'),
+                    status='completed',
+                    event_id=event.id
+                )
+            else:
+                orders = TicketingManager.get_orders(status='completed', event_id=event.id)
+
+            for order in orders:
+                if order.status == 'completed' and order.paid_via != 'free':
+                    orders_summary['orders_count'] += 1
+                    key = '{month:02d}{year:d}'.format(month=order.completed_at.month, year=order.completed_at.year)
+                    temp_month_summary[key] = order.amount
+                    orders_summary['sales'] += order.amount
+                    for order_ticket in order.tickets:
+                        tickets_summary[key]['tickets_count'] += order_ticket.quantity
+                        orders_summary['tickets_count'] += order_ticket.quantity
+
+            # Calculate discount on a monthly basis
+            month_count = 1
+            for amount in temp_month_summary:
+                if discount_coupon.max_quantity <= month_count:
+                    tickets_summary[key]['discounts'] \
+                        += amount * (discount_coupon.value/100)
+                    orders_summary['total_discounts'] += amount * (discount_coupon.value / 100)
+                month_count += 1
+
+        return self.render('/gentelella/admin/super_admin/sales/by_marketer.html',
+                           tickets_summary=tickets_summary,
+                           display_currency=self.display_currency,
+                           from_date=from_date,
+                           to_date=to_date,
+                           key_name='marketers' if not by_discount_code else 'discount codes',
+                           orders_summary=orders_summary)
+
+    @expose('/discount_code/')
+    def sales_by_discount_code_view(self):
+        return self.sales_by_marketer_view(True)
 
     @expose('/<path>/')
     def sales_by_events_view(self, path):
@@ -244,7 +306,7 @@ class SuperAdminSalesView(SuperAdminBaseView):
                                                                       self.display_currency, order.amount)
             for order_ticket in order.tickets:
                 orders_summary[str(order.status)]['tickets_count'] += order_ticket.quantity
-                ticket = TicketingManager.get_ticket(order_ticket.ticket_id)
+                ticket = CachedGetter.get_ticket(order_ticket.ticket_id)
                 tickets_summary_event_wise[str(order.event_id)][str(order.status)]['tickets_count'] \
                     += order_ticket.quantity
                 tickets_summary_organizer_wise[str(order.event.creator_id)][str(order.status)]['tickets_count'] \
