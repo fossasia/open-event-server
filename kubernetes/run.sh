79e396d742a037c44fc8ea5c3b7fbcd1256e3803
Niranjan Rajendran <niranjan94@yahoo.com>
2016-11-02 20:14:21 +0530

    Temporary hack to get database ready

--- kubernetes/run.sh
+++ kubernetes/run.sh
@@ -6,4 +6,10 @@ curl -sL https://deb.nodesource.com/setup_4.x | bash && apt-get install -y --for
 npm install bower -g && npm cache clean
 pip install --no-cache-dir -r requirements.txt
 bower install --allow-root && bower cache clean --allow-root
+export REDIS_URL=redis://${REDIS_SERVICE_HOST}:${REDIS_SERVICE_PORT}/0
+export DATABASE_URL=postgresql://postgres:test@${POSTGRES_SERVICE_HOST}:${POSTGRES_SERVICE_PORT}/opev
+export SERVER_NAME=${WEB_SERVICE_HOST}
+pwd
+python manage.py initialize_db -c open_event_test_user@fossasia.org:fossasia # TODO. Temporary hack. will be removed
+python manage.py db stamp $(python manage.py db heads | grep -o '^\S*') # TODO. Temporary hack. will be removed
 bash docker_run.sh
