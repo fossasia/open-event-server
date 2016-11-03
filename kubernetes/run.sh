b376b5bede31ca57e585d92d74deb31984220cbb
Niranjan Rajendran <niranjan94@yahoo.com>
2016-11-03 18:09:22 +0530

    Added new Dockerimage

--- kubernetes/run.sh
+++ kubernetes/run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-apt-get update
-apt-get install -y wget git ca-certificates curl && update-ca-certificates && apt-get clean -y
-apt-get install -y --no-install-recommends build-essential python-dev libpq-dev libevent-dev libmagic-dev git && apt-get clean -y
-curl -sL https://deb.nodesource.com/setup_4.x | bash && apt-get install -y --force-yes nodejs && apt-get clean -y
-npm install bower -g && npm cache clean
-pip install --no-cache-dir -r requirements.txt
-bower install --allow-root && bower cache clean --allow-root
 export REDIS_URL=redis://${REDIS_SERVICE_HOST}:${REDIS_SERVICE_PORT}/0
 export DATABASE_URL=postgresql://postgres:test@${POSTGRES_SERVICE_HOST}:${POSTGRES_SERVICE_PORT}/opev
 export SERVER_NAME=${WEB_SERVICE_HOST}
