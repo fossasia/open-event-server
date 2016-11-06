f9a5c9c91c38cfb3edb4bd2b886f9c2a3b1ea26a
Niranjan Rajendran <niranjan94@yahoo.com>
2016-11-06 20:19:10 +0530

    Added celery deployment

--- kubernetes/run.sh
+++ kubernetes/run.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+echo "Deploying ${DEPLOYMENT}"
 export REDIS_URL=redis://${REDIS_SERVICE_HOST}:${REDIS_SERVICE_PORT}/0
 export DATABASE_URL=postgresql://postgres:test@${POSTGRES_SERVICE_HOST}:${POSTGRES_SERVICE_PORT}/opev
 pip install --no-cache-dir -r requirements.txt
-bower install --allow-root && bower cache clean --allow-root
+if [ "$DEPLOYMENT" == "web" ]
+then
+    bower install --allow-root && bower cache clean --allow-root
+fi
 pwd
 python manage.py initialize_db -c open_event_test_user@fossasia.org:fossasia # TODO. Temporary hack. will be removed
 python manage.py db upgrade > /dev/null 2>&1
 python manage.py db stamp head # TODO. Temporary hack. will be removed
-bash docker_run.sh
+if [ "$DEPLOYMENT" == "web" ]
+then
+    gunicorn -b 0.0.0.0:5000 app:app --worker-class eventlet -w 1
+fi
+if [ "$DEPLOYMENT" == "celery" ]
+then
+    export INTEGRATE_SOCKETIO=false
+    celery worker -A app.celery --loglevel=debug
+fi
