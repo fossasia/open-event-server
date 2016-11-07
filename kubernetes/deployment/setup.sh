3e03a79d2b45acd3f05b3f6ba4453946f4815191
Niranjan Rajendran <niranjan94@yahoo.com>
2016-11-07 20:56:47 +0530

    Continuous deployment to Google Container Engine

---
+++ kubernetes/deployment/setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/open-event-orga-server" ]; then
+    echo "Just a PR. Skip google cloud setup."
+    exit 0
+fi
+
+# Decrypt the credentials we added to the repo using the key we added with the Travis command line tool
+openssl aes-256-cbc -K $encrypted_27e15b7757b4_key -iv $encrypted_27e15b7757b4_iv -in eventyay-8245fde7ab8a.json.enc -out eventyay-8245fde7ab8a.json -d
+if [ ! -d ${HOME}/google-cloud-sdk ]; then
+    curl https://sdk.cloud.google.com | bash;
+    gcloud components install kubectl
+fi
+
+mkdir -p lib
+gcloud auth activate-service-account --key-file eventyay-8245fde7ab8a.json
+gcloud config set project eventyay
+gcloud container clusters get-credentials eventyay-cluster
