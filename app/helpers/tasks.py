53edd9e17bbae41e37b431de87fcc1e68cd27541
Niranjan Rajendran <niranjan94@yahoo.com>
2016-11-15 19:20:31 +0530

    Added SMTP Mailer celery task

--- app/helpers/tasks.py
+++ app/helpers/tasks.py
@@ -1,6 +1,7 @@
 import requests
 from app import celery
-
+from marrow.mailer import Mailer, Message
+from app.helpers.versioning import strip_tags
 
 @celery.task(name='send.email.post')
 def send_email_task(payload, headers):
@@ -9,3 +10,27 @@ def send_email_task(payload, headers):
         data=payload,
         headers=headers
     )
+
+
+@celery.task(name='send.email.post.smtp')
+def send_mail_via_smtp_task(config, payload):
+
+    mailer_config = {
+        'transport': {
+            'use': 'smtp',
+            'host': config['host'],
+            'username': config['username'],
+            'password': config['password'],
+            'tls': config['encryption'],
+            'port': config['port']
+        }
+    }
+
+    mailer = Mailer(mailer_config)
+    mailer.start()
+    message = Message(author=payload['from'], to=payload['to'])
+    message.subject = payload['subject']
+    message.plain = strip_tags(payload['html'])
+    message.rich = payload['html']
+    mailer.send(message)
+    mailer.stop()
