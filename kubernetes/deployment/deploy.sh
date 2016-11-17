0bf322b5ea73c93ef26d3fbe635a2485d3146cda
Niranjan Rajendran <niranjan94@yahoo.com>
2016-11-17 18:04:20 +0530

    Build docker image and patch in travis

--- kubernetes/deployment/deploy.sh
+++ kubernetes/deployment/deploy.sh
@@ -5,5 +5,9 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/open-
     exit 0
 fi
 
-kubectl patch deployment web -p \ "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
-kubectl patch deployment celery -p \ "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
+cd kubernetes/image
+docker build -t gcr.io/eventyay/web:$TRAVIS_COMMIT .
+docker tag gcr.io/eventyay/web:$TRAVIS_COMMIT gcr.io/eventyay/web:latest
+gcloud docker -- push gcr.io/eventyay/web
+kubectl patch deployment web -p \ '{"spec":{"template":{"spec":{"containers":[{"name":"web","image":"gcr.io/eventyay/web:'"$TRAVIS_COMMIT"'"}]}}}}'
+kubectl patch deployment celery -p \ '{"spec":{"template":{"spec":{"containers":[{"name":"celery","image":"gcr.io/eventyay/web:'"$TRAVIS_COMMIT"'"}]}}}}'
