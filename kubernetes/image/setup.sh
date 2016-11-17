00a6f0a95f229b83144b9b2fa8a220e0925e1df9
Niranjan Rajendran <niranjan94@yahoo.com>
2016-11-17 17:55:37 +0530

    Change docker

--- kubernetes/image/setup.sh
+++ kubernetes/image/setup.sh
@@ -2,5 +2,7 @@
 git clone ${REPOSITORY} open_event
 cd open_event
 git checkout ${BRANCH}
+pip install --no-cache-dir -r requirements.txt
+bower install --allow-root && bower cache clean --allow-root
 chmod +x ./kubernetes/run.sh
-/bin/bash ./kubernetes/run.sh
+# /bin/bash ./kubernetes/run.sh
