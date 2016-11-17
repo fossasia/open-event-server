0c87e0d365f0c5550684033b619cd7aa62fb60aa
Niranjan Rajendran <niranjan94@yahoo.com>
2016-11-17 18:46:12 +0530

    Copy setup.sh

--- kubernetes/deployment/deploy.sh
+++ kubernetes/deployment/deploy.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/open-
 fi
 
 cd kubernetes/image
-docker build -t gcr.io/eventyay/web:$TRAVIS_COMMIT .
+docker build --no-cache -t gcr.io/eventyay/web:$TRAVIS_COMMIT .
 docker tag gcr.io/eventyay/web:$TRAVIS_COMMIT gcr.io/eventyay/web:latest
 gcloud docker -- push gcr.io/eventyay/web
 kubectl patch deployment web -p \ '{"spec":{"template":{"spec":{"containers":[{"name":"web","image":"gcr.io/eventyay/web:'"$TRAVIS_COMMIT"'"}]}}}}'
