670bd4dcc8697b5075e5d7bfa3c4d5b3d3ebb47e
Niranjan Rajendran <niranjan94@yahoo.com>
2016-12-16 23:05:32 +0530

    Fix errors in search/explore functionality

---
+++ app/static/js/admin/guest/explore/vue-app.js
@@ -0,0 +1,29 @@
+
+//noinspection JSUnusedGlobalSymbols
+var app = new Vue({
+    el: '#explore',
+    data: {
+
+    },
+    computed: {
+        _: function () {
+            return _;
+        }
+    },
+    watch: {
+
+
+    },
+    methods: {
+
+
+    },
+    compiled: function () {
+
+    }
+});
+
+app.$nextTick(function () {
+    var $div = $(this.$el);
+
+});
