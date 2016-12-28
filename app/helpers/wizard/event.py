a5d8a8f0fcbab54dc06540bfd93dc07b9b123ce9
Medo <medomag20@gmail.com>
2016-12-28 09:29:08 +0530

    external event url problem fixed (#2743)
    * external event url problem fixed
    
    * external event url problem fixed

--- app/helpers/wizard/event.py
+++ app/helpers/wizard/event.py
@@ -124,6 +124,7 @@ def save_event_from_json(json, event_id=None):
         abort(400)
 
     event.name = event_data['name']
+    event.event_url = event_data['event_url']
     event.location_name = event_data['location_name']
     event.show_map = 1 if event_data['show_map'] else 0
     event.start_time = start_time
