bcce57c041492c92fb0c8f22d31d706732ba6995
Niranjan Rajendran <niranjan94@yahoo.com>
2017-01-06 21:21:52 +0530

    Correct down revision

--- migrations/versions/e8222a407dd5_.py
+++ migrations/versions/e8222a407dd5_.py
@@ -1,14 +1,14 @@
 """empty message
 
 Revision ID: e8222a407dd5
-Revises: 2281c3f3b984
+Revises: 6581466d21c9
 Create Date: 2017-01-04 20:59:32.624908
 
 """
 
 # revision identifiers, used by Alembic.
 revision = 'e8222a407dd5'
-down_revision = '2281c3f3b984'
+down_revision = '6581466d21c9'
 
 from alembic import op
 import sqlalchemy as sa
