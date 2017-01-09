c9b8464344da812fbcf56280686aaaa5f791b2ce
arpitn30 <arpit.nandwani@gmail.com>
2017-01-10 00:08:33 +0530

    app/views/public/pages.py updated

--- app/views/public/pages.py
+++ app/views/public/pages.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask import render_template
+from flask import abort
 
 from app.helpers.data_getter import DataGetter
 
@@ -10,6 +11,8 @@ pages = Blueprint('basicpagesview', __name__)
 def url_view(url):
     from app import get_locale
     page = DataGetter.get_page_by_url('/' + url, get_locale())
+    if page == None:
+		return abort(404)
     return render_template('gentelella/guest/page.html', page=page)
 
 
