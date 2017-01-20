55a9d3cc3feb3087f828315cf34a1498185dd603
Niranjan Rajendran <niranjan94@yahoo.com>
2017-01-20 17:16:04 +0530

    Correct travis build fail

--- app/__init__.py
+++ app/__init__.py
@@ -415,9 +415,10 @@ def as_timezone(dt, tzname):
     """Accepts a Time aware Datetime object and a Timezone name.
         Returns Converted Timezone aware Datetime Object.
         """
-    if timezone(tzname):
+    if tzname and timezone(tzname):
         return dt.astimezone(timezone(tzname))
     return dt
+
 
 @app.template_filter('fees_by_currency')
 def fees_by_currency(currency):
