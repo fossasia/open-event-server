cc7b7bea70be75b9afb3af4159187819b931b838
Saptak Sengupta <saptak013@gmail.com>
2017-01-30 16:11:56 +0530

    Speeakers shown in public page even if schedule not published (#3079)

--- app/views/public/event_detail.py
+++ app/views/public/event_detail.py
@@ -65,7 +65,7 @@ def display_event_detail_home(identifier):
     speakers = []
     for session in accepted_sessions:
         for speaker in session.speakers:
-            if speaker not in speakers and session.microlocation_id:
+            if speaker not in speakers:
                 speakers.append(speaker)
 
     '''Timezone aware current datetime object according to event timezone'''
