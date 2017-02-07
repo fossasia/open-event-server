8f28470a9439137b69825aa7f1c7fc0b0193a716
dristybutola <drishtibutola@gmail.com>
2017-02-07 19:35:43 +0530

    Update microlocation.py

--- app/models/microlocation.py
+++ app/models/microlocation.py
@@ -5,46 +5,46 @@ class Microlocation(db.Model):
     """Microlocation model class"""
     __tablename__ = 'microlocation'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    room_name = db.Column(db.String, nullable=False)
+    floor = db.Column(db.Integer)
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
-    floor = db.Column(db.Integer)
-    room = db.Column(db.String)
+    
+    
     session = db.relationship('Session', backref="microlocation")
     event_id = db.Column(db.Integer, db.ForeignKey('events.id', ondelete='CASCADE'))
 
     def __init__(self,
-                 name=None,
+                 room_name=None,
+                 floor=None,
                  latitude=None,
                  longitude=None,
-                 floor=None,
-                 event_id=None,
-                 room=None):
-        self.name = name
+                 event_id=None):
+        self.room_name = room_name
         self.latitude = latitude
         self.longitude = longitude
         self.floor = floor
         self.event_id = event_id
-        self.room = room
+        
 
     @staticmethod
     def get_service_name():
         return 'microlocation'
 
     def __repr__(self):
-        return '<Microlocation %r>' % self.name
+        return '<Microlocation %r>' % self.room_name
 
     def __str__(self):
         return unicode(self).encode('utf-8')
 
     def __unicode__(self):
-        return self.name
+        return self.room_name
 
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {
             'id': self.id,
-            'name': self.name,
+            'room_name': self.room_name,
             'floor': self.floor
         }
