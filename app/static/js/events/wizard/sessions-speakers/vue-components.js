805feb1964bba02f4144b05f55d31a992428b573
mejariamol <amolp.mejari.eee14@itbhu.ac.in>
2017-05-07 02:07:58 +0530

    Implemented auto-choose font color property to the track, accordingly font color is used in scheduler, added session label preview in wizard step-3, added font color field to the track api endpoint

--- app/static/js/events/wizard/sessions-speakers/vue-components.js
+++ app/static/js/events/wizard/sessions-speakers/vue-components.js
@@ -1,6 +1,15 @@
 Vue.component('session-track', {
     props: ['track'],
     template: '#track-template',
+    watch: {
+        'track.color': function(value) {
+            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(value);
+            if(result) {
+                var a = 1 - (0.299 * parseInt(result[1], 16) + 0.587 * parseInt(result[2], 16) + 0.114 * parseInt(result[3], 16))/255;
+                this.track.font_color = (a < 0.5) ? '#000000' : '#ffffff';
+            }
+        }
+    },
     methods: {
         remove: function () {
             this.$emit('remove');
