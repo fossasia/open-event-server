f1631b122b4677df1edcabfe7bbc6c5bfce1bded
Niranjan Rajendran <me@niranjan.io>
2017-05-21 12:58:51 +0530

    Update kubernetes configuration and add nextgen deployments

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Getting credentials for staging cluster"
+gcloud container clusters get-credentials staging-cluster
 echo "Deploying the project to kubernetes cluster"
 export DIR=${BASH_SOURCE%/*}
 # Start KubeLego deployment
@@ -11,20 +13,6 @@ kubectl create -f ${DIR}/yamls/postgres
 kubectl create -f ${DIR}/yamls/redirector
 # Start Redis deployment & service
 kubectl create -f ${DIR}/yamls/redis
-# Starting nfs deployment for persistent storage. Tricky. So, we'll go file by file.
-# create the persistent volume using a GCE persistent disk
-kubectl create -f ${DIR}/yamls/persistent-store/nfs-server.yml
-# Let us give the NFS server ample amount of time to start up
-echo "Waiting for NFS server to start up"
-sleep 60
-# create the NFS deployment service to expose ports
-kubectl create -f ${DIR}/yamls/persistent-store/nfs-server-service.yml
-# create the persistent volume entry for the NFS Server with a ReadWriteMany access mode
-kubectl create -f ${DIR}/yamls/persistent-store/nfs-pv.yml
-# create the required claim for the NFS Server persistent volume
-kubectl create -f ${DIR}/yamls/persistent-store/nfs-pv-claim.yml
-echo "Waiting longer for NFS server to start up"
-# I found the sometimes NFS required a little bit longer to start up after all this. So, let's just give it some more time.
 sleep 30
 # Start celery deployment
 kubectl create -f ${DIR}/yamls/celery
--- kubernetes/run.sh
+++ kubernetes/run.sh
@@ -4,7 +4,7 @@ export REDIS_URL=redis://${REDIS_SERVICE_HOST}:${REDIS_SERVICE_PORT}/0
 export DATABASE_URL=postgresql://postgres:test@${POSTGRES_SERVICE_HOST}:${POSTGRES_SERVICE_PORT}/opev
 python manage.py initialize_db -c open_event_test_user@fossasia.org:fossasia
 python manage.py db upgrade > /dev/null 2>&1
-if [ "$DEPLOYMENT" == "web" ]
+if [ "$DEPLOYMENT" == "api" ]
 then
     gunicorn -b 0.0.0.0:8080 app:app -w 1 --enable-stdio-inheritance --log-level "warning" --proxy-protocol
 fi
