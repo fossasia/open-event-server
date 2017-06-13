6d9ae30f6d9b609d0d373bdcc35ab5f4d1a0060e
Niranjan Rajendran <me@niranjan.io>
2017-06-13 19:47:48 +0530

    Merge migration heads

---
+++ migrations/versions/af9215895b80_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: af9215895b80
+Revises: ('217d3c1f3c5f', '6904ae1e0e33')
+Create Date: 2017-06-13 19:47:34.126518
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'af9215895b80'
+down_revision = ('217d3c1f3c5f', '6904ae1e0e33')
+
+from alembic import op
+import sqlalchemy as sa
+import sqlalchemy_utils
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
