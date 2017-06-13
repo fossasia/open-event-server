02ea15454ca345505c289480e6af9e136fcfd184
Niranjan Rajendran <me@niranjan.io>
2017-06-13 18:51:36 +0530

    Re-organize API Blueprint and render on each merge

--- scripts/push_api_docs.sh
+++ scripts/push_api_docs.sh
@@ -1,18 +1,20 @@
 # pushes API docs to gh-pages on successful build
 #
-# thanks to https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
 #!/bin/sh
 set -e
 
 git config --global user.name "Travis CI"
 git config --global user.email "noreply+travis@fossasia.org"
 
-export DEPLOY_BRANCH = ${DEPLOY_BRANCH:-master}
+# export DEPLOY_BRANCH = ${DEPLOY_BRANCH:-master}
+export DEPLOY_BRANCH = 'nextgen' # TODO change before merging to dev
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/open-event-orga-server" -o  "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
     echo "We update docs only from master. So, let's skip this shall we ? :)"
     exit 0
 fi
+
+npm install -g aglio
 
 # set ssh
 openssl aes-256-cbc -K $encrypted_512f7587e087_key -iv $encrypted_512f7587e087_iv -in ./scripts/opev_orga.enc -out deploy_key -d
@@ -22,16 +24,14 @@ ssh-add deploy_key
 
 # clone and do
 git clone -b gh-pages "git@github.com:fossasia/open-event-orga-server.git" gh-pages
-cp static/temp/swagger.json gh-pages/api/v1/swagger.json
-cp report.html gh-pages/robot/report.html
-cp log.html gh-pages/robot/log.html
-cp output.xml gh-pages/robot/output.xml
+rm -rf gh-pages/api/v1/*
+aglio --theme-variables slate --theme-template triple -i docs/api/api_blueprint.apib -o gh-pages/api/v1/index.html
 cp -R docs/general/* gh-pages/_docs/
 cp -R docs/installation/* gh-pages/_installation/
 cp -R docs/kubernetes/* gh-pages/_kubernetes/
 cd gh-pages
 git add .
-git commit -m '[Auto] Updated API docs and robot test results'
+git commit -m '[Auto] Updated API Docs'
 git push origin gh-pages
 
 exit 0
