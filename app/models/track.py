6d08e6905619dbdd9256b38eda460fa9027e8825
magdalenesuo <medomag20@gmail.com>
2017-06-16 23:04:44 +0530

    Enhanced and corrected Tracks API

--- app/models/track.py
+++ app/models/track.py
@@ -7,22 +7,17 @@ class Track(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     description = db.Column(db.Text)
-    track_image_url = db.Column(db.Text)
     color = db.Column(db.String, nullable=False)
-    location = db.Column(db.String)
     sessions = db.relationship('Session', backref='track')
     event_id = db.Column(db.Integer, db.ForeignKey('events.id', ondelete='CASCADE'))
 
     def __init__(self, name=None, description=None, event_id=None,
-                 session=None, track_image_url=None, color=None,
-                 location=None):
+                 session=None, color=None):
         self.name = name
         self.description = description
         self.event_id = event_id
         self.session_id = session
-        self.track_image_url = track_image_url
         self.color = color
-        self.location = location
 
     @staticmethod
     def get_service_name():
