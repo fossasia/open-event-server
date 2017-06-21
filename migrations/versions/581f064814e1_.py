9224bdba548bcb08b8fb3b00e34a6b745dd77309
magdalenesuo <medomag20@gmail.com>
2017-06-22 01:52:52 +0530

    Implemented event-type api, relationship with events

---
+++ migrations/versions/581f064814e1_.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: 581f064814e1
+Revises: ('6bd24e8f99f0', 'da5c6c740257')
+Create Date: 2017-06-21 22:21:44.690181
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '581f064814e1'
+down_revision = ('6bd24e8f99f0', 'da5c6c740257')
+
+from alembic import op
+import sqlalchemy as sa
+import sqlalchemy_utils
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
