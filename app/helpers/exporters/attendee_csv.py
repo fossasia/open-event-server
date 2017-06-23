e5d9612b02b518042506bcfff6bfd1e33d52ac8e
saptaks <saptak013@gmail.com>
2017-06-24 01:47:21 +0530

    Unicode encoding fixed

--- app/helpers/exporters/attendee_csv.py
+++ app/helpers/exporters/attendee_csv.py
@@ -1,19 +1,20 @@
 from app.helpers.ticketing import TicketingManager
 
+
 class AttendeeCsv:
 
     @staticmethod
     def export(event_id):
         (_, event_id, holders, _, _) = TicketingManager.get_attendee_export_info(event_id=event_id)
         headers = 'Order#,Order Date, Status, First Name, Last Name, Email, Country,' \
-                'Payment Type, Ticket Name, Ticket Price, Ticket Type'
+            'Payment Type, Ticket Name, Ticket Price, Ticket Type'
 
         fields = ('order_invoice', 'created_at', 'status', 'firstname', 'lastname', 'email',
                   'country', 'paid_via', 'ticket_name', 'ticket_price', 'ticket_type')
         rows = [headers]
         for holder in holders:
             if holder['status'] != "deleted":
-                columns = (str(holder.get(f, '')) for f in fields)
+                columns = (str(holder.get(f, '').encode('utf-8')) for f in fields)
                 rows.append(','.join(columns))
 
         csv_content = '\n'.join(rows)
