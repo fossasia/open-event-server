6b05db86a70d428b8a15bbeeb1d6d85f0883aa1f
saptaks <saptak013@gmail.com>
2017-06-25 02:10:53 +0530

    Added file uploading for multiple files

--- app/api/helpers/files.py
+++ app/api/helpers/files.py
@@ -20,11 +20,25 @@ def uploaded_image(extension='.png', file_content=None):
     return UploadedFile(file_path, filename)
 
 
-def uploaded_file(files):
-    filename = files.filename
-    filedir = current_app.config.get('BASE_DIR') + '/static/uploads/'
-    if not os.path.isdir(filedir):
-        os.makedirs(filedir)
-    file_path = filedir + filename
-    files.save(file_path)
-    return UploadedFile(file_path, filename)
+def uploaded_file(files, multiple=False):
+    if multiple:
+        files_uploaded = []
+        for file in files:
+            filename = file.filename
+            filedir = current_app.config.get('BASE_DIR') + '/static/uploads/'
+            if not os.path.isdir(filedir):
+                os.makedirs(filedir)
+            file_path = filedir + filename
+            file.save(file_path)
+            files_uploaded.append(UploadedFile(file_path, filename))
+
+    else:
+        filename = files.filename
+        filedir = current_app.config.get('BASE_DIR') + '/static/uploads/'
+        if not os.path.isdir(filedir):
+            os.makedirs(filedir)
+        file_path = filedir + filename
+        files.save(file_path)
+        files_uploaded = UploadedFile(file_path, filename)
+
+    return files_uploaded
