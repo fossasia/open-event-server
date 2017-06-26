64de2b978e3466edeef2e3c59c0b4224b64592c3
shubham-padia <shubhamapadia@gmail.com>
2017-06-27 01:46:12 +0530

    add events api for dredd testing

--- app/factories/common.py
+++ app/factories/common.py
@@ -5,7 +5,11 @@ email_ = 'example@example.com'
 url_ = 'http://example.com'
 imageUrl_ = 'http://example.com/example.png'
 date_ = '2016-12-13T23:59:59.123456+00:00'
-
+dateEnd_ = '2016-12-14T23:59:59.123456+00:00'
+country_ = 'US'
+currency_ = 'USD'
+float_ = '1.23456789'
+timezone_ = 'UTC'
 
 def socialUrl_(name):
     return 'https://{}.com/{}'.format(name, name)
---
+++ app/factories/event.py
@@ -0,0 +1,47 @@
+import factory
+from app.models.event import db, Event
+import app.factories.common as common
+
+
+class EventFactoryBasic(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Event
+        sqlalchemy_session = db.session
+
+    name = common.string_
+    event_url = common.url_
+    starts_at = common.date_
+    ends_at = common.dateEnd_
+    timezone = common.timezone_
+    latitude = common.float_
+    longitude = common.float_
+    logo_url = common.imageUrl_
+    location_name = common.string_
+    searchable_location_name = common.string_
+    description = common.string_
+    original_image_url = common.imageUrl_
+    organizer_name = common.string_
+    is_map_shown = True
+    organizer_description = common.string_
+    is_sessions_speakers_enabled = True
+    privacy = "public"
+    state = common.string_
+    ticket_url = common.url_
+    code_of_conduct = common.string_
+    is_ticketing_enabled = True
+    payment_country = common.country_
+    payment_currency = common.currency_
+    paypal_email = common.email_
+    is_tax_enabled = True
+    can_pay_by_paypal = True
+    can_pay_by_stripe = True
+    can_pay_by_cheque = True
+    can_pay_by_bank = True
+    can_pay_onsite = True
+    cheque_details = common.string_
+    bank_details = common.string_
+    onsite_details = common.string_
+    is_sponsors_enabled = False
+    pentabarf_url = common.url_
+    ical_url = common.url_
+    xcal_url = common.url_
