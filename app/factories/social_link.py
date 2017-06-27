280e86c89da3272103bcd5be65a80afb36ba23c5
Afroz Ahamad <enigmaeth@gmail.com>
2017-06-27 17:20:27 +0530

    Add social link API for dredd testing (#3884)

---
+++ app/factories/social_link.py
@@ -0,0 +1,14 @@
+import factory
+from app.models.social_link import db, SocialLink
+from app.factories.event import EventFactoryBasic
+import app.factories.common as common
+
+
+class SocialLinkFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = SocialLink
+        sqlalchemy_session = db.session
+
+    event = factory.RelatedFactory(EventFactoryBasic)
+    name = common.string_
+    link = common.socialUrl_('facebook')
