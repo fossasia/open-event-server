3d98dc1b5a2f32cc99ae6d14984f4ecb91a22cb1
enigmaeth <enigmaeth@gmail.com>
2017-06-27 17:25:58 +0530

    Add image size API for dredd testing

---
+++ app/factories/image_size.py
@@ -0,0 +1,25 @@
+import factory
+from app.models.image_size import db, ImageSizes
+import app.factories.common as common
+
+
+class ImageSizeFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = ImageSizes
+        sqlalchemy_session = db.session
+
+    type = common.string_
+    full_width = 10
+    full_height = 10
+    full_aspect = True
+    full_quality = 10
+    icon_width = 10
+    icon_height = 10
+    icon_aspect = True
+    icon_quality = 10
+    thumbnail_width = 10
+    thumbnail_height = 10
+    thumbnail_aspect = True
+    thumbnail_quality = 10
+    logo_width = 10
+    logo_height = 10
