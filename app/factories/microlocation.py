506859e9178c43c514d9e6e394bd8c6f8f7f08d8
Shubham Padia <shubham-padia@users.noreply.github.com>
2017-07-02 12:49:56 +0530

    add microlocations api to testing (#3939)

---
+++ app/factories/microlocation.py
@@ -0,0 +1,17 @@
+import factory
+from app.models.microlocation import db, Microlocation
+from app.factories.event import EventFactoryBasic
+import app.factories.common as common
+
+
+class MicrolocationFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Microlocation
+        sqlalchemy_session = db.session
+
+    event = factory.RelatedFactory(EventFactoryBasic)
+    name = common.string_
+    latitude = common.float_
+    longitude = common.float_
+    floor = common.int_
+    room = common.string_
