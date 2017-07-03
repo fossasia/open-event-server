fad11feaccf207bfc39ae379221ccb66bb6cdbc5
shubham-padia <shubhamapadia@gmail.com>
2017-07-03 21:39:56 +0530

    Raise 409 if user already exists

--- app/api/helpers/exceptions.py
+++ app/api/helpers/exceptions.py
@@ -4,3 +4,8 @@ from flask_rest_jsonapi.exceptions import JsonApiException
 class UnprocessableEntity(JsonApiException):
     title = "Unprocessable Entity"
     status = 422
+
+
+class ConflictException(JsonApiException):
+    title = "Conflict"
+    status = 409
