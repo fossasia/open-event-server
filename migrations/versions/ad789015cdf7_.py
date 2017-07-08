d12c3f7a056c49ddd9a6fc45e6f691a9f2ff70bb
enigmaeth <enigmaeth@gmail.com>
2017-07-09 00:01:24 +0530

    migration heads: merge bc1c553a72c5 and 9e1204b2e096

---
+++ migrations/versions/ad789015cdf7_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ad789015cdf7
+Revises: ('bc1c553a72c5', '9e1204b2e096')
+Create Date: 2017-07-09 00:00:31.031203
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+import sqlalchemy_utils
+
+
+# revision identifiers, used by Alembic.
+revision = 'ad789015cdf7'
+down_revision = ('bc1c553a72c5', '9e1204b2e096')
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
