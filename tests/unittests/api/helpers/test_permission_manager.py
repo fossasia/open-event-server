d3b13cf1783e70666483dd2126945667f297887e
Piyush Agrawal <poush12@gmail.com>
2017-07-18 17:02:59 +0530

    Add tests for different permissions methods

--- tests/unittests/api/helpers/test_permission_manager.py
+++ tests/unittests/api/helpers/test_permission_manager.py
@@ -1,9 +1,13 @@
 import unittest
 
+from flask import Response
 from flask_jwt import _default_jwt_encode_handler
 
 from app import current_app as app
-from app.api.helpers.permission_manager import has_access
+from app.api.helpers.db import get_or_create, save_to_db
+from app.api.helpers.permission_manager import has_access, accessible_role_based_events, permission_manager
+from app.factories.event import EventFactoryBasic
+from app.models.users_events_role import UsersEventsRoles
 from tests.unittests.utils import OpenEventTestCase
 from app.factories.user import UserFactory
 from app.models import db
@@ -18,6 +22,11 @@ class TestPermissionManager(OpenEventTestCase):
             db.session.add(user)
             db.session.commit()
 
+            event = EventFactoryBasic()
+            event.user_id = user.id
+            db.session.add(event)
+            db.session.commit()
+
             # Authenticate User
             self.auth = {'Authorization': "JWT " + _default_jwt_encode_handler(user)}
 
@@ -25,7 +34,57 @@ class TestPermissionManager(OpenEventTestCase):
         with app.test_request_context(headers=self.auth):
             self.assertTrue(has_access('is_admin'))
             self.assertFalse(has_access('is_super_admin'))
+            self.assertTrue(has_access('is_organizer', event_id=1))
 
+    def test_accessible_role_based_events(self):
+        with app.test_request_context(headers=self.auth, method="POST"):
+            response = accessible_role_based_events(lambda *a, **b: b.get('user_id'), (), {}, (), {})
+            assert response is not None
+
+    def test_is_organizer(self):
+        with app.test_request_context(headers=self.auth, method="POST"):
+            uer, is_created = get_or_create(UsersEventsRoles, user_id=1, event_id=1)
+            uer.role_id = 1
+            save_to_db(uer)
+            self.assertTrue(has_access('is_organizer', event_id=1))
+
+    def test_is_coorganizer(self):
+        with app.test_request_context(headers=self.auth, method="POST"):
+            uer, is_created = get_or_create(UsersEventsRoles, user_id=1, event_id=1)
+            uer.role_id = 2
+            save_to_db(uer)
+            self.assertTrue(has_access('is_coorganizer', event_id=1))
+
+    def test_is_moderator(self):
+        with app.test_request_context(headers=self.auth, method="POST"):
+            uer, is_created = get_or_create(UsersEventsRoles, user_id=1, event_id=1)
+            uer.role_id = 4
+            save_to_db(uer)
+            self.assertTrue(has_access('is_moderator', event_id=1))
+
+    def test_is_track_organizer(self):
+        with app.test_request_context(headers=self.auth, method="POST"):
+            uer, is_created = get_or_create(UsersEventsRoles, user_id=1, event_id=1)
+            uer.role_id = 4
+            save_to_db(uer)
+            self.assertTrue(has_access('is_moderator', event_id=1))
+
+    def test_is_registrar(self):
+        with app.test_request_context(headers=self.auth, method="POST"):
+            uer, is_created = get_or_create(UsersEventsRoles, user_id=1, event_id=1)
+            uer.role_id = 6
+            save_to_db(uer)
+            self.assertTrue(has_access('is_registrar', event_id=1))
+
+    def test_permission_manager_attributes(self):
+        with app.test_request_context():
+            kwargs = {'leave_if': lambda a: True}
+            perm = permission_manager(lambda *a, **b : True, [], {}, 'is_admin', **kwargs)
+            self.assertTrue(perm)
+
+            kwargs = {'check': lambda a: False}
+            perm = permission_manager(lambda *a, **b : False, [], {}, 'is_admin', **kwargs)
+            self.assertIsInstance(perm, Response)
 
 if __name__ == '__main__':
     unittest.main()
