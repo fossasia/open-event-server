f2fd3430cb63521c56a63a92cab5df227c502f3a
Shubham Padia <shubham-padia@users.noreply.github.com>
2017-07-20 19:30:03 +0530

    Add seperate resourcelist class, docs, permissions for direct endpoint of microlocations (#4105)

--- app/api/helpers/exceptions.py
+++ app/api/helpers/exceptions.py
@@ -9,3 +9,11 @@ class UnprocessableEntity(JsonApiException):
 class ConflictException(JsonApiException):
     title = "Conflict"
     status = 409
+
+
+class ForbiddenException(JsonApiException):
+    """
+    Default class for 403 Error
+    """
+    title = 'Access Forbidden'
+    status = 403
