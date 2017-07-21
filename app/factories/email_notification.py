41f3aad89e3816f402ded604b8271022e71f1d91
magdalenesuo <medomag20@gmail.com>
2017-07-21 20:53:53 +0530

    Fix Access Denied error on accessing notifications/<id> and email-notifications/<id>

--- app/factories/email_notification.py
+++ app/factories/email_notification.py
@@ -1,6 +1,7 @@
 import factory
 from app.models.email_notification import db, EmailNotification
 from app.factories.user import UserFactory
+from app.factories.event import EventFactoryBasic
 
 
 class EmailNotificationFactory(factory.alchemy.SQLAlchemyModelFactory):
@@ -9,8 +10,11 @@ class EmailNotificationFactory(factory.alchemy.SQLAlchemyModelFactory):
         sqlalchemy_session = db.session
 
     user = factory.RelatedFactory(UserFactory)
+    event = factory.RelatedFactory(EventFactoryBasic)
     next_event = 0
     new_paper = 0
     session_accept_reject = 0
     session_schedule = 1
     after_ticket_purchase = 1
+    event_id = 1
+    user_id = 1
