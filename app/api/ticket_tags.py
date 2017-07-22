72629e44a21ad8ecbd02464f9f42192532d60831
enigmaeth <enigmaeth@gmail.com>
2017-07-23 00:16:32 +0530

    TicketTag: Add seperate resourcelist class, docs, permissions for direct endpoints

--- app/api/ticket_tags.py
+++ app/api/ticket_tags.py
@@ -1,6 +1,8 @@
 from flask_rest_jsonapi import ResourceDetail, ResourceList, ResourceRelationship
 from marshmallow_jsonapi.flask import Schema, Relationship
 from marshmallow_jsonapi import fields
+from sqlalchemy.orm.exc import NoResultFound
+from flask_rest_jsonapi.exceptions import ObjectNotFound
 
 from app.api.helpers.utilities import dasherize
 from app.api.helpers.permissions import jwt_required
@@ -9,6 +11,8 @@ from app.models.ticket import Ticket, TicketTag, ticket_tags_table
 from app.models.event import Event
 from app.api.helpers.db import safe_query
 from app.api.helpers.utilities import require_relationship
+from app.api.helpers.exceptions import ForbiddenException
+from app.api.helpers.permission_manager import has_access
 
 
 class TicketTagSchema(Schema):
@@ -44,14 +48,61 @@ class TicketTagSchema(Schema):
                          type_='event')
 
 
-class TicketTagList(ResourceList):
+class TicketTagListPost(ResourceList):
     """
     List and create TicketTag
     """
     def before_post(self, args, kwargs, data):
+        """
+        before post method for checking required relationship
+        :param args:
+        :param kwargs:
+        :param data:
+        :return:
+        """
         require_relationship(['event'], data)
 
+        if not has_access('is_coorganizer', event_id=data['event']):
+            raise ForbiddenException({'source': ''}, 'Co-organizer access is required.')
+
+    def after_create_object(self, obj, data, view_kwargs):
+        """
+        method to add ticket tags and ticket in association table
+        :param obj:
+        :param data:
+        :param view_kwargs:
+        :return:
+        """
+        if 'tickets' in data:
+            ticket_ids = data['tickets']
+            for ticket_id in ticket_ids:
+                try:
+                    ticket = Ticket.query.filter_by(id=ticket_id).one()
+                except NoResultFound:
+                    raise ObjectNotFound({'parameter': 'ticket_id'},
+                                         "Ticket: {} not found".format(ticket_id))
+                else:
+                    ticket.tags.append(obj)
+                    self.session.commit()
+
+    schema = TicketTagSchema
+    data_layer = {'session': db.session,
+                  'model': TicketTag,
+                  'methods': {
+                    'after_create_object': after_create_object
+                  }}
+
+
+class TicketTagList(ResourceList):
+    """
+    List TicketTags based on event_id or ticket_id
+    """
     def query(self, view_kwargs):
+        """
+        method to query Ticket tags based on different params
+        :param view_kwargs:
+        :return:
+        """
         query_ = self.session.query(TicketTag)
         if view_kwargs.get('ticket_id'):
             ticket = safe_query(self, Ticket, 'id', view_kwargs['ticket_id'], 'ticket_id')
@@ -64,32 +115,13 @@ class TicketTagList(ResourceList):
             query_ = query_.join(Event).filter(Event.id == event.id)
         return query_
 
-    def before_create_object(self, data, view_kwargs):
-        if view_kwargs.get('ticket_id'):
-            ticket = safe_query(self, Ticket, 'id', view_kwargs['ticket_id'], 'ticket_id')
-            data['event_id'] = ticket.event_id
-        if view_kwargs.get('event_id'):
-            event = safe_query(self, Event, 'id', view_kwargs['event_id'], 'event_id')
-            data['event_id'] = event.id
-        elif view_kwargs.get('event_identifier'):
-            event = safe_query(self, Event, 'identifier', view_kwargs['event_identifier'], 'event_identifier')
-            data['event_id'] = event.id
-
-    def after_create_object(self, obj, data, view_kwargs):
-        if view_kwargs.get('ticket_id'):
-            ticket = safe_query(self, Ticket, 'id', view_kwargs['ticket_id'], 'ticket_id')
-            ticket.tags.append(obj)
-            self.session.commit()
-
     view_kwargs = True
-    decorators = (jwt_required,)
     schema = TicketTagSchema
+    methods = ['GET', ]
     data_layer = {'session': db.session,
                   'model': TicketTag,
                   'methods': {
-                      'query': query,
-                      'before_create_object': before_create_object,
-                      'after_create_object': after_create_object
+                      'query': query
                   }}
 
 
