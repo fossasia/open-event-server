87a58400fb5465fd89eac04641d3ec00d459ed6f
shubham-padia <shubhamapadia@gmail.com>
2017-07-25 00:43:47 +0530

    Health-check: check whether db is up to date with migrations

--- app/views/healthcheck.py
+++ app/views/healthcheck.py
@@ -32,3 +32,29 @@ def health_check_db():
         return True, 'database ok'
     except:
         return False, 'Error connecting to database'
+
+
+def health_check_migrations():
+    """
+    Checks whether database is up to date with migrations by performing a select query on each model
+    :return:
+    """
+    # Get all the models in the db, all models should have a explicit __tablename__
+    classes, models, table_names = [], [], []
+    # noinspection PyProtectedMember
+    for class_ in db.Model._decl_class_registry.values():
+        try:
+            table_names.append(class_.__tablename__)
+            classes.append(class_)
+        except:
+            pass
+    for table in db.metadata.tables.items():
+        if table[0] in table_names:
+            models.append(classes[table_names.index(table[0])])
+
+    for model in models:
+        try:
+            db.session.query(model).first()
+        except:
+            return False, '{} model out of date with migrations'.format(model)
+    return True, 'database up to date with migrations'
