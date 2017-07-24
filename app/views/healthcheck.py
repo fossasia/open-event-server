e6ce6b9868d0fff73421f6b8fe4c6e0f727ec45f
shubham-padia <shubhamapadia@gmail.com>
2017-07-24 23:08:51 +0530

    remove /environment endpoint

--- app/views/healthcheck.py
+++ app/views/healthcheck.py
@@ -1,8 +1,6 @@
 from app.models import db
 from celery.task.control import inspect
 from errno import errorcode
-import json
-from flask import current_app
 
 
 def health_check_celery():
@@ -34,33 +32,3 @@ def health_check_db():
         return True, 'database ok'
     except:
         return False, 'Error connecting to database'
-
-
-def safe_dump(skip_words, dictionary):
-    """
-    Modified version of safe_dump function from healthcheck library to accept custom substrings to skip in output
-    :param skip_words:
-    :param dictionary:
-    :return:
-    """
-    result = {}
-    for key in dictionary.keys():
-        if any(x in key.lower() for x in skip_words):
-            # Try to avoid listing passwords and access tokens or keys in the output
-            result[key] = "********"
-        else:
-            try:
-                json.dumps(dictionary[key])
-                result[key] = dictionary[key]
-            except TypeError:
-                pass
-    return result
-
-
-def get_safe_config():
-    """
-    Get safe config
-    :return:
-    """
-    skip_words = ['key', 'pass', 'dsn', 'database_uri', 'token']
-    return safe_dump(skip_words, current_app.config)
