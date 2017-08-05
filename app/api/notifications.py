8d94c9936f885d66a29ecd28d4a916f24f6b8f2d
shubham-padia <shubhamapadia@gmail.com>
2017-08-05 23:54:55 +0530

    Bug fix: Notifications API is sending same responses for different users.

--- app/api/notifications.py
+++ app/api/notifications.py
@@ -63,8 +63,8 @@ class NotificationList(ResourceList):
         :return:
         """
         query_ = self.session.query(Notification)
-        if view_kwargs.get('id'):
-            user = safe_query(self, User, 'id', view_kwargs['id'], 'id')
+        if view_kwargs.get('user_id'):
+            user = safe_query(self, User, 'id', view_kwargs['user_id'], 'user_id')
             query_ = query_.join(User).filter(User.id == user.id)
         return query_
 
