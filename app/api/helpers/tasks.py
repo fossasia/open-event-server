687c620305719c0fb1c3060c1d764c01144059f0
magdalenesuo <medomag20@gmail.com>
2017-08-15 15:05:20 +0530

    Save notifications for completion of export event

--- app/api/helpers/tasks.py
+++ app/api/helpers/tasks.py
@@ -15,7 +15,7 @@ from flask import url_for
 
 from app.api.helpers.request_context_task import RequestContextTask
 from app.api.helpers.mail import send_export_mail, send_import_mail
-from app.api.helpers.notification import send_notif_after_import
+from app.api.helpers.notification import send_notif_after_import, send_notif_after_export
 from app.api.helpers.db import safe_query
 from import_helpers import update_import_job
 from app.models.event import Event
@@ -61,6 +61,7 @@ def send_mail_via_smtp_task(config, payload):
 @celery.task(base=RequestContextTask, name='export.event', bind=True)
 def export_event_task(self, email, event_id, settings):
     event = safe_query(db, Event, 'id', event_id, 'event_id')
+    user = db.session.query(User).filter_by(email=email).first()
     try:
         logging.info('Exporting started')
         path = event_export_task_base(event_id, settings)
@@ -72,11 +73,13 @@ def export_event_task(self, email, event_id, settings):
         }
         logging.info('Exporting done.. sending email')
         send_export_mail(email=email, event_name=event.name, download_url=download_url)
+        send_notif_after_export(user=user, event_name=event.name, download_url=download_url)
     except Exception as e:
         print(traceback.format_exc())
         result = {'__error': True, 'result': str(e)}
         logging.info('Error in exporting.. sending email')
         send_export_mail(email=email, event_name=event.name, error_text=str(e))
+        send_notif_after_export(user=user, event_name=event.name, error_text=str(e))
 
     return result
 
