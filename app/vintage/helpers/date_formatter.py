9a31fead6a5815d3f06df48b1808be200d05f29b
shubham-padia <shubhamapadia@gmail.com>
2017-08-25 00:47:41 +0530

    Delete vintage directory

--- app/vintage/helpers/date_formatter.py
+++
@@ -1,6 +0,0 @@
-class DateFormatter(object):
-    """Date format class"""
-
-    def format_date(self, date):
-        """Return date with proper format"""
-        return str(date.strftime("%Y-%m-%dT%H:%M:%S%Z"))
