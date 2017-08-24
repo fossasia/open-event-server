9f49807c038006d4a23a45e5795b5d757665fb99
shubham-padia <shubhamapadia@gmail.com>
2017-08-25 00:47:41 +0530

    Delete vintage directory

--- app/vintage/helpers/cache.py
+++
@@ -1,3 +0,0 @@
-from flask_cache import Cache
-
-cache = Cache()
--- app/vintage/helpers/formatter.py
+++
@@ -1,9 +0,0 @@
-def operation_name(char):
-    if char == 'c':
-        return 'Create'
-    elif char == 'r':
-        return 'Read'
-    elif char == 'u':
-        return 'Update'
-    elif char == 'd':
-        return 'Delete'
