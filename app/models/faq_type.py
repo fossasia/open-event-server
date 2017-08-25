4ae6703f2ca7b632ef9dd2be77e176ffdfa86e42
shubham-padia <shubhamapadia@gmail.com>
2017-08-25 14:19:46 +0530

    Add faq types db model and migrations

---
+++ app/models/faq_type.py
@@ -0,0 +1,31 @@
+from app.models import db
+
+
+class FaqType(db.Model):
+    """Page model class"""
+    __tablename__ = 'faq_types'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    event_id = db.Column(db.Integer,
+                         db.ForeignKey('events.id', ondelete='CASCADE'))
+
+    def __init__(self, name=None, event_id=None):
+        self.name = name
+        self.event_id = event_id
+
+    def __repr__(self):
+        return '<FAQ %r>' % self.name
+
+    def __str__(self):
+        return unicode(self).encode('utf-8')
+
+    def __unicode__(self):
+        return self.name
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
