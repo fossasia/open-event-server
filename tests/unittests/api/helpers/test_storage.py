b9a5096a0647db05802733a298881c4a2d17220c
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2018-05-17 01:08:24 +0530

    Add tests for upload_local function

---
+++ tests/unittests/api/helpers/test_storage.py
@@ -0,0 +1,51 @@
+"""Test file for storage functions."""
+import os
+from urllib.parse import urljoin
+
+from app import current_app as app
+from app.api.helpers.storage import generate_hash, UploadedFile, upload_local
+from tests.unittests.setup_database import Setup
+from tests.unittests.utils import OpenEventTestCase
+
+
+class TestStorageHelperValidation(OpenEventTestCase):
+    """Test class for testing storage helper functions."""
+
+    def setUp(self):
+        """Method to set up app for testing."""
+        self.app = Setup.create_app()
+
+    def test_upload_local(self):
+        """Method to test the upload_local function."""
+        test_name = 'image.png'
+        test_path = os.path.join(app.config['BASE_DIR'],
+                                 'static/media/temp',
+                                 test_name
+                                )
+        with open(test_path, 'wb') as img_file:
+            img_file.write(b'test content')
+
+        upfile = UploadedFile(test_path, 'image.png')
+        test_key = 'test_key'
+
+        with app.test_request_context():
+            test_hash = generate_hash(test_key)
+            upload_url = upload_local(upfile, key=test_key)
+
+        expected_url = urljoin('http://localhost:5000',
+                               os.path.join('static/media/',
+                                            test_key,
+                                            test_hash,
+                                            test_name)
+                              )
+
+        self.assertEqual(upload_url, expected_url)
+
+        os.unlink(test_path)  # Removes the file created for testing.
+        os.unlink(os.path.join(app.config['BASE_DIR'],
+                               'static/media',
+                               test_key,
+                               test_hash,
+                               test_name
+                              )
+                 )  # Removes the temporary 'uploaded' test file.
