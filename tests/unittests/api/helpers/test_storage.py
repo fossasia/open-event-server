220fda3e957625ff3a5730fdf918fa2a3a28bf24
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2018-05-18 01:15:06 +0530

    Simplify tests

--- tests/unittests/api/helpers/test_storage.py
+++ tests/unittests/api/helpers/test_storage.py
@@ -1,11 +1,6 @@
 """Test file for storage functions."""
-import os
-from urllib.parse import urljoin, urlparse
 
-from flask import request
-
-from app import current_app as app
-from app.api.helpers.storage import generate_hash, UploadedFile, upload_local
+from app.api.helpers.storage import create_url
 from tests.unittests.setup_database import Setup
 from tests.unittests.utils import OpenEventTestCase
 
@@ -17,39 +12,23 @@ class TestStorageHelperValidation(OpenEventTestCase):
         """Method to set up app for testing."""
         self.app = Setup.create_app()
 
-    def test_upload_local(self):
-        """Method to test the upload_local function."""
-        test_name = 'image.png'
-        test_path = os.path.join(app.config['BASE_DIR'],
-                                 'static/media/temp',
-                                 test_name
-                                 )
-        with open(test_path, 'wb') as img_file:
-            img_file.write(b'test content')
+    def test_create_url(self):
+        """Method to test the create_url function."""
 
-        upfile = UploadedFile(test_path, 'image.png')
-        test_key = 'test_key'
+        request_url = 'https://localhost:5000'
+        file_relative_path = '/some/path/image.png'
+        expected_file_url = 'https://localhost:5000/some/path/image.png'
 
-        with app.test_request_context():
-            test_hash = generate_hash(test_key)
-            test_url = request.url
-            upload_url = upload_local(upfile, key=test_key)
-
-        expected_url = urljoin(
-            '{url.scheme}://{url.netloc}'.format(url=urlparse(test_url)),
-            os.path.join('static/media/',
-            test_key,
-            test_hash,
-            test_name)
+        self.assertEqual(
+            create_url(request_url, file_relative_path), expected_file_url
             )
-
-        self.assertEqual(upload_url, expected_url)
-
-        os.unlink(test_path)  # Removes the file created for testing.
-        os.unlink(os.path.join(app.config['BASE_DIR'],
-                               'static/media',
-                               test_key,
-                               test_hash,
-                               test_name
-                               )
-                )  # Removes the temporary 'uploaded' test file.
+        request_url = 'https://localhost:443'
+        expected_file_url = 'https://localhost/some/path/image.png'
+        self.assertEqual(
+            create_url(request_url, file_relative_path), expected_file_url
+            )
+        request_url = 'http://localhost:80'
+        expected_file_url = 'http://localhost/some/path/image.png'
+        self.assertEqual(
+            create_url(request_url, file_relative_path), expected_file_url
+            )
