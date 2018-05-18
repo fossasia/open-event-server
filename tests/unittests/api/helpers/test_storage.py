39f657f38611eb71f7b0845a45e3a81afdf25a96
Saurabh S. Chaturvedi <saurabh.chaturvedi63@gmail.com>
2018-05-18 16:00:22 +0530

    Split tests

--- tests/unittests/api/helpers/test_storage.py
+++ tests/unittests/api/helpers/test_storage.py
@@ -1,19 +1,14 @@
 """Test file for storage functions."""
+from unittest import TestCase
 
 from app.api.helpers.storage import create_url
-from tests.unittests.setup_database import Setup
-from tests.unittests.utils import OpenEventTestCase
 
 
-class TestStorageHelperValidation(OpenEventTestCase):
+class TestStorageHelperValidation(TestCase):
     """Test class for testing storage helper functions."""
 
-    def setUp(self):
-        """Method to set up app for testing."""
-        self.app = Setup.create_app()
-
-    def test_create_url(self):
-        """Method to test the create_url function."""
+    def test_arbitrary_url(self):
+        """Method to test a url with arbitrary port."""
 
         request_url = 'https://localhost:5000'
         file_relative_path = '/some/path/image.png'
@@ -22,13 +17,19 @@ class TestStorageHelperValidation(OpenEventTestCase):
         self.assertEqual(
             create_url(request_url, file_relative_path), expected_file_url
             )
-        request_url = 'https://localhost:443'
-        expected_file_url = 'https://localhost/some/path/image.png'
-        self.assertEqual(
-            create_url(request_url, file_relative_path), expected_file_url
-            )
+    
+    def test_http_url(self):
+        """Method to test a url with port 80."""
         request_url = 'http://localhost:80'
         expected_file_url = 'http://localhost/some/path/image.png'
         self.assertEqual(
-            create_url(request_url, file_relative_path), expected_file_url
+            create_url(request_url, '/some/path/image.png'), expected_file_url
+            )
+
+    def test_https_url(self):
+        """Method to test a url with port 443."""
+        request_url = 'https://localhost:443'
+        expected_file_url = 'https://localhost/some/path/image.png'
+        self.assertEqual(
+            create_url(request_url, '/some/path/image.png'), expected_file_url
             )
