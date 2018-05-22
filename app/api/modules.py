fb835d00ff98640198d6cd8b49b7cf862dcb23e5
bhaveshAn <bhaveshanand7@gmail.com>
2018-05-22 22:39:27 +0530

    Fixes #4761 Modules API gives 404 at localhost

--- app/api/modules.py
+++ app/api/modules.py
@@ -1,9 +1,29 @@
-from flask_rest_jsonapi import ResourceDetail
+from flask_rest_jsonapi import ResourceDetail, ResourceList
 
 from app.api.bootstrap import api
 from app.api.schema.modules import ModuleSchema
 from app.models import db
 from app.models.module import Module
+
+
+class ModuleList(ResourceList):
+    """
+    module list
+    """
+    def before_get(self, args, kwargs):
+        """
+        before get method to get the resource id for fetching details
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        kwargs['id'] = 1
+
+    decorators = (api.has_permission('is_super_admin', methods="POST", id="1"),)
+    methods = ['GET', 'POST']
+    schema = ModuleSchema
+    data_layer = {'session': db.session,
+                  'model': Module}
 
 
 class ModuleDetail(ResourceDetail):
