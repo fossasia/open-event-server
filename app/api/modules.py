d6ba0c29b2bccf29e8fdb8c3615075f783824e64
bhaveshAn <bhaveshanand7@gmail.com>
2018-05-23 23:13:40 +0530

    Allow users for GET method

--- app/api/modules.py
+++ app/api/modules.py
@@ -1,13 +1,9 @@
 from flask_rest_jsonapi import ResourceDetail
-from flask import request
-from flask_jwt import _jwt_required
-from flask import current_app as app
 
 from app.api.bootstrap import api
 from app.api.schema.modules import ModuleSchema
 from app.models import db
 from app.models.module import Module
-from app.api.helpers.exceptions import ForbiddenException
 
 
 class ModuleDetail(ResourceDetail):
@@ -23,13 +19,6 @@ class ModuleDetail(ResourceDetail):
         :return:
         """
         kwargs['id'] = 1
-
-        if 'Authorization' in request.headers:
-            _jwt_required(app.config['JWT_DEFAULT_REALM'])
-        else:
-            raise ForbiddenException(
-                {'pointer': '/data/attributes'}, "Authorization Required"
-            )
 
     decorators = (api.has_permission('is_admin', methods='PATCH', id='1'),)
     methods = ['GET', 'PATCH']
