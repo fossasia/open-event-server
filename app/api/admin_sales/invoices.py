62a3bce45d732a2ab3c7dce8aea499840900ac12
Max Lorenz <max-lorenz@gmx.net>
2018-06-12 21:45:32 +0200

    Add invoice route

---
+++ app/api/admin_sales/invoices.py
@@ -0,0 +1,42 @@
+from marshmallow_jsonapi import fields
+from marshmallow_jsonapi.flask import Schema
+from flask_rest_jsonapi import ResourceList
+
+from app.api.bootstrap import api
+from app.api.helpers.utilities import dasherize
+from app.models import db
+from app.models.event_invoice import EventInvoice
+
+
+class AdminSalesInvoicesSchema(Schema):
+    """
+    Sales invoices
+    """
+
+    class Meta:
+        type_ = 'admin-sales-invoices'
+        self_view = 'v1.admin_sales_invoices'
+        inflect = dasherize
+
+    id = fields.String()
+    identifier = fields.String()
+    status = fields.String()
+    amount = fields.Float()
+    created_at = fields.DateTime()
+    completed_at = fields.DateTime()
+    event_name = fields.Method('format_event_name')
+
+    @staticmethod
+    def format_event_name(self):
+        '{}'.format(self.event.name)
+
+
+class AdminSalesInvoicesList(ResourceList):
+    """
+    Resource for sales invoices
+    """
+
+    methods = ['GET']
+    decorators = (api.has_permission('is_admin'), )
+    schema = AdminSalesInvoicesSchema
+    data_layer = {'model': EventInvoice, 'session': db.session}
