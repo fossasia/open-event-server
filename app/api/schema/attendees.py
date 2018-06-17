857a923c72680201e705493fefd4d1acb675b056
Priyanshu Khandelwal <b16027@students.iitmandi.ac.in>
2018-06-17 17:58:14 +0530

    feat: API for Attendee Notes (#4918)

--- app/api/schema/attendees.py
+++ app/api/schema/attendees.py
@@ -46,6 +46,7 @@ class AttendeeSchemaPublic(Schema):
     ticket_id = fields.Str(allow_none=True)
     is_checked_in = fields.Boolean()
     checkin_times = fields.Str(allow_none=True)
+    attendee_notes = fields.Str(allow_none=True)
     pdf_url = fields.Url(dump_only=True)
     event = Relationship(attribute='event',
                          self_view='v1.attendee_event',
