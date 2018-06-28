85d1f0b3af005d456b4d9a990bc6a80787444a34
Kreijstal <2415206+Kreijstal@users.noreply.github.com>
2018-06-28 19:35:06 +0200

    Update events.py

--- app/api/events.py
+++ app/api/events.py
@@ -109,14 +109,14 @@ class EventList(ResourceList):
     def before_post(self, args, kwargs, data=None):
         """
         before post method to verify if the event location is provided before publishing the event
-        AND checks that the user is verified
+        and checks that the user is verified
         :param args:
         :param kwargs:
         :param data:
         :return:
         """
-        isverified = User.query.filter_by(id=kwargs['user_id']).first().is_verified
-        if (data.get('state', None) == 'published' and not isverified):
+        is_verified = User.query.filter_by(id=kwargs['user_id']).first().is_verified
+        if (data.get('state', None) == 'published' and not is_verified):
             raise ForbiddenException({'source': ''},
                                       "Only verified accounts can publish events")
         if data.get('state', None) == 'published' and not data.get('location_name', None):
