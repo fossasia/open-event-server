46dbe671c4c9aca59ccf5bf4e1f919a65bc33275
Max Lorenz <maxlorenz@users.noreply.github.com>
2018-07-06 21:52:01 +0200

    WIP: Deployment (#5005)
    * Add api blueprint preview description to README
    
    * [deploy] Fix docker image and docker-compose file
    
    * Disable ES until PR for it is merged
    
    * [WIP] Add autodeploy scripts
    
    * Fix docker-compose web port
    
    * Add upgrade script
    
    * Fix Dockerfile
    
    * [minor] Fix deprecated logger call
    
    * Fix minor issues with upgrade scripts
    
    * Fix build
    
    * Add first initialization script
    
    * Add first_start
    
    * Fix init script
    
    * Add default user and pass for test
    
    * [minor] Fix spelling error in init
    
    * Add db upgrade
    
    * Fix docker-compose inception error
    
    * Fix docker rebuild
    
    * Fix remaining functioncs
    
    * Add main script to auto updater
    
    * Add working config file for all projects
    
    * Update log format
    
    * Fix minor bug for missing directories
    
    * Add commit date to logs; ignore failure in projects if docker-compose missing
    
    * [minor] Fix git log invocation
    
    * Add readme for autodeploy
    
    * [minor] Fix codacy suggestions
    
    * Move admin credentials to `.env` file

---
+++ autodeploy/docker.py
@@ -0,0 +1,55 @@
+import logging
+
+from command import execute
+
+logger = logging.getLogger(__name__)
+
+
+class DockerComposeError(Exception):
+    def __init__(self, message, errors):
+        super().__init__(message)
+        self.message = message
+        self.errors = errors
+
+    def __str__(self):
+        return '{}:\n {}'.format(self.message, self.errors)
+
+
+def _docker_compose(cwd, *cmd):
+    retcode, out, err = execute(cwd, '/usr/bin/docker-compose', *cmd)
+    if retcode == 0:
+        return out
+
+    logger.error('docker-compose failed: %s', cwd)
+    raise DockerComposeError('docker-compose exited with a non-zero exit code',
+                             err)
+
+
+class DockerCompose():
+    def __init__(self, cwd):
+        self.cwd = cwd
+
+    def ps(self):
+        return _docker_compose(self.cwd, 'ps')
+
+    def start(self):
+        logger.info('starting up...')
+        res = _docker_compose(self.cwd, 'up', '-d')
+        logger.info('started')
+        return res
+
+    def stop(self):
+        logger.info('stopping...')
+        res = _docker_compose(self.cwd, 'stop')
+        logger.info('stopped')
+        return res
+
+    def update(self):
+        logger.info('updating containers...')
+        res = _docker_compose(self.cwd, 'up', '-d', '--build')
+        logger.info('updated')
+        return res
+
+    def exec(self, container, command):
+        res = _docker_compose(self.cwd, 'exec', container, command)
+        return res
