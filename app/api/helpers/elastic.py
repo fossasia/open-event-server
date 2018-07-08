766c3fd7c63a2ccfff5b2e761193aaa3f6f48cf8
Max Lorenz <max-lorenz@gmx.net>
2018-07-08 20:53:00 +0200

    [minor] Remove elastic helper file

--- app/api/helpers/elastic.py
+++
@@ -1,9 +0,0 @@
-"""Elasticsearch helper functions"""
-
-
-def to_dict(response):
-    "Converts elasticsearch responses to dicts for serialization"
-    response = response.to_dict()
-    response['meta'] = response.meta.to_dict()
-
-    return response
