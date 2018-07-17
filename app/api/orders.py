b3d769ce4bcf1f58c197b69160e4507e8e8668f2
Saurabh Chaturvedi <saurabh.chaturvedi63@gmail.com>
2018-07-17 23:22:04 +0530

    Fix HTTP 500 for order's user relationship
    Remove unnecessary comment

--- app/api/orders.py
+++ app/api/orders.py
@@ -319,6 +319,22 @@ class OrderRelationship(ResourceRelationship):
     """
     Order relationship
     """
+
+    def before_get(self, args, kwargs):
+        """
+        before get method to get the resource id for fetching details
+        :param view_kwargs:
+        :return:
+        """
+        if kwargs.get('order_identifier'):
+            order = safe_query(db, Order, 'identifier', kwargs['order_identifier'], 'order_identifier')
+            kwargs['id'] = order.id
+        elif kwargs.get('id'):
+            order = safe_query(db, Order, 'id', kwargs['id'], 'id')
+
+        if not has_access('is_coorganizer', event_id=order.event_id, user_id=order.user_id):
+            return ForbiddenException({'source': ''}, 'You can only access your orders or your event\'s orders')
+
     decorators = (jwt_required,)
     schema = OrderSchema
     data_layer = {'session': db.session,
