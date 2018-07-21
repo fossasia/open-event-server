89d4ff455f1bd1f85db104e03f13a36d329e9f72
Atharva Padhye <32419214+starlord1311@users.noreply.github.com>
2018-07-22 01:56:17 +0530

    Update tasks.py

--- app/api/helpers/tasks.py
+++ app/api/helpers/tasks.py
@@ -397,6 +397,14 @@ def export_speakers_pdf_task(self, event_id):
             UPLOAD_PATHS['exports-temp']['pdf'].format(event_id=event_id, identifier=''))
         result = {
             'download_url': speakers_pdf_url
+        }
+    except Exception as e:
+        print(traceback.format_exc())
+        result = {'__error': True, 'result': str(e)}
+
+    return result
+
+
 @celery.task(base=RequestContextTask, name='export.sessions.pdf', bind=True)
 def export_sessions_pdf_task(self, event_id):
     sessions = db.session.query(Session).filter_by(event_id=event_id)
