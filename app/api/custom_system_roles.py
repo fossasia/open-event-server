a0a434ea92bb0d79938724deaba71415b412dc1b
Bhavesh Anand <bhaveshanand7@gmail.com>
2018-08-07 00:18:09 +0530

    Fixes #5293 feat: Add CRUD APIs for PanelPermission (#5294)

--- app/api/custom_system_roles.py
+++ app/api/custom_system_roles.py
@@ -4,6 +4,8 @@ from app.api.bootstrap import api
 from app.api.schema.custom_system_roles import CustomSystemRoleSchema
 from app.models import db
 from app.models.custom_system_role import CustomSysRole
+from app.models.panel_permission import PanelPermission
+from app.api.helpers.db import safe_query
 
 
 class CustomSystemRoleList(ResourceList):
@@ -20,6 +22,18 @@ class CustomSystemRoleDetail(ResourceDetail):
     """
     Custom System Role detail by id
     """
+    def before_get_object(self, view_kwargs):
+        """
+        before get method for user object
+        :param view_kwargs:
+        :return:
+        """
+        if view_kwargs.get('role_id') is not None:
+            panel_perm = safe_query(self, PanelPermission, 'id', view_kwargs['role_id'], 'role_id')
+            if panel_perm.role_id is not None:
+                view_kwargs['id'] = panel_perm.role_id
+            else:
+                view_kwargs['id'] = None
 
     decorators = (api.has_permission('is_admin', methods="PATCH,DELETE"),)
     schema = CustomSystemRoleSchema
