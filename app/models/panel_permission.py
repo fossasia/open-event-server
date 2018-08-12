8d93e3aa6cce42a59a729b1c3ab1043a67904452
bhaveshAn <bhaveshanand7@gmail.com>
2018-08-12 21:39:40 +0530

    Fixes #5318 feat: Add relationship for panel_permission in custom system role APIs

--- app/models/panel_permission.py
+++ app/models/panel_permission.py
@@ -1,6 +1,12 @@
 from app.models import db
 
 
+roles_panels = db.Table('roles_panels',
+                        db.Column('role_id', db.Integer, db.ForeignKey('custom_sys_roles.id', ondelete='CASCADE')),
+                        db.Column('panel_permission_id', db.Integer,
+                                  db.ForeignKey('panel_permissions.id', ondelete='CASCADE')))
+
+
 class PanelPermission(db.Model):
     """Super-Admin Panel Permissions
     """
@@ -10,14 +16,18 @@ class PanelPermission(db.Model):
     # Super Admin panel name
     panel_name = db.Column(db.String)
     # Custom System Role
-    role_id = db.Column(db.Integer, db.ForeignKey('custom_sys_roles.id',  ondelete='CASCADE'))
-    role = db.relationship('CustomSysRole')
+    roles = db.relationship('CustomSysRole',
+                            secondary=roles_panels,
+                            backref=db.backref('panels', lazy='dynamic'))
 
     can_access = db.Column(db.Boolean)
 
-    def __init__(self, panel_name, role, can_access=True):
+    def __init__(self, panel_name, roles=None, can_access=True):
         self.panel_name = panel_name
-        self.role = role
+        if roles is None:
+            self.roles = []
+        else:
+            self.roles = roles
         self.can_access = can_access
 
     def __repr__(self):
