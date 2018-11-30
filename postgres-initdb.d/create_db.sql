198fd76879ee2661b478e6a1f6bdbb89f4882f71
Areeb Jamal <jamal.areeb@gmail.com>
2018-11-30 11:32:38 +0530

    chore(docker): Add celery and improve docker compose (#5408)
    - Add celery container
    - Expose correct port for web app
    - Remove port bindings from postgres and redis containers
    - Use proper db name and password
    - Update docker compose version

--- postgres-initdb.d/create_db.sql
+++ postgres-initdb.d/create_db.sql
@@ -1,7 +1,6 @@
 -- Open Event database
-CREATE USER john WITH PASSWORD 'start';
-CREATE DATABASE oevent_2 WITH OWNER john;
+CREATE USER open_event_user WITH PASSWORD 'opev_pass';
+CREATE DATABASE open_event WITH OWNER open_event_user;
 
 -- Test database
-CREATE USER open_event_user WITH PASSWORD 'test';
 CREATE DATABASE opev_test WITH OWNER open_event_user;
