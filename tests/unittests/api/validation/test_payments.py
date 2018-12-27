9d650bdfcbe11a32c8705324ee4fc30216f578af
Sai Charan <saicharan@Rajas-MacBook-Pro.local>
2018-12-27 15:06:41 -0600

    constructor params fix

--- tests/unittests/api/validation/test_payments.py
+++ tests/unittests/api/validation/test_payments.py
@@ -1,5 +1,6 @@
 import unittest
 
+import stripe
 from tests.unittests.setup_database import Setup
 from tests.unittests.utils import OpenEventTestCase
 from app.api.helpers.payment import StripePaymentsManager, PayPalPaymentsManager
@@ -8,7 +9,7 @@ from app.models.stripe_authorization import StripeAuthorization
 
 
 class OrderInvoice:
-    def __init__(self,user,amount,stripe_token):
+    def __init__(self):
         self.user = {'email': 'test@gmail.com'}
         self.event = {'name': 'test_name'}
         self.user_id = '99999'
@@ -16,23 +17,26 @@ class OrderInvoice:
         self.amount = 75
         self.stripe_token = 'pk_test_GzzeUfMBivRmjuVICl5rpAJZ'
 
+
 class TestPaymentsTestCase(OpenEventTestCase):
     def setUp(self):
         self.app = Setup.create_app()
         order_invoice = OrderInvoice()
+        stripe_payments = StripePaymentsManager()
 
     def test_stripe_credentials(self):
         """
         Test stripe credentials without integer as parameter
         :return:
         """
-        settings['stripe_secret_key'] = 'sk_test_zgizVSrtc7DyWDMFPCACErVa'
-        settings["stripe_publishable_key"] = 'pk_test_GzzeUfMBivRmjuVICl5rpAJZ'
+        credentials = {}
+        credentials['stripe_secret_key'] = 'sk_test_zgizVSrtc7DyWDMFPCACErVa'
+        credentials["stripe_publishable_key"] = 'pk_test_GzzeUfMBivRmjuVICl5rpAJZ'
         data = {
-            'SECRET_KEY': settings['stripe_secret_key'],
-            'PUBLISHABLE_KEY': settings["stripe_publishable_key"]
+            'SECRET_KEY': credentials['stripe_secret_key'],
+            'PUBLISHABLE_KEY': credentials["stripe_publishable_key"]
             }
-        return self.assertEqual(get_credentials(), data)
+        return self.assertEqual(stripe_payments.get_credentials(), data)
 
     def test_stripe_credentials_event_integer(self):
         """
@@ -45,7 +49,7 @@ class TestPaymentsTestCase(OpenEventTestCase):
                     'SECRET_KEY': authorization.stripe_secret_key,
                     'PUBLISHABLE_KEY': authorization.stripe_publishable_key
             }
-        return self.assertEqual(get_credentials(1), data)
+        return self.assertEqual(stripe_payments.get_credentials(1), data)
 
     def test_capture_payment(self):
         """
@@ -58,10 +62,9 @@ class TestPaymentsTestCase(OpenEventTestCase):
         }
         currency = 'USD'
         stripe.api_key = credentials['publishable_key']
-        captured_payment = capture_payment()
+        captured_payment = stripe_payments.capture_payment(order_invoice, currency, credentials)
         charge_object_test = stripe.Charge()
-        assert type(captured_payment)==type(charge_object_test)
+        assert isinstance(captured_payment) == isinstance(charge_object_test)
 
-    
     def tearDown(self):
         Setup.drop_db()
