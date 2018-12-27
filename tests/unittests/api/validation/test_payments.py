0f9c8d6fab5be3fe5dcf8dcf889faceea11e5410
Sai Charan <saicharan@Rajas-MacBook-Pro.local>
2018-12-27 12:40:38 -0600

    Indentations and whitespace fixes

--- tests/unittests/api/validation/test_payments.py
+++ tests/unittests/api/validation/test_payments.py
@@ -9,33 +9,30 @@ from app.models.stripe_authorization import StripeAuthorization
 
 class OrderInvoice:
     def __init__(self,user,amount,stripe_token):
-      self.user = {'email':'test@gmail.com'}
-      self.event = {'name':'test_name'}
-      self.user_id = '99999'
-      self.event_id = '99999'
-      self.amount = 75
-      self.stripe_token = 'pk_test_GzzeUfMBivRmjuVICl5rpAJZ'
+        self.user = {'email': 'test@gmail.com'}
+        self.event = {'name': 'test_name'}
+        self.user_id = '99999'
+        self.event_id = '99999'
+        self.amount = 75
+        self.stripe_token = 'pk_test_GzzeUfMBivRmjuVICl5rpAJZ'
 
-
-    
 class TestPaymentsTestCase(OpenEventTestCase):
     def setUp(self):
         self.app = Setup.create_app()
         order_invoice = OrderInvoice()
-        
-    
+
     def test_stripe_credentials(self):
         """
         Test stripe credentials without integer as parameter
         :return:
         """
         settings['stripe_secret_key'] = 'sk_test_zgizVSrtc7DyWDMFPCACErVa'
-        settings["stripe_publishable_key"]= 'pk_test_GzzeUfMBivRmjuVICl5rpAJZ'
+        settings["stripe_publishable_key"] = 'pk_test_GzzeUfMBivRmjuVICl5rpAJZ'
         data = {
             'SECRET_KEY': settings['stripe_secret_key'],
             'PUBLISHABLE_KEY': settings["stripe_publishable_key"]
             }
-        return self.assertEqual(get_credentials(),data)
+        return self.assertEqual(get_credentials(), data)
 
     def test_stripe_credentials_event_integer(self):
         """
@@ -48,16 +45,16 @@ class TestPaymentsTestCase(OpenEventTestCase):
                     'SECRET_KEY': authorization.stripe_secret_key,
                     'PUBLISHABLE_KEY': authorization.stripe_publishable_key
             }
-        return self.assertEqual(get_credentials(1),data) 
+        return self.assertEqual(get_credentials(1), data)
 
     def test_capture_payment(self):
-         """
+        """
         Testing payment capture
         :return:
         """
         credentials = {
-        'secret_key': 'sk_test_zgizVSrtc7DyWDMFPCACErVa',
-        'publishable_key':'pk_test_GzzeUfMBivRmjuVICl5rpAJZ'
+            'secret_key': 'sk_test_zgizVSrtc7DyWDMFPCACErVa',
+            'publishable_key': 'pk_test_GzzeUfMBivRmjuVICl5rpAJZ'
         }
         currency = 'USD'
         stripe.api_key = credentials['publishable_key']
