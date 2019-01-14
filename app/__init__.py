e8657ea1258fb537c5741a5bbbd2d131827e6f24
prateekj117 <prateekj1171998@gmail.com>
2019-01-14 20:12:10 +0530

    WIP: Set up logging correctly

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from celery.signals import after_task_publish
+from logging.config import dictConfig
 import logging
 import os.path
 from envparse import env
@@ -93,8 +94,26 @@ def create_app():
     app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
     app.config['FILE_SYSTEM_STORAGE_FILE_VIEW'] = 'static'
 
-    app.logger.addHandler(logging.StreamHandler(sys.stdout))
-    app.logger.setLevel(logging.ERROR)
+    dictConfig({
+        'version': 1,
+        'handlers': {
+            'console': {
+                'level': 'WARN',
+                'class': 'logging.StreamHandler',
+                'stream': 'ext://sys.stdout'
+            }
+        },
+        'loggers': {
+            'root': {
+                'handlers': ['console'],
+                'level': 'WARN'
+            },
+            'default': {
+                'handlers': ['console'],
+                'level': 'INFO'
+            }
+        }
+    })
 
     # set up jwt
     app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
