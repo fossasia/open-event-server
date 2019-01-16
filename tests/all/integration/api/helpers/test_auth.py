12ef143ef6ad0ba65b68a1623d945bf3fd67498d
sai <saicharan.reddy1@gmail.com>
2019-01-16 13:23:35 +0530

    Unused modules of auth test removed

--- tests/all/integration/api/helpers/test_auth.py
+++ tests/all/integration/api/helpers/test_auth.py
@@ -1,11 +1,11 @@
 from app import current_app as app
 from tests.all.integration.auth_helper import create_user
-from app.api.helpers.db import save_to_db
 from tests.all.integration.utils import OpenEventTestCase
 from app.api.helpers.auth import *
 from tests.all.integration.setup_database import Setup
 
 import unittest
+
 
 class TestAuthentication(OpenEventTestCase):
     def setUp(self):
@@ -15,7 +15,10 @@ class TestAuthentication(OpenEventTestCase):
         """Method to test the registered user details"""
 
         with app.test_request_context():
-            auth_manager = AuthManager()
+            auth_manager = auth.AuthManager()
             auth_manager.init_login(app)
             user = create_user(email = 'test@gmail.com', password = 'password')
             self.assertEqual(user, auth_manager.load_user(user.id))
+
+if __name__ == '__main__':
+    unittest.main()
