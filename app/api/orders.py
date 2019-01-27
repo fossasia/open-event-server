4db1192a847d630e4d9d986fe71af00c18a6c747
Abhinav Khare <abhinav.khare31@gmail.com>
2019-01-27 23:21:48 +0800

    Orders API: Remove verified user constraint

--- app/api/orders.py
+++ app/api/orders.py
@@ -50,10 +50,6 @@ class OrdersListPost(ResourceList):
         """
         require_relationship(['event'], data)
 
-        if not current_user.is_verified:
-            raise ForbiddenException({'source': ''},
-                                     "Only verified accounts can place orders")
-
         # Create on site attendees.
         if request.args.get('onsite', False):
             create_onsite_attendees_for_order(data)
