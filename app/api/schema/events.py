a0c3cf9b6a8d4c0346005dc3806dd07f208ab39c
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-02-04 21:22:17 +0530

    Adding is_featured field in events (#5575)

--- app/api/schema/events.py
+++ app/api/schema/events.py
@@ -100,6 +100,7 @@ class EventSchemaPublic(SoftDeletionSchema):
     paypal_email = fields.Str(allow_none=True)
     is_tax_enabled = fields.Bool(default=False)
     is_donation_enabled = fields.Bool(default=False)
+    is_featured = fields.Bool(default=False)
     can_pay_by_paypal = fields.Bool(default=False)
     can_pay_by_stripe = fields.Bool(default=False)
     can_pay_by_cheque = fields.Bool(default=False)
