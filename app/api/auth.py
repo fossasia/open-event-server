af837986b5e80da7e33870a5a775c37b6aaaf6a6
saicharan <saicharan.reddy1@gmail.com>
2019-02-09 09:59:45 +0530

    Facebook login key error
    Facebook login key error
    
    Codacy fix

--- app/api/auth.py
+++ app/api/auth.py
@@ -95,7 +95,7 @@ def login_user(provider, auth_code):
             'code': auth_code
         }
         if not payload['client_id'] or payload['client_secret']:
-            return make_response(jsonify(message="Facebook Login hasn't been configured"), 501)
+            return make_response(jsonify(message="Facebook has not been configured yet"), 501)
         access_token = requests.get('https://graph.facebook.com/v3.0/oauth/access_token', params=payload).json()
         payload_details = {
             'input_token': access_token['access_token'],
