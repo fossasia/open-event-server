87d11b95d035783c36bb9091da7d1fe9bc7c4338
Saicharan Reddy <saicharan.reddy1@gmail.com>
2019-02-09 19:57:01 +0530

    fix: Handle absence of auth keys exception (#5589)

--- app/api/auth.py
+++ app/api/auth.py
@@ -94,6 +94,8 @@ def login_user(provider, auth_code):
             'client_secret': provider_class.get_client_secret(),
             'code': auth_code
         }
+        if not payload['client_id'] or payload['client_secret']:
+            raise NotImplementedError({'source': ''}, 'Facebook Login Not Configured')
         access_token = requests.get('https://graph.facebook.com/v3.0/oauth/access_token', params=payload).json()
         payload_details = {
             'input_token': access_token['access_token'],
