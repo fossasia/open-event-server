15daecfca1f018301d9e4596ed592eb60e48e640
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-02-26 20:09:28 +0530

    adding speaker type queries (beta)
    Currently using python segments

--- app/api/schema/event_statistics.py
+++ app/api/schema/event_statistics.py
@@ -1,10 +1,12 @@
 from marshmallow_jsonapi import fields
 from marshmallow_jsonapi.flask import Schema
+from sqlalchemy import func
 
 from app.api.helpers.utilities import dasherize
 from app.models.session import Session
 from app.models.speaker import Speaker
 from app.models.sponsor import Sponsor
+from app.models import db
 
 
 class EventStatisticsGeneralSchema(Schema):
@@ -29,6 +31,10 @@ class EventStatisticsGeneralSchema(Schema):
     sessions_pending = fields.Method("sessions_pending_count")
     sessions_rejected = fields.Method("sessions_rejected_count")
     speakers = fields.Method("speakers_count")
+    speakers_accepted = fields.Method("speakers_accepted_count")
+    speakers_confirmed = fields.Method("speakers_confirmed_count")
+    speakers_pending = fields.Method("speakers_pending_count")
+    speakers_rejected = fields.Method("speakers_rejected_count")
     sessions = fields.Method("sessions_count")
     sponsors = fields.Method("sponsors_count")
 
@@ -40,6 +46,34 @@ class EventStatisticsGeneralSchema(Schema):
 
     def sessions_accepted_count(self, obj):
         return Session.query.filter_by(event_id=obj.id, state='accepted').count()
+
+    def speakers_accepted_count(self, obj):
+        current_query = Session.query.filter_by(event_id=obj.id, state='accepted')
+        all_speakers = []
+        for sessions in current_query:
+            all_speakers += [speaker for speaker in sessions.speakers]
+        return len(set(all_speakers))
+
+    def speakers_confirmed_count(self, obj):
+        current_query = Session.query.filter_by(event_id=obj.id, state='confirmed')
+        all_speakers = []
+        for sessions in current_query:
+            all_speakers += [speaker for speaker in sessions.speakers]
+        return len(set(all_speakers))
+
+    def speakers_pending_count(self, obj):
+        current_query = Session.query.filter_by(event_id=obj.id, state='pending')
+        all_speakers = []
+        for sessions in current_query:
+            all_speakers += [speaker for speaker in sessions.speakers]
+        return len(set(all_speakers))
+
+    def speakers_rejected_count(self, obj):
+        current_query = Session.query.filter_by(event_id=obj.id, state='rejected')
+        all_speakers = []
+        for sessions in current_query:
+            all_speakers += [speaker for speaker in sessions.speakers]
+        return len(set(all_speakers))
 
     def sessions_confirmed_count(self, obj):
         return Session.query.filter_by(event_id=obj.id, state='confirmed').count()
