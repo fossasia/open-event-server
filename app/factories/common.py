bbd0d18506d2b244ad00c579beab6da06cc48303
Saicharan Reddy <saicharan.reddy1@gmail.com>
2019-03-22 10:23:21 +0530

    test:ICalExporter test added (#5689)
    * ical export test
    
    Travis CI datetime requirement
    
    * ical export test
    
    Naming made compliant to codacy
    
    whitespace
    
    Datetime objects in common
    
    * Update common.py

--- app/factories/common.py
+++ app/factories/common.py
@@ -1,4 +1,5 @@
 import factory
+import datetime
 from app.api.helpers.utilities import static_page, image_link
 # use camelCase for naming variables
 
@@ -7,10 +8,10 @@ email_ = factory.Sequence(lambda n: 'user{0}@example.com'.format(n))
 integer_ = 25
 url_ = static_page
 imageUrl_ = image_link
-date_ = '2016-12-13T23:59:59.123456+00:00'
-dateFuture_ = '2099-12-13T23:59:59.123456+00:00'
-dateEndFuture_ = '2099-12-14T23:59:59.123456+00:00'
-dateEnd_ = '2020-12-14T23:59:59.123456+00:00'
+date_ = datetime.datetime(2016, 12, 13)
+dateFuture_ = datetime.datetime(2099, 12, 13)
+dateEndFuture_ = datetime.datetime(2099, 12, 14)
+dateEnd_ = datetime.datetime(2020, 12, 14)
 country_ = 'US'
 currency_ = 'USD'
 int_ = '1'
