3136d9051c3150769887a264a495d6f0a7e367ee
Prateek Jain <prateekj1171998@gmail.com>
2019-04-02 03:20:41 +0530

    Calculate number of migration head correctly. (#5742)

--- scripts/test_multiple_heads.sh
+++ scripts/test_multiple_heads.sh
@@ -1,4 +1,4 @@
-lines=`python3 manage.py db heads | wc | awk '{print $1}'`
+lines=`python3 manage.py db heads | grep -c "head" | wc | awk '{print $1}'`
 if [ $lines -ne 1 ]
 then
     echo "Error: Multiple Migration Heads"
