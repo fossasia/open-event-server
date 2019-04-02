598323c28a036942c60817057709dd5792af7203
Anupam Dagar <dagaranupam@gmail.com>
2019-04-02 22:02:42 +0530

    Add test for get_ticket_purchased_organizer_notification_actions function in system notifications helper. Reference: #5320

--- tests/all/integration/api/helpers/test_systemnotifications.py
+++ tests/all/integration/api/helpers/test_systemnotifications.py
@@ -8,7 +8,8 @@ from app.api.helpers.system_notifications import (
     get_monthly_payment_notification_actions,
     get_monthly_payment_follow_up_notification_actions,
     get_ticket_purchased_notification_actions,
-    get_ticket_purchased_attendee_notification_actions
+    get_ticket_purchased_attendee_notification_actions,
+    get_ticket_purchased_organizer_notification_actions
     )
 from tests.all.integration.setup_database import Setup
 
@@ -68,6 +69,15 @@ class TestSystemNotificationHelperValidation(OpenEventTestCase):
         with app.test_request_context():
             request_pdfurl = 'https://localhost/pdf/e/24324/'
             response = get_ticket_purchased_attendee_notification_actions(request_pdfurl)
+            self.assertIsInstance(response, list)
+
+    def test_get_ticket_purchased_organizer_notification_actions(self):
+        """Method to test the actions associated with a notification of tickets purchased for the event organizer."""
+
+        with app.test_request_context():
+            request_url = 'https://localhost/e/345525/order'
+            request_order_id = 1
+            response = get_ticket_purchased_organizer_notification_actions(request_order_id, request_url)
             self.assertIsInstance(response, list)
 
 
