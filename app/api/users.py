215c33b691c6114a628ea67f42496d6c010803c8
Shreyansh Dwivedi <shreyanshdwivedi1997@gmail.com>
2019-04-11 21:45:35 +0530

    fix condition for restoring deleted users

--- app/api/users.py
+++ app/api/users.py
@@ -208,8 +208,8 @@ class UserDetail(ResourceDetail):
         #     data['thumbnail_image_url'] = uploaded_images['thumbnail_image_url']
         #     data['icon_image_url'] = uploaded_images['icon_image_url']
 
-        if has_access('is_admin') and data.get('deleted_at') and data['deleted_at'] != user.deleted_at:
-            user.deleted_at = data['deleted_at']
+        if has_access('is_admin') and data.get('deleted_at') != user.deleted_at:
+            user.deleted_at = data.get('deleted_at')
 
         if data.get('email') and data['email'] != user.email:
             try:
