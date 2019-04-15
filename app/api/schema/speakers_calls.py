7dbda5c694be7949f07ce024bfe5985b696cd3d7
Shreyansh Dwivedi <shreyanshdwivedi1997@gmail.com>
2019-04-15 18:46:44 +0530

    Adds tests to validate speakers_call date

--- app/api/schema/speakers_calls.py
+++ app/api/schema/speakers_calls.py
@@ -32,14 +32,17 @@ class SpeakersCallSchema(SoftDeletionSchema):
             if 'ends_at' not in data:
                 data['ends_at'] = speakers_calls.ends_at
 
+            if 'event_ends_at' not in data:
+                data['event_ends_at'] = speakers_calls.event.ends_at
+
         if data['starts_at'] >= data['ends_at']:
             raise UnprocessableEntity({'pointer': '/data/attributes/ends-at'}, "ends-at should be after starts-at")
 
-        if data['starts_at'] > speakers_calls.event.ends_at:
+        if data['starts_at'] > data['event_ends_at']:
             raise UnprocessableEntity({'pointer': '/data/attributes/starts-at'},
                                       "speakers-call starts-at should be before event ends-at")
 
-        if data['ends_at'] > speakers_calls.event.ends_at:
+        if data['ends_at'] > data['event_ends_at']:
             raise UnprocessableEntity({'pointer': '/data/attributes/ends-at'},
                                       "speakers-call ends-at should be before event ends-at")
 
