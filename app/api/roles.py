93dda9b4aff59d043cd2c6b3a0e71516e563513b
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-05-17 04:12:31 +0530

    add organiser transfer endpoint in server

--- app/api/roles.py
+++ app/api/roles.py
@@ -8,7 +8,14 @@ from app.models.role import Role
 from app.models.role_invite import RoleInvite
 from app.models.users_events_role import UsersEventsRoles
 from app.api.helpers.exceptions import UnprocessableEntity
+from app.api.helpers.errors import NotFoundError, ServerError
+from sqlalchemy.orm.exc import NoResultFound
+from flask import Blueprint, request, jsonify
+from app.models.users_events_role import UsersEventsRoles as UER
+from app.models.role import Role
+from app.api.helpers.db import save_to_db
 
+role_misc_routes = Blueprint('role_misc', __name__, url_prefix='/v1')
 
 class RoleList(ResourceList):
     """
@@ -74,3 +81,45 @@ class RoleDetail(ResourceDetail):
                   'methods': {
                       'before_get_object': before_get_object
                   }}
+
+
+@role_misc_routes.route('/change-organiser', methods=['POST'])
+def change_organiser():
+    current_organiser_id = request.json.get('current_org_id', None)
+    new_organiser_id = request.json.get('new_org_id', None)
+    event_id = request.json.get('event_id', None)
+
+    # Check if the user is actually an organiser of the event
+    # Delete the User Event Role
+    try:
+        role_entry_organiser_to_delete = UER.query.filter_by(user_id=current_organiser_id,
+                                                            event_id=event_id,
+                                                            role_id=1).one()
+        db.session.delete(role_entry_organiser_to_delete)
+
+    except NoResultFound:
+        return NotFoundError({'source': ''}, 'Organiser Role Entry not found').respond()
+
+    except Exception as e:
+        return ServerError({'source': ''}, e).respond()
+
+    # Check if the coorganiser is not found but the earlier entry has been deleted
+    # Rollback the last commit in case of exception to restore organiser status
+    try:
+        role_entry_coorg_to_organiser = UER.query.filter_by(user_id=new_organiser_id,
+                                                            event_id=event_id,
+                                                            role_id=2).one()
+        role_entry_coorg_to_organiser.role_id = 1
+        save_to_db(role_entry_coorg_to_organiser)
+
+    except NoResultFound:
+        db.session.rollback()
+        return NotFoundError({'source': ''}, 'Coorganiser Role Entry not found').respond()
+
+    # Handle Unexpected Errors
+    except Exception as e:
+        return ServerError({'source': ''}, e).respond()
+
+    return jsonify({
+        "message": 'Success'
+    })
