80742a3a907b13bd6c07e3dc7ac22f718475741a
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-05-21 19:41:43 +0530

    resolve merge conflicts and resolve hound issues

--- app/api/schema/ticket_fees.py
+++ app/api/schema/ticket_fees.py
@@ -1,12 +1,9 @@
-from marshmallow import validate, validates_schema
+from marshmallow import validate
 from marshmallow_jsonapi import fields
 from marshmallow_jsonapi.flask import Schema
 
-from sqlalchemy.orm.exc import NoResultFound
 from app.api.helpers.static import PAYMENT_CURRENCY_CHOICES
 from app.api.helpers.utilities import dasherize
-from app.api.helpers.exceptions import ConflictException, UnprocessableEntity
-from app.models.ticket_fee import TicketFees
 
 
 class TicketFeesSchema(Schema):
@@ -22,27 +19,8 @@ class TicketFeesSchema(Schema):
         self_view_kwargs = {'id': '<id>'}
         inflect = dasherize
 
-    @validates_schema(pass_original=True)
-    def validate_currency_country(self, data, original_data):
-        if 'country' in data and 'currency' in data:
-            if data['country'] and data['currency']:
-                try:
-                    TicketFees.query.filter_by(country=data['country'], currency=data['currency']).one()
-                except NoResultFound:
-                    pass
-                else:
-                    # modifications for PATCH request
-                    if 'id' not in original_data:
-                        raise ConflictException(
-                            {'pointer': ''},
-                            "({}-{}) Combination already exists".format(data['currency'], data['country']))
-            else:
-                raise UnprocessableEntity({'source': ''}, "Country or Currency cannot be NULL")
-        else:
-            raise UnprocessableEntity({'source': ''}, "Country or Currency Attribute is missing")
-
     id = fields.Integer(dump_only=True)
-    currency = fields.Str(validate=validate.OneOf(choices=PAYMENT_CURRENCY_CHOICES), allow_none=True)
-    country = fields.String(allow_none=True)
+    currency = fields.Str(validate=validate.OneOf(choices=PAYMENT_CURRENCY_CHOICES), allow_none=False)
+    country = fields.String(allow_none=False)
     service_fee = fields.Float(validate=lambda n: n >= 0, allow_none=True)
     maximum_fee = fields.Float(validate=lambda n: n >= 0, allow_none=True)
--- app/api/ticket_fees.py
+++ app/api/ticket_fees.py
@@ -1,26 +1,62 @@
 from flask_rest_jsonapi import ResourceDetail, ResourceList
-
+from sqlalchemy.orm.exc import NoResultFound
 from app import db
 from app.api.bootstrap import api
 from app.api.schema.ticket_fees import TicketFeesSchema
 from app.models.ticket_fee import TicketFees
+from app.api.helpers.exceptions import ConflictException
 
 
 class TicketFeeList(ResourceList):
     """
     List and create TicketFees
     """
+
+    def before_post(self, args, kwargs, data):
+        """
+        method to check for existing country currency combination
+        :param args:
+        :param kwargs:
+        :param data:
+        :return:
+        """
+        if (data['country'] and data['currency']):
+            try:
+                already_existing_combination = TicketFees.query.filter_by(country=data['country'],
+                                                                          currency=data['currency']).one()
+                if already_existing_combination:
+                    raise ConflictException({'pointer': 'data/attributes/country'}, 'Combination Exists')
+            except NoResultFound:
+                pass
+
     decorators = (api.has_permission('is_admin'),)
     schema = TicketFeesSchema
     data_layer = {'session': db.session,
-                  'model': TicketFees
-                  }
+                  'model': TicketFees}
 
 
 class TicketFeeDetail(ResourceDetail):
     """
     ticket_fee detail by id
     """
+
+    def before_patch(self, args, kwargs, data):
+        """
+        method to check if patched combination id is same as new one
+        :param args:
+        :param kwargs:
+        :param data:
+        :return:
+        """
+        if (data['country'] and data['currency']):
+            try:
+                already_existing_combination = TicketFees.query.filter_by(country=data['country'],
+                                                                          currency=data['currency']).one()
+                if already_existing_combination.id != kwargs['id']:
+                    raise ConflictException({'pointer': 'data/attributes/country'}, 'Combination Exists')
+            except NoResultFound:
+                pass
+
     decorators = (api.has_permission('is_admin'),)
     schema = TicketFeesSchema
     data_layer = {'session': db.session,
