567008c5f4ac4214082d2ad952b9dd4f318a7d83
Areeb Jamal <jamal.areeb@gmail.com>
2019-06-20 12:38:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -131,8 +131,7 @@ class TestingConfig(Config):
 
     ENV = 'testing'
     TESTING = True
-    DEBUG = True
-    CELERY_ALWAYS_EAGER = True
+    CELERY_TASK_ALWAYS_EAGER = True
     CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
     SQLALCHEMY_RECORD_QUERIES = True
     DEBUG_TB_ENABLED = False
