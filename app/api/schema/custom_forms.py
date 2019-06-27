a45e958f1bd4f759e719a203d74969a2156cfe8c
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2019-06-27 23:46:07 +0530

    remove redundant required parameter

--- app/api/schema/custom_forms.py
+++ app/api/schema/custom_forms.py
@@ -31,7 +31,7 @@ class CustomFormSchema(SoftDeletionSchema):
     is_included = fields.Boolean(default=False)
     is_fixed = fields.Boolean(default=False)
     is_custom_question = fields.Boolean(default=False, allow_none=False)
-    prompt = fields.Str(required=False, allow_none=True)
+    prompt = fields.Str(allow_none=True)
     event = Relationship(attribute='event',
                          self_view='v1.custom_form_event',
                          self_view_kwargs={'id': '<id>'},
