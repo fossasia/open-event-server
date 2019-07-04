1d7c9824694d0aecfe553a9b2ff7ffb4906de64c
Anupam Dagar <dagaranupam@gmail.com>
2019-07-04 07:09:40 +0530

    rename event date function name.

--- app/api/admin_sales/fees.py
+++ app/api/admin_sales/fees.py
@@ -25,7 +25,7 @@ class AdminSalesFeesSchema(Schema):
     maximum_fee = fields.Float(attribute='maximum_fee')
     revenue = fields.Method('calc_revenue')
     ticket_count = fields.Method('calc_ticket_count')
-    event_date = fields.Method('get_eventdate')
+    event_date = fields.Method('get_event_date')
 
     @staticmethod
     def calc_ticket_count(obj):
@@ -39,7 +39,7 @@ class AdminSalesFeesSchema(Schema):
             [o.get_revenue() for o in obj.orders if o.status == 'completed'])
 
     @staticmethod
-    def get_eventdate(obj):
+    def get_event_date(obj):
         return obj.starts_at.isoformat()
 
 
