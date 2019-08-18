f244528c004b9cbc0fdcd741e0561e7ac1cfcbca
prateekj117 <prateekj1171998@gmail.com>
2019-08-18 23:15:48 +0530

    Update sql query.

--- migrations/versions/rev-2019-08-17-20:46:39-9172ceb350b8_duplicate_ticket_tag.py
+++ migrations/versions/rev-2019-08-17-20:46:39-9172ceb350b8_duplicate_ticket_tag.py
@@ -10,18 +10,18 @@ from alembic import op
 import sqlalchemy as sa
 import sqlalchemy_utils
 
-
 # revision identifiers, used by Alembic.
 revision = '9172ceb350b8'
 down_revision = '246b5b6123af'
 
 
 def upgrade():
-    op.execute("UPDATE ticket_tag SET deleted_at = current_timestamp, name = concat(name, '.deleted') where id in (SELECT ticket_tag.id FROM ticket_tag LEFT OUTER JOIN (SELECT MIN(id) as id, name, event_id FROM ticket_tag GROUP BY name, event_id) as KeepRows ON ticket_tag.id = KeepRows.id WHERE KeepRows.id IS NULL);",
-               execution_options=None)
+    op.execute(
+        "UPDATE ticket_tag SET deleted_at = current_timestamp, name = concat(name, id, '.deleted') where id in (SELECT ticket_tag.id FROM ticket_tag LEFT OUTER JOIN (SELECT MIN(id) as id, name, event_id FROM ticket_tag GROUP BY name, event_id) as KeepRows ON ticket_tag.id = KeepRows.id WHERE KeepRows.id IS NULL);",
+        execution_options=None)
 
 
 def downgrade():
     op.execute(
-        "UPDATE ticket_tag SET deleted_at = null, name = left(name, length(name)-8) where right(name, 8) = '.deleted';",
+        "UPDATE ticket_tag SET deleted_at = null, name = left(name, length(name)-length(cast(id as varchar))-8) where right(name, 8) = '.deleted';",
         execution_options=None)
