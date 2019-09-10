62d7810b4d24d360a4f0a6cfb95772bb9e2c080b
Prateek Jain <prateekj1171998@gmail.com>
2019-09-11 03:58:34 +0530

    fix: should not return fields related to a deleted field (#6361)
    * fix: should not return fields related to a deleted field
    
    * Add condition for get_trashed query param
    
    * Remove duplication of code
    
    * Remove duplication of one
    
    * Checking dredd tests
    
    * Add check to see if 'deleted_at' attribute exists.
    
    * Undo changes in hook_main

--- app/api/helpers/db.py
+++ app/api/helpers/db.py
@@ -1,6 +1,7 @@
 import logging
 import traceback
 
+from flask import request
 from flask_rest_jsonapi.exceptions import ObjectNotFound
 from sqlalchemy import func
 from sqlalchemy.orm.exc import NoResultFound
@@ -66,7 +67,11 @@ def safe_query(self, model, column_name, value, parameter_name):
     :return:
     """
     try:
-        record = self.session.query(model).filter(getattr(model, column_name) == value).one()
+        record = self.session.query(model).filter(getattr(model, column_name) == value)
+        if request.args.get('get_trashed') != 'true':
+            if hasattr(model, 'deleted_at'):
+                record = record.filter(model.deleted_at == None)
+        record = record.one()
     except NoResultFound:
         raise ObjectNotFound({'parameter': '{}'.format(parameter_name)},
                              "{}: {} not found".format(model.__name__, value))
