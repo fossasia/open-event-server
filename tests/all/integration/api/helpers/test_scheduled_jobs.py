9ae08d2c58885deb0572dcf59d8641d7f3ae2fda
Areeb Jamal <jamal.areeb@gmail.com>
2019-10-08 22:52:40 +0530

    Update test_scheduled_jobs.py

--- tests/all/integration/api/helpers/test_scheduled_jobs.py
+++ tests/all/integration/api/helpers/test_scheduled_jobs.py
@@ -14,23 +14,22 @@ class TestScheduledJobs(OpenEventTestCase):
         """Method to test marking of event invoices as due"""
 
         with app.test_request_context():
-            event_invoice = EventInvoiceFactory(event__ends_at=datetime.datetime(2019, 7, 20))
+            event_invoice_new = EventInvoiceFactory(event__ends_at=datetime.datetime(2019, 7, 20))
+            event_invoice_paid = EventInvoiceFactory(status="paid")
+            event_invoice_future = EventInvoiceFactory(created_at=datetime.datetime(2099, 12, 14))
+            
             db.session.commit()
-            event_invoice_id = event_invoice.id
-            status_1 = EventInvoice.query.get(event_invoice_id).status
-
-            event_invoice = EventInvoiceFactory(status="paid")
-            db.session.commit()
-            event_invoice_id = event_invoice.id
-            status_2 = EventInvoice.query.get(event_invoice_id).status
-
-            event_invoice = EventInvoiceFactory()
-            event_invoice.created_at = datetime.datetime(2099, 12, 14)
-            db.session.commit()
-            event_invoice_id = event_invoice.id
-            status_3 = EventInvoice.query.get(event_invoice_id).status
-
+            
+            event_invoice_new_id = event_invoice_new.id
+            event_invoice_paid_id = event_invoice_paid.id
+            event_invoice_future_id = event_invoice_future.id
+            
             event_invoices_mark_due()
-            self.assertEqual(status_1, "due")
-            self.assertNotEqual(status_2, "due")
-            self.assertNotEqual(status_3, "due")
+            
+            status_new = EventInvoice.query.get(event_invoice_new_id).status
+            status_paid = EventInvoice.query.get(event_invoice_paid_id).status
+            status_future = EventInvoice.query.get(event_invoice_future_id).status
+
+            self.assertEqual(status_new, "due")
+            self.assertNotEqual(status_paid, "due")
+            self.assertNotEqual(status_future, "due")
