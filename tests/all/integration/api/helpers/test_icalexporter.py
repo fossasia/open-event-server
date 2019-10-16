7189c6a15b5a1b2d3ff3610f16e9cd6f7c6c2f9a
iamareebjamal <jamal.areeb@gmail.com>
2019-10-17 00:44:57 +0530

    fix

--- tests/all/integration/api/helpers/test_icalexporter.py
+++ tests/all/integration/api/helpers/test_icalexporter.py
@@ -17,8 +17,6 @@ from app.api.helpers.ICalExporter import ICalExporter
 
 
 class TestICalExporter(OpenEventTestCase):
-    def setUp(self):
-        self.app = Setup.create_app()
 
     def test_export_basic(self):
         """Test to export ical format event"""
@@ -33,21 +31,18 @@ class TestICalExporter(OpenEventTestCase):
     def test_export_subcomponents(self):
         """Test to check presence of subcomponents"""
         with app.test_request_context():
-            test_session = SessionFactory()
-            save_to_db(test_session)
+            test_session = SessionFactory(event__identifier='abcd')
 
-            session_obj = Session.query.get(test_session.id)
-            speaker_obj = Speaker(name="xyz", email="test@xyz.com")
-            session_obj.speakers = [speaker_obj]
+            speaker = SpeakerFactory(name="xyz", email="test@xyz.com")
+            test_session.speakers = [speaker]
 
-            icalexport_object = ICalExporter()
-            test_cal_str = icalexport_object.export(test_session.event_id)
-            event_identifier = db.session.query(Event).filter_by(id=test_session.event_id).one().identifier
-            test_cal = icalendar.Calendar.from_ical(test_cal_str)
-            self.assertIn('URL:http://localhost/v1/events?identifier={}'.format(event_identifier),
-                          str(test_cal_str))
-            self.assertIn('LOCATION:example', test_cal.content_lines())
-            self.assertIn('MAILTO:test@xyz.com', test_cal.content_lines())
+            test_cal = icalendar.Calendar.from_ical(ICalExporter().export(test_session.event_id))
+            
+            cal_content_lines = test_cal.content_lines()
+            self.assertIn('URL:http://localhost/v1/events?identifier={}'.format(test_session.event.identifier),
+                          cal_content_lines)
+            self.assertIn('LOCATION:example', cal_content_lines)
+            self.assertIn('ATTENDEE;CN=xyz:MAILTO:test@xyz.com', cal_content_lines)
 
 
 if __name__ == '__main__':
