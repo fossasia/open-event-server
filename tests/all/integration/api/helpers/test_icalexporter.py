9899fb3d5d018710ca0557ee4d8f1ea0cab062d2
iamareebjamal <jamal.areeb@gmail.com>
2019-10-17 00:48:53 +0530

    fix

--- tests/all/integration/api/helpers/test_icalexporter.py
+++ tests/all/integration/api/helpers/test_icalexporter.py
@@ -2,17 +2,10 @@ import unittest
 import icalendar
 from app import current_app as app
 
-from tests.all.integration.setup_database import Setup
-from app.api.helpers.db import save_to_db
-from app.models import db
-from app.models.event import Event
-from app.models.speaker import Speaker
-from app.models.session import Session
 
 from tests.all.integration.utils import OpenEventTestCase
 from app.factories.session import SessionFactory
 from app.factories.speaker import SpeakerFactory
-from app.factories.event import EventFactoryBasic
 from app.api.helpers.ICalExporter import ICalExporter
 
 
@@ -31,9 +24,9 @@ class TestICalExporter(OpenEventTestCase):
     def test_export_subcomponents(self):
         """Test to check presence of subcomponents"""
         with app.test_request_context():
-            test_session = SessionFactory(event__identifier='abcd')
+            test_session = SessionFactory()
 
-            speaker = SpeakerFactory(name="xyz", email="test@xyz.com")
+            speaker = SpeakerFactory(name="xyz", email="test@xyz.com", user_id=1)
             test_session.speakers = [speaker]
 
             test_cal = icalendar.Calendar.from_ical(ICalExporter().export(test_session.event_id))
