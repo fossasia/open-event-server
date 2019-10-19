63a4f89bec819f703ab73778197351159bfcce3a
lladhibhutall <debjyoti.biswas@outlook.com>
2019-10-19 09:06:46 +0530

    Move the register blueprints to original place

--- app/__init__.py
+++ app/__init__.py
@@ -155,10 +155,11 @@ def create_app():
         app.register_blueprint(event_copy)
         app.register_blueprint(upload_routes)
         app.register_blueprint(export_routes)
+        app.register_blueprint(import_routes)
         app.register_blueprint(celery_routes)
         app.register_blueprint(auth_routes)
-        app.register_blueprint(event_statistics)
         app.register_blueprint(info_route)
+        app.register_blueprint(event_statistics)
         app.register_blueprint(user_misc_routes)
         app.register_blueprint(attendee_misc_routes)
         app.register_blueprint(order_misc_routes)
@@ -168,7 +169,6 @@ def create_app():
         app.register_blueprint(admin_blueprint)
         app.register_blueprint(alipay_blueprint)
         app.register_blueprint(admin_misc_routes)
-        app.register_blueprint(import_routes)
 
         add_engine_pidguard(db.engine)
 
