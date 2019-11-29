a20ac00552863abc393effe3900c0dfcc0c97b7a
Prateek Jain <prateekj1171998@gmail.com>
2019-11-29 17:19:15 +0530

    Create attendee using custom orders API

--- app/api/custom/orders.py
+++ app/api/custom/orders.py
@@ -3,20 +3,22 @@ from flask_jwt_extended import current_user, jwt_required
 from flask_limiter.util import get_remote_address
 from sqlalchemy.orm.exc import NoResultFound
 
-
 from app import limiter
 from app.models import db
 from app.api.auth import return_file
-from app.api.helpers.db import safe_query
+from app.api.helpers.db import safe_query, get_count
 from app.api.helpers.mail import send_email_to_attendees
 from app.api.helpers.errors import ForbiddenError, UnprocessableEntityError, NotFoundError
 from app.api.helpers.order import calculate_order_amount, create_pdf_tickets_for_holder
 from app.api.helpers.storage import UPLOAD_PATHS
 from app.api.helpers.storage import generate_hash
 from app.api.helpers.ticketing import TicketingManager
+from app.api.schema.attendees import AttendeeSchema
 from app.api.helpers.permission_manager import has_access
 from app.models.discount_code import DiscountCode
 from app.models.order import Order
+from app.models.ticket import Ticket
+from app.models.ticket_holder import TicketHolder
 
 order_blueprint = Blueprint('order_blueprint', __name__, url_prefix='/v1/orders')
 ticket_blueprint = Blueprint('ticket_blueprint', __name__, url_prefix='/v1/tickets')
@@ -93,3 +95,34 @@ def calculate_amount():
             return UnprocessableEntityError({'source': 'discount-code'}, 'Discount Usage Exceeded').respond()
 
     return jsonify(calculate_order_amount(tickets, discount_code))
+
+
+@order_blueprint.route('/create-order', methods=['POST'])
+@jwt_required
+def create_order():
+    data = request.get_json()
+    tickets = data['tickets']
+    attendee = data['attendee']
+    for attribute in attendee:
+        attendee[attribute.replace('-', '_')] = attendee.pop(attribute)
+    schema = AttendeeSchema()
+    json_api_attendee = {"data": {"attributes": data['attendee'], "type": "attendee"}}
+    result = schema.load(json_api_attendee)
+    if result.errors:
+        return jsonify(result.errors)
+    for ticket in tickets:
+        ticket_info = db.session.query(Ticket).filter_by(id=int(ticket['id']), deleted_at=None).first()
+        if ticket_info is None:
+            return jsonify(status='Order Unsuccessful', error='Ticket with id {} was not found.'.format(ticket['id']))
+        if ticket_info.event_id != int(data['event_id']):
+            return jsonify(status='Order Unsuccessful',
+                           error="Ticket with id {} belongs to a different Event.".format(ticket['id']))
+        if get_count(db.session.query(TicketHolder.id).filter_by(ticket_id=int(ticket['id']), deleted_at=None)) >= ticket['quantity']:
+            return jsonify(status='Order Unsuccessful', error='Ticket already sold out.')
+    for ticket in tickets:
+        for ticket_amount in range(ticket['quantity']):
+            attendee = TicketHolder(**result[0], event_id=int(data['event_id']), ticket_id=int(ticket['id']))
+            db.session.add(attendee)
+            db.session.commit()
+
+    return jsonify("Worked !!")
