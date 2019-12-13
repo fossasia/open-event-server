720598e755956d9b33f9d90cf09ecb76b6e544b8
iamareebjamal <jamal.areeb@gmail.com>
2019-12-13 05:36:45 +0530

    fix

--- app/api/schema/__init__.py
+++ app/api/schema/__init__.py
@@ -4,7 +4,7 @@ from marshmallow_jsonapi.flask import Relationship
 
 def serialize(self, attr, obj, accessor=None):
     if self.include_resource_linkage or self.include_data:
-        return super().serialize(attr, obj, accessor)
+        return super(Relationship, self).serialize(attr, obj, accessor)
     return self._serialize(None, attr, obj)
 
 
