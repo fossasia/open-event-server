24c571a1527500472d9e739c8e01811c9b262f7c
Travis CI <noreply+travis@fossasia.org>
2020-01-18 13:32:15 +0530

    chore(refactor): Refactor code for custom attendee route

--- app/api/attendees.py
+++ app/api/attendees.py
@@ -1,10 +1,7 @@
 import datetime
 
-from flask import Blueprint, request, jsonify, abort, make_response
 from flask_jwt_extended import current_user
 from flask_rest_jsonapi import ResourceDetail, ResourceList, ResourceRelationship
-from flask_rest_jsonapi.exceptions import ObjectNotFound
-from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy import or_, and_
 
 from app.api.bootstrap import api
@@ -14,7 +11,6 @@ from app.api.helpers.exceptions import (
     ForbiddenException,
     UnprocessableEntity,
 )
-from app.api.helpers.mail import send_email_to_attendees
 from app.api.helpers.permission_manager import has_access
 from app.api.helpers.permissions import jwt_required
 from app.api.helpers.query import event_query
@@ -27,8 +23,6 @@ from app.models.ticket_holder import TicketHolder
 from app.models.user import User
 
 from app.settings import get_settings
-
-attendee_misc_routes = Blueprint('attendee_misc', __name__, url_prefix='/v1')
 
 
 def get_sold_and_reserved_tickets_count(event_id):
@@ -275,34 +269,3 @@ class AttendeeRelationshipOptional(ResourceRelationship):
     schema = AttendeeSchema
     data_layer = {'session': db.session,
                   'model': TicketHolder}
-
-
-@attendee_misc_routes.route('/attendees/send-receipt', methods=['POST'])
-@jwt_required
-def send_receipt():
-    """
-    Send receipts to attendees related to the provided order.
-    :return:
-    """
-    order_identifier = request.json.get('order-identifier')
-    if order_identifier:
-        try:
-            order = db.session.query(Order).filter_by(identifier=order_identifier).one()
-        except NoResultFound:
-            raise ObjectNotFound({'parameter': '{identifier}'}, "Order not found")
-
-        if (order.user_id != current_user.id) and (not has_access('is_registrar', event_id=order.event_id)):
-            abort(
-                make_response(jsonify(error="You need to be the event organizer or order buyer to send receipts."), 403)
-            )
-        elif order.status != 'completed':
-            abort(
-                make_response(jsonify(error="Cannot send receipt for an incomplete order"), 409)
-            )
-        else:
-            send_email_to_attendees(order, current_user.id)
-            return jsonify(message="receipt sent to attendees")
-    else:
-        abort(
-            make_response(jsonify(error="Order identifier missing"), 422)
-        )
---
+++ app/api/custom/attendees.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, request, jsonify, abort, make_response
+from flask_jwt_extended import current_user
+from sqlalchemy.orm.exc import NoResultFound
+
+from app.api.helpers.mail import send_email_to_attendees
+from app.api.helpers.permissions import jwt_required
+from app.api.helpers.errors import (
+    UnprocessableEntityError,
+    NotFoundError,
+    ForbiddenError,
+)
+from app.api.helpers.permission_manager import has_access
+
+from app.models.order import Order
+from app.models import db
+
+attendee_blueprint = Blueprint('attendee_blueprint', __name__, url_prefix='/v1')
+
+
+@attendee_blueprint.route('/attendees/send-receipt', methods=['POST'])
+@jwt_required
+def send_receipt():
+    """
+    Send receipts to attendees related to the provided order.
+    :return:
+    """
+    order_identifier = request.json.get('order-identifier')
+    if order_identifier:
+        try:
+            order = db.session.query(Order).filter_by(identifier=order_identifier).one()
+        except NoResultFound:
+            return NotFoundError({'parameter': '{order_identifier}'}, "Order not found").respond()
+
+        if (order.user_id != current_user.id) and (not has_access('is_registrar', event_id=order.event_id)):
+            return ForbiddenError({'source': ''},
+                                  'You need to be the event organizer or order buyer to send receipts.').respond()
+        elif order.status != 'completed':
+            abort(
+                make_response(jsonify(error="Cannot send receipt for an incomplete order"), 409)
+            )
+        else:
+            send_email_to_attendees(order, current_user.id)
+            return jsonify(message="receipt sent to attendees")
+    else:
+        return UnprocessableEntityError({'source': ''}, 'Order identifier missing').respond()
