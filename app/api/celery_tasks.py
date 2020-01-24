ad327b8cd53ffc6ecc2a40b6a1765e3be0820867
Areeb Jamal <jamal.areeb@gmail.com>
2020-01-25 00:46:48 +0530

    fix: Remaining import of celery instance (#6783)

--- app/api/celery_tasks.py
+++ app/api/celery_tasks.py
@@ -20,7 +20,7 @@ def celery_task(task_id):
         state = TASK_RESULTS[task_id]['state']
         info = TASK_RESULTS[task_id]['result']
     else:
-        from app.views.celery_ import celery
+        from app.api.helpers.tasks import celery
         result = AsyncResult(id=task_id, app=celery)
         state = result.state
         info = result.info
