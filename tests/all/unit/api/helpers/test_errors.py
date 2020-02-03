9519fb0caacf32a7eeaae397a1b6d359f50b3275
diru1100 <rohitdandamudi.1100@gmail.com>
2020-02-03 16:34:30 +0530

    changed test_errors to pytest

--- tests/all/unit/api/helpers/test_errors.py
+++ tests/all/unit/api/helpers/test_errors.py
@@ -1,5 +1,4 @@
-from unittest import TestCase
-
+import pytest
 from app.api.helpers.errors import (
     BadRequestError,
     ErrorResponse,
@@ -9,43 +8,32 @@ from app.api.helpers.errors import (
     UnprocessableEntityError,
 )
 
+"""Test for error responses"""
 
-class TestErrorDetails(TestCase):
-    """Test for error responses"""
 
-    def test_error_response_dict_details(self):
-        """To test details in the form of dict"""
+def test_error_response_dict_details():
+    """To test details in the form of dict"""
+    error_response = ErrorResponse(source="test source", detail="test detail")
+    expected_dict = {
+        'status': error_response.status,
+        'source': error_response.source,
+        'title': error_response.title,
+        'detail': error_response.detail,
+    }
+    assert error_response.to_dict() == expected_dict
 
-        error_response = ErrorResponse(source="test source", detail="test detail")
-        expected_dict = {
-            'status': error_response.status,
-            'source': error_response.source,
-            'title': error_response.title,
-            'detail': error_response.detail,
-        }
-        self.assertEqual(error_response.to_dict(), expected_dict)
 
-    def test_errors(self):
-        """Method to test the status code of all errors"""
-
-        # Forbidden Error
-        forbidden_error = ForbiddenError({'source': ''}, 'Super admin access is required')
-        self.assertEqual(forbidden_error.status, 403)
-
-        # Not Found Error
-        not_found_error = NotFoundError({'source': ''}, 'Object not found.')
-        self.assertEqual(not_found_error.status, 404)
-
-        # Server Error
-        server_error = ServerError({'source': ''}, 'Internal Server Error')
-        self.assertEqual(server_error.status, 500)
-
-        # UnprocessableEntity Error
-        unprocessable_entity_error = UnprocessableEntityError(
-            {'source': ''}, 'Entity cannot be processed'
-        )
-        self.assertEqual(unprocessable_entity_error.status, 422)
-
-        # Bad Request Error
-        bad_request_error = BadRequestError({'source': ''}, 'Request cannot be served')
-        self.assertEqual(bad_request_error.status, 400)
+def test_errors():
+    """Method to test the status code of all errors"""
+    forbidden_error = ForbiddenError({'source': ''}, 'Super admin access is required')
+    assert forbidden_error.status == 403
+    not_found_error = NotFoundError({'source': ''}, 'Object not found.')
+    assert not_found_error.status == 404
+    server_error = ServerError({'source': ''}, 'Internal Server Error')
+    assert server_error.status == 500
+    unprocessable_entity_error = UnprocessableEntityError(
+        {'source': ''}, 'Entity cannot be processed'
+    )
+    assert unprocessable_entity_error.status == 422
+    bad_request_error = BadRequestError({'source': ''}, 'Request cannot be served')
+    assert bad_request_error.status == 400
