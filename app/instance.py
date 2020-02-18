b28b9bf423e1a1b406dc9cefc5932f18c537823f
rits1272 <ritikjain1272@gmail.com>
2020-02-18 14:11:03 +0530

    update

--- app/instance.py
+++ app/instance.py
@@ -43,7 +43,6 @@ from app.views.healthcheck import (
     health_check_migrations,
 )
 from app.views.redis_store import redis_store
-from app.api.helpers.errors import ErrorResponse
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -283,8 +282,7 @@ def ratelimit_handler(error):
 
 @app.errorhandler(JsonApiException)
 def handle_exception(error):
-    return ErrorResponse({'source': error.source}, status=error.status,
-                         title=error.title, detail=error.detail).respond()
+    return error.to_dict()
 
 
 if __name__ == '__main__':
