ec7f088eb6e8caca6cbfce041a0f2bc3395c6862
satya7289 <satyaprakashaman60@gmail.com>
2020-05-17 12:59:00 +0530

    move the function

--- app/api/helpers/db.py
+++ app/api/helpers/db.py
@@ -37,6 +37,23 @@ def safe_query_by(model, value, param='id'):
     return safe_query_without_soft_deleted_entries(model, param, value, param)
 
 
+def safe_query_kwargs(model, kwargs, parameter_name):
+    """
+    :param model: db Model to be queried
+    :param kwargs: it contains parameter_name's value eg kwargs['event_id']
+                where parameter_name='event_id'
+    :param parameter_name: Name of parameter to be printed in json-api error
+                message eg 'event_id'
+    :return:
+    """
+    return safe_query(
+        model,
+        'id',
+        kwargs[parameter_name],
+        parameter_name,
+    )
+
+
 def safe_query_without_soft_deleted_entries(
     model, column_name, value, parameter_name, filter_deleted=True
 ):
@@ -76,25 +93,6 @@ def safe_query(model, column_name, value, parameter_name):
         model,
         column_name,
         value,
-        parameter_name,
-        # TODO(Areeb): Check that only admin can pass this parameter
-        request.args.get('get_trashed') != 'true',
-    )
-
-
-def safe_query_kwargs(model, kwargs, parameter_name):
-    """
-    :param model: db Model to be queried
-    :param kwargs: it contains parameter_name's value eg kwargs['event_id']
-                where parameter_name='event_id'
-    :param parameter_name: Name of parameter to be printed in json-api error
-                message eg 'event_id'
-    :return:
-    """
-    return safe_query_without_soft_deleted_entries(
-        model,
-        'id',
-        kwargs[parameter_name],
         parameter_name,
         # TODO(Areeb): Check that only admin can pass this parameter
         request.args.get('get_trashed') != 'true',
