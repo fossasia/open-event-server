e4290e489624f153321cb68f78b83d1bd6e10bae
mansiag <mansi.29ag@gmail.com>
2020-05-21 14:54:49 +0530

    Fix issues

--- app/api/helpers/humanize_helper.py
+++ app/api/helpers/humanize_helper.py
@@ -7,5 +7,4 @@ import pytz
 def humanize_helper(time):
     """Returns time passed from now in a human readable duration"""
 
-    return humanize.naturaldelta(datetime.now(pytz.utc) 
-                - time.astimezone(pytz.utc)) + ' ago'
+    return humanize.naturaltime(datetime.now(pytz.utc) - time.astimezone(pytz.utc))
--- tests/all/integration/api/helpers/test_humanize_helper.py
+++ tests/all/integration/api/helpers/test_humanize_helper.py
@@ -4,13 +4,12 @@ from app.api.helpers.humanize_helper import humanize_helper
 from tests.factories.order import OrderFactory
 
 
-class TestHumanizeHelper(OpenEventTestCase): 
-    def test_humanize_helper(self):
-        """Method to test humanization of order creation time"""
+def test_humanize_helper(db):
+    """Method to test humanization of order creation time"""
 
-        with self.app.test_request_context():
-            test_order = OrderFactory(created_at=datetime.datetime.now() 
-                                        - datetime.timedelta(days=10))
-            actual_response = humanize_helper(test_order.created_at)
-            expected_response = '10 days ago'
-            self.assertEqual(actual_response, expected_response)
+    test_order = OrderFactory(
+        created_at=datetime.datetime.now() - datetime.timedelta(days=10)
+    )
+    actual_response = humanize_helper(test_order.created_at)
+    expected_response = '10 days ago'
+    assert actual_response == expected_response
