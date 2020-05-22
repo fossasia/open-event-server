18a19fa89149b1b05fbe6de7ff2341a48c4e4e29
Mansi Agrawal <mansi.29ag@gmail.com>
2020-05-22 15:07:38 +0530

    chore(refactor): Remove arrow dependency from the project (#7022)

---
+++ tests/all/integration/api/helpers/test_humanize_helper.py
@@ -0,0 +1,19 @@
+import datetime
+
+from app.templates.flask_ext.jinja.filters import humanize_helper
+from tests.factories.order import OrderFactory
+
+
+def test_humanize_helper(db):
+    """Method to test humanization of order creation time"""
+
+    test_order = OrderFactory(
+        created_at=datetime.datetime.now() - datetime.timedelta(days=10)
+    )
+    actual_response = humanize_helper(test_order.created_at)
+    expected_response = '10 days ago'
+    assert actual_response == expected_response
+
+    actual_response = humanize_helper(test_order.completed_at)
+    expected_response = 'N/A'
+    assert actual_response == expected_response
