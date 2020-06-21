14feb7eefd064147e4b2bc9d538aa62204284d5c
iamareebjamal <jamal.areeb@gmail.com>
2020-06-21 09:41:00 +0530

    test: Add update custom form test
    Also rename test file so pytest registers it

--- tests/all/integration/models/test_custom_forms.py
+++ tests/all/integration/models/test_custom_forms.py
@@ -42,6 +42,25 @@ def test_override_custom_form_name(db):
     assert custom_custom_form.name == 'Portable Number'
 
 
+def test_edit_custom_form(db):
+    event = EventFactoryBasic()
+    overridden_custom_form = CustomForms(
+        event=event,
+        field_identifier='shippingAddress',
+        name='Home Address',
+        form='attendee',
+        type='text',
+    )
+    db.session.commit()
+
+    assert overridden_custom_form.name == 'Shipping Address'
+
+    overridden_custom_form.name = 'Daddy Address'
+    db.session.commit()
+
+    assert overridden_custom_form.name == 'Shipping Address'
+
+
 def test_assigning_old_form_name(db):
     event = EventFactoryBasic()
     speaker_custom_form = CustomForms(
@@ -54,14 +73,14 @@ def test_assigning_old_form_name(db):
         event=event, field_identifier='longAbstract', form='session', type='text'
     )
     none_custom_form = CustomForms(
-        event=event, field_identifier='showNumber', form='attendee', type='text'
+        event=event, field_identifier='showNumber', name='Show Number', form='attendee', type='text'
     )
     db.session.commit()
 
     assert speaker_custom_form.name == 'Speaking Experience'
-    assert none_custom_form.name is None
+    assert none_custom_form.name == 'Show Number'
 
-    db.session.execute('update custom_forms set name = null')
+    db.session.execute("update custom_forms set name = 'None'")
     db.session.commit()
 
     db.session.refresh(speaker_custom_form)
@@ -69,10 +88,10 @@ def test_assigning_old_form_name(db):
     db.session.refresh(session_custom_form)
     db.session.refresh(none_custom_form)
 
-    assert speaker_custom_form.name is None
-    assert attendee_custom_form.name is None
-    assert session_custom_form.name is None
-    assert none_custom_form.name is None
+    assert speaker_custom_form.name == 'None'
+    assert attendee_custom_form.name == 'None'
+    assert session_custom_form.name == 'None'
+    assert none_custom_form.name == 'None'
 
     assign_field_names(db.session)
 
@@ -84,4 +103,5 @@ def test_assigning_old_form_name(db):
     assert speaker_custom_form.name == 'Speaking Experience'
     assert attendee_custom_form.name == 'Tax Business Info'
     assert session_custom_form.name == 'Long Abstract'
-    assert none_custom_form.name is None
+    # cannot restore custom form field name
+    assert none_custom_form.name == 'None'
