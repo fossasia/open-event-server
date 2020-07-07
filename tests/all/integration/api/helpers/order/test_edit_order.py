95388438857eb080ebdd46228f23e375fbc0b3e9
Haider Ali <haider.lee23@gmail.com>
2020-07-07 16:37:51 +0530

    Add placed failure test and remove simple custom form test

--- tests/all/integration/api/helpers/order/test_edit_order.py
+++ tests/all/integration/api/helpers/order/test_edit_order.py
@@ -94,33 +94,6 @@ def test_ignore_on_order_event_update(client, db, user, jwt):
     assert order.event == order_event
 
 
-def get_simple_custom_form_order(db, user):
-    order = OrderSubFactory(amount=234, status='initializing', user=user)
-    attendee = AttendeeSubFactory(order=order)
-
-    CustomForms(
-        event=attendee.event,
-        form='attendee',
-        field_identifier='jobTitle',
-        type='text',
-        is_included=True,
-        is_required=True,
-    )
-
-    CustomForms(
-        event=attendee.event,
-        form='attendee',
-        field_identifier='taxBusinessInfo',
-        type='text',
-        is_included=True,
-        is_required=True,
-    )
-
-    db.session.commit()
-
-    return str(order.id)
-
-
 def get_complex_custom_form_order(db, user):
     order = OrderSubFactory(amount=234, status='initializing', user=user)
     attendee = AttendeeSubFactory(order=order)
@@ -163,45 +136,6 @@ def get_complex_custom_form_order(db, user):
     return str(order.id)
 
 
-def test_order_pending_incomplete_simple_custom_form(client, db, user, jwt):
-    order_id = get_simple_custom_form_order(db, user)
-    order = Order.query.get(order_id)
-
-    data = json.dumps(
-        {
-            "data": {
-                "attributes": {"status": "pending", "order-notes": "take me to pending"},
-                "type": "order",
-                "id": order_id,
-            }
-        }
-    )
-
-    response = client.patch(
-        f'/v1/orders/{order_id}',
-        content_type='application/vnd.api+json',
-        headers=jwt,
-        data=data,
-    )
-
-    db.session.refresh(order)
-
-    assert response.status_code == 422
-    assert order.status != "pending"
-    assert order.order_notes != "take me to pending"
-    assert json.loads(response.data) == {
-        'errors': [
-            {
-                'status': 422,
-                'source': {'pointer': '/data/attributes'},
-                'title': 'Unprocessable Entity',
-                'detail': "Missing required fields ['job_title', 'tax_business_info']",
-            }
-        ],
-        'jsonapi': {'version': '1.0'},
-    }
-
-
 def test_order_pending_incomplete_complex_custom_form(client, db, user, jwt):
     order_id = get_complex_custom_form_order(db, user)
     order = Order.query.get(order_id)
@@ -228,6 +162,45 @@ def test_order_pending_incomplete_complex_custom_form(client, db, user, jwt):
     assert response.status_code == 422
     assert order.status != "pending"
     assert order.order_notes != "do it pending"
+    assert json.loads(response.data) == {
+        'errors': [
+            {
+                'status': 422,
+                'source': {'pointer': '/data/attributes'},
+                'title': 'Unprocessable Entity',
+                'detail': "Missing required fields ['what_college', 'what_university']",
+            }
+        ],
+        'jsonapi': {'version': '1.0'},
+    }
+
+
+def test_order_placed_incomplete_complex_custom_form(client, db, user, jwt):
+    order_id = get_complex_custom_form_order(db, user)
+    order = Order.query.get(order_id)
+
+    data = json.dumps(
+        {
+            "data": {
+                "attributes": {"status": "placed", "order-notes": "just place it"},
+                "type": "order",
+                "id": order_id,
+            }
+        }
+    )
+
+    response = client.patch(
+        f'/v1/orders/{order_id}',
+        content_type='application/vnd.api+json',
+        headers=jwt,
+        data=data,
+    )
+
+    db.session.refresh(order)
+
+    assert response.status_code == 422
+    assert order.status != "placed"
+    assert order.order_notes != "just place it"
     assert json.loads(response.data) == {
         'errors': [
             {
