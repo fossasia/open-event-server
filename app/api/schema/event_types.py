0d0a90246817fde73b759efe19fc58491f1ce780
Ranjit1312 <jadhawar.ranjit@gmail.com>
2020-08-12 18:13:10 +0530

    removed duplicate code for event_type Schema

--- app/api/schema/event_types.py
+++ app/api/schema/event_types.py
@@ -2,10 +2,14 @@ from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from app.models.event_type import EventType
 
 
+
 class AuthorSchema(SQLAlchemyAutoSchema):
 
+    """
+    Using SQLAlchemyAutoSchema of marshmallow_sqlalchemy
+    """
     class Meta:
         model = EventType
         include_relationships = True
         include_fk = True
-        load_instance = True  # Optional: deserialize to model instances
+        load_instance = True  """ Optional: deserialize to model instances"""
