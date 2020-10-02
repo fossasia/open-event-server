ca2243e651d7bb15481f0bac9f3550b681b98b3d
Areeb Jamal <jamal.areeb@gmail.com>
2020-10-02 07:47:39 +0530

    fix: Invoice identifier and generation behaviour change (#7314)

--- app/models/event_invoice.py
+++ app/models/event_invoice.py
@@ -62,11 +62,15 @@ class EventInvoice(SoftDeletionModel):
             self.identifier = self.get_new_id()
 
     def __repr__(self):
-        return '<EventInvoice %r>' % self.invoice_pdf_url
+        return '<EventInvoice %r %r %r>' % (
+            self.id,
+            self.identifier,
+            self.invoice_pdf_url,
+        )
 
     def get_new_id(self) -> str:
         with db.session.no_autoflush:
-            identifier = self.issued_at.strftime('%Y%mU-') + str(
+            identifier = self.issued_at.strftime('%Y%mU-') + '%06d' % (
                 EventInvoice.query.count() + 1
             )
             count = EventInvoice.query.filter_by(identifier=identifier).count()
@@ -105,9 +109,7 @@ class EventInvoice(SoftDeletionModel):
                 or TicketFees.query.filter_by(country='global').first()
             )
             if not ticket_fee_object:
-                logger.error(
-                    'Ticket Fee not found for event id {id}'.format(id=self.event.id)
-                )
+                logger.error('Ticket Fee not found for event %s', self.event)
                 return
 
             ticket_fee_percentage = ticket_fee_object.service_fee
@@ -119,6 +121,12 @@ class EventInvoice(SoftDeletionModel):
             if invoice_amount > ticket_fee_maximum:
                 invoice_amount = ticket_fee_maximum
             self.amount = round_money(invoice_amount)
+            if self.amount == 0:
+                logger.warning(
+                    'Invoice amount of Event %s is 0, hence skipping generation',
+                    self.event,
+                )
+                return
             net_revenue = round_money(gross_revenue - invoice_amount)
             orders_query = self.event.get_orders_query(
                 start=latest_invoice_date, end=self.issued_at
