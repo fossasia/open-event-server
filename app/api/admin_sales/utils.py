1db9f34b356f5cc5d91478b98ef0fbaba0198196
iamareebjamal <jamal.areeb@gmail.com>
2020-10-05 17:50:49 +0530

    fix

--- app/api/admin_sales/utils.py
+++ app/api/admin_sales/utils.py
@@ -4,9 +4,9 @@ admin section
 """
 from sqlalchemy import func
 
+from app.api.helpers.db import get_count
 from app.models.order import Order
 from app.models.ticket_holder import TicketHolder
-from app.api.helpers.db import get_count
 
 
 def status_summary(event, status):
@@ -14,8 +14,17 @@ def status_summary(event, status):
     Groups orders by status and returns the total sales and ticket count as a
     dictionary
     """
-    sales = Order.query.filter_by(event_id=event.id, status=status).with_entities(func.sum(Order.amount)).scalar() or 0
-    tickets = get_count(TicketHolder.query.join(Order).filter(Order.event_id == event.id, Order.status == status))
+    sales = (
+        Order.query.filter_by(event_id=event.id, status=status)
+        .with_entities(func.sum(Order.amount))
+        .scalar()
+        or 0
+    )
+    tickets = get_count(
+        TicketHolder.query.join(Order).filter(
+            Order.event_id == event.id, Order.status == status
+        )
+    )
     return {
         'sales_total': sales,
         'ticket_count': tickets,
