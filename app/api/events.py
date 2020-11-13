8b4e7c3f34c68c976cd401c346e3c314d5ae75ad
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2020-11-13 09:40:27 +0530

    fix: if ticket is not available for sale ,don't show event

--- app/api/events.py
+++ app/api/events.py
@@ -858,7 +858,7 @@ class UpcomingEventList(EventList):
                         Event.event_type_id != None,
                         Event.event_topic_id != None,
                         Event.event_sub_topic_id != None,
-                        Event.tickets.any(and_(Ticket.deleted_at == None, Ticket.is_hidden == False, Ticket.sales_ends_at > current_time)),
+                        Event.tickets.any(and_(Ticket.deleted_at == None, Ticket.is_hidden == False, Ticket.sales_ends_at > current_time, Ticket.is_available == True)),
                     ),
                 ),
             )
