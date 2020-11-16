1c66b63ea389db5cd6a14290db4705ef0b95a6d8
Schin Chauhan <sachinchauhan2889@gmail.com>
2020-11-16 12:51:30 +0530

    add-weblate

---
+++ migrations/versions/rev-2020-11-16-11:42:09-31a3b9375794_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 31a3b9375794
+Revises: aa9daed401f7
+Create Date: 2020-11-16 11:42:09.670115
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+import sqlalchemy_utils
+
+
+# revision identifiers, used by Alembic.
+revision = '31a3b9375794'
+down_revision = 'aa9daed401f7'
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
