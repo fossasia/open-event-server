4ebf12cce8954a4f59783bdd95cea1b278c701b9
Schin Chauhan <sachinchauhan2889@gmail.com>
2020-11-17 15:56:37 +0530

    fix-5010

--- app/api/schema/event_statistics.py
+++ app/api/schema/event_statistics.py
@@ -31,6 +31,8 @@ class EventStatisticsGeneralSchema(Schema):
     sessions_confirmed = fields.Method("sessions_confirmed_count")
     sessions_pending = fields.Method("sessions_pending_count")
     sessions_rejected = fields.Method("sessions_rejected_count")
+    sessions_withdrawn = fields.Method("sessions_withdrawn_count")
+    sessions_canceled = fields.Method("sessions_canceled_count")
     speakers = fields.Method("speakers_count")
     sessions = fields.Method("sessions_count")
     sponsors = fields.Method("sponsors_count")
@@ -63,6 +65,16 @@ class EventStatisticsGeneralSchema(Schema):
             event_id=obj.id, state='rejected', deleted_at=None
         ).count()
 
+    def sessions_withdrawn_count(self, obj):
+        return Session.query.filter_by(
+            event_id=obj.id, state='withdrawn', deleted_at=None
+        ).count()
+
+    def sessions_canceled_count(self, obj):
+        return Session.query.filter_by(
+            event_id=obj.id, state='canceled', deleted_at=None
+        ).count()
+
     def speakers_count_type(self, obj, state='pending'):
         return SessionsSpeakersLink.query.filter_by(
             event_id=obj.id, session_state=state, deleted_at=None
@@ -73,12 +85,16 @@ class EventStatisticsGeneralSchema(Schema):
         confirmed = self.speakers_count_type(obj=obj, state='confirmed')
         pending = self.speakers_count_type(obj=obj, state='pending')
         rejected = self.speakers_count_type(obj=obj, state='rejected')
+        withdrawn = self.speakers_count_type(obj=obj, state='withdrawn')
+        canceled = self.speakers_count_type(obj=obj, state='canceled')
         total = Speaker.query.filter_by(event_id=obj.id, deleted_at=None).count()
         serial_data = {
             'accepted': accepted,
             'confirmed': confirmed,
             'pending': pending,
             'rejected': rejected,
+            'withdrawn': withdrawn,
+            'canceled': canceled,
             'total': total,
         }
         return serial_data
