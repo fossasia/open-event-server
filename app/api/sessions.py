dcbe4a51170a41381d1d585e955d2193b682df65
sundaram <dubeysundaram057@gmail.com>
2020-12-11 19:47:30 +0530

    test

--- app/api/sessions.py
+++ app/api/sessions.py
@@ -270,7 +270,7 @@ class SessionDetail(ResourceDetail):
 
         new_state = data.get('state')
 
-        if new_state and new_state != session.state and (is_organizer or (session.ends_at and session.ends_at < datetime.now())):
+        if new_state and new_state != session.state and (is_organizer or session.ends_at):
             # State change detected. Verify that state change is allowed
             g.send_email = new_state in [
                 'accepted',
