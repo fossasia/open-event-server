f8710c1bf51b38a62e0e491a188ce2e38cf8acb7
sundaram <dubeysundaram057@gmail.com>
2020-12-11 08:44:03 +0530

    f

--- app/api/sessions.py
+++ app/api/sessions.py
@@ -60,7 +60,8 @@ class SessionListPost(ResourceList):
             raise ForbiddenError({'pointer': ''}, "Sessions are disabled for this Event")
 
         data['complex_field_values'] = validate_custom_form_constraints_request(
-            'session', self.schema, Session(event_id=data['event']), data)
+            'session', self.schema, Session(event_id=data['event']), data
+        )
 
     def after_create_object(self, session, data, view_kwargs):
         """
