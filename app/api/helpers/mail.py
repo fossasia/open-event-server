c9f8ab1c0b51bdc8a1a53b835fe290e7363ee153
Sachin Chauhan <sachinchauhan2889@gmail.com>
2020-12-25 15:58:37 +0530

    Update file

--- app/api/helpers/mail.py
+++ app/api/helpers/mail.py
@@ -110,7 +110,7 @@ def send_email(to, action, subject, html, attachments=None, bcc=None):
         action=action,
         subject=subject,
         message=html,
-        time=datetime.now(),
+        time=datetime.now()
     )
 
     save_to_db(mail, 'Mail Recorded')
