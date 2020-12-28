c3849bd8a195c56d6405302036d7b0aad9b7202c
sundaram <dubeysundaram057@gmail.com>
2020-12-28 11:43:04 +0530

    upgrade..

--- migrations/versions/rev-2020-12-28-11:07:56-7374b18af581_.py
+++ migrations/versions/rev-2020-12-28-11:07:56-7374b18af581_.py
@@ -19,6 +19,6 @@ down_revision = 'b0c55c767022'
 def upgrade():
     op.execute("UPDATE custom_forms SET is_public=True WHERE form='session';")
     op.execute("UPDATE custom_forms SET is_public=True WHERE form='speaker' and (field_identifier='name' or field_identifier='photo' or field_identifier='country' or field_identifier='short_biography' or field_identifier='website' or field_identifier='facebook' or field_identifier='github' or field_identifier='twitter' or field_identifier='linkedin');")
-
+    op.execute("UPDATE custom_forms SET is_public=True WHERE form='attendee';")
 def downgrade():
     pass
