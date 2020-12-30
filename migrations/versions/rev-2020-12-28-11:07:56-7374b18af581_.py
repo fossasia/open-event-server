9c876d2ddf82a08f65648688f819ff73cc9e7a46
sundaram <dubeysundaram057@gmail.com>
2020-12-30 23:55:45 +0530

    -

--- migrations/versions/rev-2020-12-28-11:07:56-7374b18af581_.py
+++ migrations/versions/rev-2020-12-28-11:07:56-7374b18af581_.py
@@ -19,7 +19,9 @@ down_revision = 'b0c55c767022'
 def upgrade():
 	op.execute("UPDATE custom_forms SET is_public=True WHERE form='session' and (field_identifier='title' or field_identifier='short_abstract' or field_identifier='track' or field_identifier='session_type' or field_identifier='language' or field_identifier='slides' or field_identifier='video');")
 	op.execute("UPDATE custom_forms SET is_public=True WHERE form='speaker' and (field_identifier='name' or field_identifier='photo' or field_identifier='country' or field_identifier='short_biography' or field_identifier='website' or field_identifier='facebook' or field_identifier='github' or field_identifier='twitter' or field_identifier='linkedin');")
+	op.execute("UPDATE custom_forms SET name='Organisation' WHERE form='attendee' and (field_identifier='company');")
 
 def downgrade():
 	op.execute("UPDATE custom_forms SET is_public=False WHERE form='session' and (field_identifier='title' or field_identifier='short_abstract' or field_identifier='track' or field_identifier='session_type' or field_identifier='language' or field_identifier='slides' or field_identifier='video');")
 	op.execute("UPDATE custom_forms SET is_public=False WHERE form='speaker' and (field_identifier='name' or field_identifier='photo' or field_identifier='country' or field_identifier='short_biography' or field_identifier='website' or field_identifier='facebook' or field_identifier='github' or field_identifier='twitter' or field_identifier='linkedin');")
+	op.execute("UPDATE custom_forms SET name='Company' WHERE form='attendee' and (field_identifier='company');")
