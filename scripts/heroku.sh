7a11a877e1aa72a63b71136cd2c0d5819815e9f6
Areeb Jamal <jamal.areeb@gmail.com>
2021-01-06 18:40:08 +0530

    chore: Make every command parallel

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-chmod -R 0777 ./static
+chmod -R 0777 ./static &
 ./scripts/l10n.sh generate &
-touch .env
+touch .env &
 celery -A app.instance.celery worker --loglevel=info -c 1 &
 if [ "$APP_CONFIG" = "config.DevelopmentConfig" ]; then
     python manage.py runserver -h 0.0.0.0 -p ${PORT:-8000} --no-reload
