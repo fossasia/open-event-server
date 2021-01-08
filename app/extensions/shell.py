3ce180a272685ad93e677f7690565cb1289eee26
Areeb Jamal <jamal.areeb@gmail.com>
2021-01-08 14:41:59 +0530

    chore: Register all models in shell

--- app/extensions/shell.py
+++ app/extensions/shell.py
@@ -1,25 +1,12 @@
 from app.models import db
-from app.models.event import Event
-from app.models.order import Order
-from app.models.session import Session
-from app.models.setting import Setting
-from app.models.speaker import Speaker
-from app.models.ticket import Ticket
-from app.models.track import Track
-from app.models.user import User
 
 
 def init_app(app):
     @app.shell_context_processor
     def shell_context():
-        return dict(
-            db=db,
-            Event=Event,
-            Session=Session,
-            Ticket=Ticket,
-            Order=Order,
-            Setting=Setting,
-            Speaker=Speaker,
-            Track=Track,
-            User=User,
-        )
+        models = {
+            model.__name__: model
+            for model in list(db.Model._decl_class_registry.values())
+            if getattr(model, '__table__', None) is not None
+        }
+        return dict(db=db, **models)
