586ebc631bfb1cb4cf3c6aff5f552f7c9dc7c15b
Sachin Chauhan <sachinchauhan2889@gmail.com>
2021-01-09 15:45:22 +0530

    string

--- app/api/custom/events.py
+++ app/api/custom/events.py
@@ -11,15 +11,14 @@ events_routes = Blueprint('events_routes', __name__, url_prefix='/v1/events')
 @events_routes.route('/<string:event_identifier>/sessions/dates')
 @jwt_required
 def get_dates(event_identifier):
-
     event=Event.query.filter_by(identifier=event_identifier).first()
     dates = (
-            list(zip(
+            list(map(str, list(zip(
                 *db.session.query(func.date(Session.starts_at))
                 .distinct()
                 .filter(Session.event_id==event.id, Session.starts_at != None, or_(Session.state == 'accepted', Session.state == 'confirmed'))
                 .order_by(asc(func.date(Session.starts_at)))
                 .all()
-            ))[0]
+            ))[0]))
     )
     return jsonify(dates)
