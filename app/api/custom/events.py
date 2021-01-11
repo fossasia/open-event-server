478b3a5868abe3d198f1d51770e3ea6e2402c5e6
Sachin Chauhan <sachinchauhan2889@gmail.com>
2021-01-11 12:07:52 +0530

    fix-identation

--- app/api/custom/events.py
+++ app/api/custom/events.py
@@ -9,22 +9,13 @@ events_routes = Blueprint('events_routes', __name__, url_prefix='/v1/events')
 
 @events_routes.route('/<int:event_id>/sessions/dates')
 def get_dates(event_id):
-    dates = list(
-        map(
-            str,
-            list(
-                zip(
-                    *db.session.query(func.date(Session.starts_at))
-                    .distinct()
-                    .filter(
-                        Session.event_id == event_id,
-                        Session.starts_at != None,
-                        or_(Session.state == 'accepted', Session.state == 'confirmed'),
-                    )
-                    .order_by(asc(func.date(Session.starts_at)))
-                    .all()
-                )
-            )[0],
-        )
+    dates = (
+        list(map(str, list(zip(
+            *db.session.query(func.date(Session.starts_at))
+            .distinct()
+            .filter(Session.event_id==event_id, Session.starts_at != None, or_(Session.state == 'accepted', Session.state == 'confirmed'))
+            .order_by(asc(func.date(Session.starts_at)))
+            .all()
+        ))[0]))
     )
     return jsonify(dates)
