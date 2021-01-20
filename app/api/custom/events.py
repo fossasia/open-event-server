979ced9f1e42a758fbea11aa64d798bc40a1a23b
Schin Chauhan <sachinchauhan2889@gmail.com>
2021-01-21 02:48:14 +0530

    fixes

--- app/api/custom/events.py
+++ app/api/custom/events.py
@@ -1,9 +1,9 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, render_template, request
 from flask_jwt_extended import current_user
 from sqlalchemy import asc, func, or_
 
 from app.api.helpers.mail import send_email
-from app.api.helpers.permissions import jwt_required
+from app.api.helpers.permissions import jwt_required, to_event_id
 from app.models import db
 from app.models.event import Event
 from app.models.mail import CONTACT_ORGANIZERS
@@ -14,38 +14,51 @@ events_routes = Blueprint('events_routes', __name__, url_prefix='/v1/events')
 
 @events_routes.route('/<int:event_id>/sessions/dates')
 def get_dates(event_id):
-    dates = (
-        list(map(str, list(zip(
-            *db.session.query(func.date(Session.starts_at))
-            .distinct()
-            .filter(Session.event_id==event_id, Session.starts_at != None, or_(Session.state == 'accepted', Session.state == 'confirmed'))
-            .order_by(asc(func.date(Session.starts_at)))
-            .all()
-        ))[0]))
+    dates = list(
+        map(
+            str,
+            list(
+                zip(
+                    *db.session.query(func.date(Session.starts_at))
+                    .distinct()
+                    .filter(
+                        Session.event_id == event_id,
+                        Session.starts_at != None,
+                        or_(Session.state == 'accepted', Session.state == 'confirmed'),
+                    )
+                    .order_by(asc(func.date(Session.starts_at)))
+                    .all()
+                )
+            )[0],
+        )
     )
     return jsonify(dates)
 
 
-@events_routes.route('/<int:event_id>/contact-organizer', methods=['POST'])
+@events_routes.route('/<string:event_identifier>/contact-organizer', methods=['POST'])
+@to_event_id
 @jwt_required
 def contact_organizer(event_id):
     event = Event.query.get_or_404(event_id)
-    organizers_emails = set(list(map(lambda x: x.email, event.organizers)) + list(map(lambda x: x.email, event.coorganizers)))
+    organizers_emails = list(
+        set(
+            list(map(lambda x: x.email, event.organizers))
+            + list(map(lambda x: x.email, event.coorganizers))
+        )
+    )
     context = {
         'attendee_name': current_user.fullname,
         'attendee_email': current_user.email,
         'event_name': event.name,
-        'email_copy': request.json.get('email'),
     }
     organizer_mail = (
-        "{attendee_name} ({attendee_email}) has a question for you about your event {event_name} </br>"
+        "{attendee_name} ({attendee_email}) has a question for you about your event {event_name} <br/><br/>"
         + request.json.get('email')
     )
-    attendee_mail = "Hello, you have contacted the organizers of the event {event_name}. Below you find a copy of your email. Organizers have received your message and will follow up with you. This is a system message. Please do not reply to this message. Replies are not monitored. Thank you. <br/> {email_copy}"
     send_email(
         to=event.owner.email,
         action=CONTACT_ORGANIZERS,
-        subject="You are contacted by " + current_user.fullname,
+        subject=event.name + ": Question from " + current_user.fullname,
         html=organizer_mail.format(**context),
         bcc=organizers_emails,
         reply_to=current_user.email,
@@ -53,9 +66,13 @@ def contact_organizer(event_id):
     send_email(
         to=current_user.email,
         action=CONTACT_ORGANIZERS,
-        subject="Organizers are succesfully contacted",
-        html=attendee_mail.format(**context),
+        subject=event.name + ": Organizers are succesfully contacted",
+        html=render_template(
+            'email/organizer_contact_attendee.html',
+            event_name=event.name,
+            email_copy=request.json.get('email'),
+        ),
     )
     return jsonify(
-        message="email sent to organizers",
+        success=True,
     )
