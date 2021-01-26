1efd48ea464990bff62594bf5f704b4ad8d0ed8f
Sneha Mishra <itm2017004@iiita.ac.in>
2021-01-26 15:55:30 +0530

    add tests

--- tests/all/integration/api/helpers/test_events.py
+++ tests/all/integration/api/helpers/test_events.py
@@ -1,7 +1,11 @@
 import unittest
 
 from app.api.helpers.db import get_count, save_to_db
-from app.api.helpers.events import create_custom_forms_for_attendees
+from app.api.helpers.events import (
+    create_custom_forms_for_attendees,
+    create_custom_forms_for_sessions,
+    create_custom_forms_for_speakers,
+)
 from app.models.custom_form import CustomForms
 from tests.all.integration.utils import OpenEventTestCase
 from tests.factories.event import EventFactoryBasic
@@ -15,6 +19,22 @@ class TestEventUtilities(OpenEventTestCase):
             save_to_db(event)
             create_custom_forms_for_attendees(event)
             self.assertEqual(get_count(CustomForms.query), 3)
+
+    def test_should_create_speaker_forms(self):
+        """Method to test custom forms for attendees of an event."""
+        with self.app.test_request_context():
+            event = EventFactoryBasic()
+            save_to_db(event)
+            create_custom_forms_for_speakers(event)
+            self.assertEqual(get_count(CustomForms.query), 2)
+
+    def test_should_create_session_forms(self):
+        """Method to test custom forms for attendees of an event."""
+        with self.app.test_request_context():
+            event = EventFactoryBasic()
+            save_to_db(event)
+            create_custom_forms_for_sessions(event)
+            self.assertEqual(get_count(CustomForms.query), 1)
 
 
 if __name__ == '__main__':
