1b706ad313930e6d1b682dadbc6bd143a1e224ff
sundaram <dubeysundaram057@gmail.com>
2021-01-28 13:41:48 +0530

    fix

--- app/api/schema/access_codes.py
+++ app/api/schema/access_codes.py
@@ -36,7 +36,7 @@ class AccessCodeSchema(SoftDeletionSchema):
             if 'valid_till' not in data:
                 data['valid_till'] = access_code.valid_till
 
-        if data['valid_from'] > data['valid_till']:
+        if data['valid_till'] and data['valid_from'] > data['valid_till']:
             raise UnprocessableEntityError(
                 {'pointer': '/data/attributes/valid-till'},
                 "valid_till should be after valid_from",
@@ -81,8 +81,8 @@ class AccessCodeSchema(SoftDeletionSchema):
 
     min_quantity = fields.Integer(validate=lambda n: n >= 0, allow_none=True)
     max_quantity = fields.Integer(validate=lambda n: n >= 0, allow_none=True)
-    valid_from = fields.DateTime(required=True)
-    valid_till = fields.DateTime(required=True)
+    valid_from = fields.DateTime(required=True, allow_none=True)
+    valid_till = fields.DateTime(required=True, allow_none=True)
     event = Relationship(
         self_view='v1.access_code_event',
         self_view_kwargs={'id': '<id>'},
