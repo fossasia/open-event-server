be7c9c1e4b6d4757e75af618faadf071ff480b62
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2021-01-29 09:23:00 +0530

    Update app/models/discount_code.py
    Co-authored-by: Areeb Jamal <jamal.areeb@gmail.com>

--- app/models/discount_code.py
+++ app/models/discount_code.py
@@ -60,9 +60,7 @@ class DiscountCode(SoftDeletionModel):
 
     @property
     def valid_expire_time(self):
-        if self.valid_till:
-            return self.valid_till
-        return self.event.ends_at
+        return self.valid_till or self.event.ends_at
     
     def get_supported_tickets(self, ticket_ids=None):
         query = Ticket.query.with_parent(self).filter_by(deleted_at=None)
