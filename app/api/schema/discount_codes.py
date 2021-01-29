31adfeb7590edfe24de7ea0529e31e0bf40d3441
maze-runner <dubesundaram99@gmail.com>
2021-01-29 17:50:31 +0530

    fixing discount-code

--- app/api/schema/discount_codes.py
+++ app/api/schema/discount_codes.py
@@ -116,8 +116,7 @@ class DiscountCodeSchemaEvent(DiscountCodeSchemaPublic):
             if 'valid_from' not in data:
                 data['valid_from'] = discount_code.valid_from
 
-            if 'valid_till' not in data:
-                data['valid_till'] = discount_code.valid_expire_time
+            data['valid_till'] = discount_code.valid_expire_time
 
         if data['valid_from'] >= data['valid_till']:
             raise UnprocessableEntityError(
@@ -242,8 +241,7 @@ class DiscountCodeSchemaTicket(DiscountCodeSchemaPublic):
             if 'valid_from' not in data:
                 data['valid_from'] = discount_code.valid_from
 
-            
-            data['valid_till'] = discount_code.valid_till
+            data['valid_till'] = discount_code.valid_expire_time
 
         if data['valid_till'] and data['valid_from'] >= data['valid_till']:
             raise UnprocessableEntityError(
