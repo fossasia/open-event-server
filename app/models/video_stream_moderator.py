e6d3089a969b153cc8a14b6a1f2d7e935defb247
maze-runner <dubesundaram99@gmail.com>
2021-02-04 22:09:01 +0530

    f

--- app/models/video_stream_moderator.py
+++ app/models/video_stream_moderator.py
@@ -3,7 +3,7 @@ from app.models.base import SoftDeletionModel
 
 
 class VideoStreamModerator(SoftDeletionModel):
-    __tablename__ = 'video_stream_moderator'
+    __tablename__ = 'video_stream_moderators'
 
     id = db.Column(db.Integer, primary_key=True)
 
@@ -14,7 +14,7 @@ class VideoStreamModerator(SoftDeletionModel):
         db.Integer, db.ForeignKey('video_streams.id', ondelete='CASCADE'), nullable=False
     )
     user = db.relationship("User")
-    video_stream = db.relationship("VideoStream")
+    video_stream = db.relationship("VideoStream", backref="moderators")
 
     def __repr__(self):
         return f'<UER {self.user!r}:{self.event_id!r}>'
