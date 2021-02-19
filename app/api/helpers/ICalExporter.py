5691d3311a01459205967ed88d50567234696612
Sneha Mishra <itm2017004@iiita.ac.in>
2021-02-19 16:09:37 +0530

    fix test

--- app/api/helpers/ICalExporter.py
+++ app/api/helpers/ICalExporter.py
@@ -4,9 +4,9 @@ from app.models.event import Event
 
 class ICalExporter:
     @staticmethod
-    def export(event_id):
+    def export(event_id, user_id):
         """Takes an event id and returns the event in iCal format"""
 
         event = Event.query.get(event_id)
 
-        return to_ical(event, include_sessions=True)
+        return to_ical(event, user_id, include_sessions=True)
--- tests/all/integration/api/helpers/test_icalexporter.py
+++ tests/all/integration/api/helpers/test_icalexporter.py
@@ -25,29 +25,4 @@ def test_export_basic(db, user):
     assert session['summary'] == 'Gooseberry Muffin'
     assert session['url'] == f'http://eventyay.com/e/asdfgh/session/{test_session.id}'
 
-    assert ICalExporter.export(test_session.event_id) == test_cal_str
-
-
-def test_export_my_schedule(db, user):
-    test_session = SessionSubFactory(
-        title='Gooseberry Muffin',
-        event__name='Hoopa Loopa',
-        event__identifier='asdfgh',
-        event__location_name='Narnia',
-    )
-    db.session.commit()
-    test_cal_str = to_ical(
-        test_session.event, user.id, include_sessions=True, my_schedule=True
-    )
-    test_cal = icalendar.Calendar.from_ical(test_cal_str)
-
-    event = test_cal.subcomponents[0]
-    assert event['summary'] == 'Hoopa Loopa'
-    assert event['url'] == 'http://eventyay.com/e/asdfgh'
-    assert event['location'] == 'Narnia'
-
-    session = test_cal.subcomponents[1]
-    assert session['summary'] == 'Gooseberry Muffin'
-    assert session['url'] == f'http://eventyay.com/e/asdfgh/session/{test_session.id}'
-
-    assert ICalExporter.export(test_session.event_id) == test_cal_str
+    assert ICalExporter.export(test_session.event_id, user.id) == test_cal_str
