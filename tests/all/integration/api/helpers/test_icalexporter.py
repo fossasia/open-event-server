16e1874fe9d18fa83f0bdc724b96888dcf929b0c
Sneha Mishra <itm2017004@iiita.ac.in>
2021-02-19 15:38:50 +0530

    fix test

--- tests/all/integration/api/helpers/test_icalexporter.py
+++ tests/all/integration/api/helpers/test_icalexporter.py
@@ -5,7 +5,7 @@ from app.api.helpers.ICalExporter import ICalExporter
 from tests.factories.session import SessionSubFactory
 
 
-def test_export_basic(db):
+def test_export_basic(db, user):
     test_session = SessionSubFactory(
         title='Gooseberry Muffin',
         event__name='Hoopa Loopa',
@@ -13,7 +13,32 @@ def test_export_basic(db):
         event__location_name='Narnia',
     )
     db.session.commit()
-    test_cal_str = to_ical(test_session.event, include_sessions=True)
+    test_cal_str = to_ical(test_session.event, user.id, include_sessions=True)
+    test_cal = icalendar.Calendar.from_ical(test_cal_str)
+
+    event = test_cal.subcomponents[0]
+    assert event['summary'] == 'Hoopa Loopa'
+    assert event['url'] == 'http://eventyay.com/e/asdfgh'
+    assert event['location'] == 'Narnia'
+
+    session = test_cal.subcomponents[1]
+    assert session['summary'] == 'Gooseberry Muffin'
+    assert session['url'] == f'http://eventyay.com/e/asdfgh/session/{test_session.id}'
+
+    assert ICalExporter.export(test_session.event_id) == test_cal_str
+
+
+def test_export_my_schedule(db, user):
+    test_session = SessionSubFactory(
+        title='Gooseberry Muffin',
+        event__name='Hoopa Loopa',
+        event__identifier='asdfgh',
+        event__location_name='Narnia',
+    )
+    db.session.commit()
+    test_cal_str = to_ical(
+        test_session.event, user.id, include_sessions=True, my_schedule=True
+    )
     test_cal = icalendar.Calendar.from_ical(test_cal_str)
 
     event = test_cal.subcomponents[0]
