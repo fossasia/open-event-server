1a836865bde5c038d8e97e3ccad88e2d1f6fe301
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2021-03-10 00:54:12 +0530

    fix: adding hidden in scheduler for microlocation (#7813)

--- app/models/microlocation.py
+++ app/models/microlocation.py
@@ -11,6 +11,7 @@ class Microlocation(SoftDeletionModel):
     latitude = db.Column(db.Float)
     longitude = db.Column(db.Float)
     floor = db.Column(db.Integer)
+    hidden_in_scheduler = db.Column(db.Boolean, default=False, nullable=False)
     position = db.Column(db.Integer, default=0, nullable=False)
     room = db.Column(db.String)
     session = db.relationship('Session', backref="microlocation")
