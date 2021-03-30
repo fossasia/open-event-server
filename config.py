0262a6ff2e72b6c6866b478b41b1443ada972b87
Schin Chauhan <sachinchauhan2889@gmail.com>
2021-03-31 01:03:51 +0530

    set expiry time

--- config.py
+++ config.py
@@ -68,7 +68,7 @@ class Config:
     ]
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     ERROR_404_HELP = False
-    CSRF_ENABLED = False
+    CSRF_ENABLED = True
     SERVER_NAME = env('SERVER_NAME', default=None)
     CORS_HEADERS = 'Content-Type'
     SQLALCHEMY_DATABASE_URI = env('DATABASE_URL', default=None)
