63df9edb8a209efaba1668fac82ea860292ee9d5
Meghal Bisht <mailtomeghu25@gmail.com>
2021-04-08 19:17:47 +0530

    f

--- app/api/schema/events.py
+++ app/api/schema/events.py
@@ -83,7 +83,6 @@ class EventSchemaPublic(SoftDeletionSchema):
     is_billing_info_mandatory = fields.Bool(default=False)
     is_donation_enabled = fields.Bool(default=False)
     is_chat_enabled = fields.Bool(default=False)
-    chat_room_id = fields.Str(dump_only=True, allow_none=True)
     can_pay_by_paypal = fields.Bool(default=False)
     can_pay_by_stripe = fields.Bool(default=False)
     can_pay_by_cheque = fields.Bool(default=False)
