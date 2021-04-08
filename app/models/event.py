688c43b3d82cc3924d3b1d35086f458aa8c23584
MeghalBisht <mailtomeghu25@gmail.com>
2021-04-09 01:53:44 +0530

    use regex to change event room name

--- app/models/event.py
+++ app/models/event.py
@@ -1,3 +1,4 @@
+import re
 from argparse import Namespace
 from datetime import datetime
 
@@ -387,7 +388,7 @@ class Event(SoftDeletionModel):
 
     @property
     def chat_room_name(self):
-        return self.name.replace(' ', '-') + '-' + self.identifier
+        return re.sub('[^0-9a-zA-Z!]', '-', self.name) + '-' + self.identifier
 
     @property
     def tickets_available(self):
