568468202ce392c10f0de49b0315aa8bdc4a8a04
mrkeylee <83806655+mrkeylee@users.noreply.github.com>
2021-05-09 17:20:28 +0300

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -15,7 +15,7 @@ def create_default_user(email, password):
     if not email:
         ask_email = True
         while ask_email:
-            email = input("Enter email for super_admin: ")
+            email = input("Enter email for super_admin:")
             if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
                 print('\nInvalid email address\n')
                 continue
@@ -23,11 +23,11 @@ def create_default_user(email, password):
     if not password:
         ask_password = True
         while ask_password:
-            password = getpass.getpass("Enter password for super_admin: ")
+            password = getpass.getpass("Enter password for super_admin:")
             if len(password) < 8:
                 print('\nPassword should have minimum 8 characters')
                 continue
-            repassword = getpass.getpass("Enter your password again to confirm: ")
+            repassword = getpass.getpass("Enter your password again to confirm:")
             if password != repassword:
                 print('\nPassword did not match')
                 continue
