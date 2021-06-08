e65b2ab72e058e623737ecf34404c5e10e72a543
maze-runner <dubeysundaram057@gmail.com>
2021-06-08 23:37:06 +0530

    completing unit test

---
+++ tests/all/unit/api/validation/test_sessions_types.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest import TestCase
+
+from app.api.schema.session_types import SessionTypeSchema
+
+
+class TestEventValidation(TestCase):
+    def test_validate_length_pass(self):
+        """
+        Events Validate Timezone - Tests if the function runs without an exception
+        :return:
+        """
+        schema = SessionTypeSchema()
+        data = {'length': '%12:%23'}
+        SessionTypeSchema.validate_length(schema, data)
+
+
+if __name__ == '__main__':
+    unittest.main()
