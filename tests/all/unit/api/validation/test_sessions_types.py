9dd2b0967f9e96f771ae022dd8f7e0989f28f969
maze-runner <dubeysundaram057@gmail.com>
2021-06-10 07:32:52 +0530

    adding ...

--- tests/all/unit/api/validation/test_sessions_types.py
+++ tests/all/unit/api/validation/test_sessions_types.py
@@ -4,15 +4,25 @@ from unittest import TestCase
 from app.api.schema.session_types import SessionTypeSchema
 
 
-class TestEventValidation(TestCase):
+class TestSessionTypesValidation(TestCase):
     def test_validate_length_pass(self):
         """
-        Events Validate Timezone - Tests if the function runs without an exception
+        Session Types Validate length - Tests if the function runs without an exception
         :return:
         """
         schema = SessionTypeSchema()
         data = {'length': '12:23'}
         SessionTypeSchema.validate_length(schema, data)
+
+    def test_validate_length_format(self):
+        """
+        Session Types Validate length - Tests if the function runs without an exception
+        :return:
+        """
+        schema = SessionTypeSchema()
+        data = {'length': '1223'}
+        with self.assertRaises(UnprocessableEntityError):
+            SessionTypeSchema.validate_length(schema, data)
 
 
 if __name__ == '__main__':
