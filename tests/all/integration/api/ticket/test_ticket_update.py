0721939ba5ee753be6155d9dcaef796997480528
maze-runner <dubeysundaram057@gmail.com>
2021-06-10 17:17:31 +0530

    test for ticket deletion prevention

--- tests/all/integration/api/ticket/test_ticket_update.py
+++ tests/all/integration/api/ticket/test_ticket_update.py
@@ -37,3 +37,39 @@ def test_ticket_sales_end(db, client, admin_jwt):
         'jsonapi': {'version': '1.0'},
     }
     assert response.status_code == 422
+
+
+def test_ticket_has_sales(db, client, admin_jwt):
+    ticket = TicketSubFactory()
+    db.session.commit()
+
+    data = json.dumps(
+        {
+            'data': {
+                'type': 'ticket',
+                'id': str(ticket.id),
+                "has-current-orders": True,
+                "attributes": {"deleted-at": "2020-10-01T1:00:00+00:00"},
+            }
+        }
+    )
+
+    response = client.patch(
+        f'/v1/tickets/{ticket.id}',
+        content_type='application/vnd.api+json',
+        headers=admin_jwt,
+        data=data,
+    )
+
+    assert json.loads(response.data) == {
+        'errors': [
+            {
+                'detail': "Can't delete a ticket that has sales",
+                'source': {'param': 'ticket_id'},
+                'status': 403,
+                'title': 'Forbidden',
+            }
+        ],
+        'jsonapi': {'version': '1.0'},
+    }
+    assert response.status_code == 403
