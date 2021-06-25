3a7d1592ddfc0ec9080ef67e15e5348f54bbda77
maze-runner <dubeysundaram057@gmail.com>
2021-06-25 07:16:15 +0530

    make test timezone aware

--- tests/all/integration/api/helpers/test_humanize_helper.py
+++ tests/all/integration/api/helpers/test_humanize_helper.py
@@ -1,5 +1,7 @@
 import datetime
 
+import pytz
+
 from app.templates.flask_ext.jinja.filters import humanize_helper
 from tests.factories.order import OrderFactory
 
@@ -8,7 +10,7 @@ def test_humanize_helper(db):
     """Method to test humanization of order creation time"""
 
     test_order = OrderFactory(
-        created_at=datetime.datetime.now() - datetime.timedelta(days=10)
+        created_at=datetime.datetime.now(pytz.utc) - datetime.timedelta(days=10)
     )
     actual_response = humanize_helper(test_order.created_at)
     expected_response = '10 days ago'
