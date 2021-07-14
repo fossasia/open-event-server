2ae2f52177e51f267c4b9bdf6e5913fbda6c90cd
maze-runner <dubeysundaram057@gmail.com>
2021-07-14 22:22:34 +0530

    updating nvoices

--- app/api/schema/event_invoices.py
+++ app/api/schema/event_invoices.py
@@ -25,7 +25,7 @@ class EventInvoiceSchema(SoftDeletionSchema):
     created_at = fields.DateTime(allow_none=True)
     issued_at = fields.DateTime(dump_only=True)
     due_at = fields.DateTime(dump_only=True)
-    completed_at = fields.DateTime(default=None)
+    completed_at = fields.DateTime(default=None, allow_none=True)
     transaction_id = fields.Str(allow_none=True)
     paid_via = fields.Str(
         validate=validate.OneOf(
