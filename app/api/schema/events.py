1904bbbe3136831233027e206aa78e22577cdac7
Schin Chauhan <sachinchauhan2889@gmail.com>
2021-07-14 19:04:42 +0530

    add is_cfs_enabled in schema of event

--- app/api/schema/events.py
+++ app/api/schema/events.py
@@ -83,6 +83,7 @@ class EventSchemaPublic(SoftDeletionSchema):
     is_promoted = fields.Bool(default=False)
     is_demoted = fields.Bool(default=False)
     is_ticket_form_enabled = fields.Bool(default=True)
+    is_cfs_enabled = fields.Bool(default=False)
     payment_country = fields.Str(allow_none=True)
     payment_currency = fields.Str(allow_none=True)
     paypal_email = TrimmedEmail(allow_none=True)
