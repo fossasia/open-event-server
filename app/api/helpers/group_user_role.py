0642fd0d7d8f83ba16ab261a996efcf5811db626
ranjit <ranjitmaheshshah@gmail.com>
2021-08-14 23:10:08 +0530

    pytype fix

--- app/api/helpers/group_user_role.py
+++ app/api/helpers/group_user_role.py
@@ -11,9 +11,12 @@ def get_user_group_role(user_id, group_id):
     :return:
     """
     group = Group.query.filter_by(id=group_id).first()
+    user_group_role = UsersGroupsRoles.query.filter_by(
+        user_id=user_id, group_id=group_id
+    ).first()
     if group.user_id == user_id:
         return 'Owner'
-    elif UsersGroupsRoles.query.filter_by(user_id=user_id, group_id=group_id).first():
+    elif user_group_role:
         role = Role.query.filter_by(id=user_group_role.role_id).first()
         return role.name
     else:
