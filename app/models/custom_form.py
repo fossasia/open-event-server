032c1d5f245af29fda9fd16cd966fbc1d31d061f
Shubham Upreti <shubham.upreti@gmail.com>
2021-09-04 23:43:42 +0530

    fix: change 'Test Slide' to 'Slides' (#8197)

--- app/models/custom_form.py
+++ app/models/custom_form.py
@@ -81,7 +81,7 @@ CUSTOM_FORM_IDENTIFIER_NAME_MAP = {
         "level": "Level",
         "language": "Language",
         "slidesUrl": "Slide",
-        "slides": "Test Slide",
+        "slides": "Slides",
         "videoUrl": "Video",
         "audioUrl": "Audio",
         "website": "Website",
