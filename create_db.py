6629f017f682b3c80cb70224714e023cabac70e8
Shubham Upreti <shubham.upreti@gmail.com>
2021-09-27 16:07:52 +0530

    added default admin mail

--- create_db.py
+++ create_db.py
@@ -7,31 +7,37 @@ from flask_migrate import stamp
 from app.instance import current_app
 from app.models import db
 from populate_db import populate
+from sys import argv
 from tests.all.integration.auth_helper import create_super_admin
 
 
 def create_default_user(email, password):
     print("Your login is 'super_admin'.")
-    if not email:
-        ask_email = True
-        while ask_email:
-            email = input("Enter email for super_admin: ")
-            if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-                print('\nInvalid email address\n')
-                continue
-            ask_email = False
-    if not password:
-        ask_password = True
-        while ask_password:
-            password = getpass.getpass("Enter password for super_admin: ")
-            if len(password) < 8:
-                print('\nPassword should have minimum 8 characters')
-                continue
-            repassword = getpass.getpass("Enter your password again to confirm: ")
-            if password != repassword:
-                print('\nPassword did not match')
-                continue
-            ask_password = False
+
+    if len(argv) > 1 and len(argv) < 4:
+        email = argv[1]
+        password = argv[2]
+    else:
+        if not email:
+            ask_email = True
+            while ask_email:
+                email = input("Enter email for super_admin: ")
+                if not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+                    print('\nInvalid email address\n')
+                    continue
+                ask_email = False
+        if not password:
+            ask_password = True
+            while ask_password:
+                password = getpass.getpass("Enter password for super_admin: ")
+                if len(password) < 8:
+                    print('\nPassword should have minimum 8 characters')
+                    continue
+                repassword = getpass.getpass("Enter your password again to confirm: ")
+                if password != repassword:
+                    print('\nPassword did not match')
+                    continue
+                ask_password = False
     create_super_admin(email, password)
 
 
