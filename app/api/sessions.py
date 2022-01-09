95bbaec0fb1c91bd7183c5e181f5c8765fa1d57d
Mario Behling <mb@mariobehling.de>
2022-01-09 06:06:04 +0100

    Revert "fix: add details of all speakers to new session mail (#8488)"
    This reverts commit 03127e496becaa8dcd000c1d4435142235d358fe.

--- app/api/sessions.py
+++ app/api/sessions.py
@@ -76,12 +76,7 @@ class SessionListPost(ResourceList):
         if session.event.get_owner():
             owner = session.event.get_owner()
             owner_email = owner.email
-            speakers = Speaker.query.filter_by(
-                event_id=session.event_id, deleted_at=None
-            ).all()
-
-            # TODO: Send to all organizers
-            send_email_new_session(owner_email, session, speakers)
+            send_email_new_session(owner_email, session)  # TODO: Send to all organizers
             notify_new_session(session)
 
         for speaker in session.speakers:
@@ -350,7 +345,7 @@ class SessionDetail(ResourceDetail):
             )
 
     def after_update_object(self, session, data, view_kwargs):
-        """Send email if session accepted or rejected"""
+        """ Send email if session accepted or rejected """
 
         if data.get('send_email', None) and g.get('send_email'):
             notify_for_session(session)
