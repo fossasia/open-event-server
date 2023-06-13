9e25e7df5f8f3673c11c9dba0faf6efd7dc65816
lthanhhieu <lthanhhieu@tma.com.vn>
2023-06-13 17:08:36 +0700

    fix-8740: Create migration Custom Form Ticket and update Custom Form

--- app/models/custom_form_ticket.py
+++ app/models/custom_form_ticket.py
@@ -6,9 +6,10 @@ class CustomFormTickets(db.Model):
     __tablename__ = 'custom_form_ticket'
     id = db.Column(db.Integer, primary_key=True)
     form_id = db.Column(db.String, nullable=False)
-    ticket_id = db.Column(
+    ticket_id: int = db.Column(
         db.Integer, db.ForeignKey('tickets.id', ondelete='CASCADE')
     )
+    ticket = db.relationship('Ticket', backref='custom_form_ticket', foreign_keys=[ticket_id])
 
     def __repr__(self):
         return '<CustomFormTicket %r>' % self.id
