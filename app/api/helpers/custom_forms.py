00025983e8180e0d0d439d9cfdb26f17374e5d38
khangon <odkhang@tma.com.vn>
2023-06-13 18:20:53 +0700

    feature-8784:Add type year drop down custom from field

--- app/api/helpers/custom_forms.py
+++ app/api/helpers/custom_forms.py
@@ -21,6 +21,8 @@ def get_schema(form_fields):
             field_type = TrimmedEmail
         elif field.type == 'number':
             field_type = marshmallow.fields.Float
+        elif field.type == 'year':
+            field_type = marshmallow.fields.number
         else:
             raise UnprocessableEntityError(
                 {'pointer': '/data/complex-field-values/' + field.identifier},
