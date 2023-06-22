f2d067bd7e4868445c4bdfe5788321cb9a2c2bd5
Hieu Lam - TMA <135117837+lthanhhieu@users.noreply.github.com>
2023-06-22 20:36:20 +0700

    feature-8788: Implement language form fields in the attendee form (#8903)
    * fix-8740: Create migration Custom Form Ticket and update Custom Form
    
    * fix-8740: Create migration Custom Form Ticket and update Custom Form
    
    * feature-8740: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Merge code development
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * feature-8470: Make attendee form ticket specific
    
    * Feature-8788: Implement language form fields in the attendee form
    
    * feature-8788: Implement language form fields in the attendee form
    
    * Add comma back

--- app/models/custom_form.py
+++ app/models/custom_form.py
@@ -63,6 +63,8 @@ ATTENDEE_FORM = {
     "age_group": {"include": 0, "require": 0},
     "home_wiki": {"include": 0, "require": 0},
     "accept_video_recording": {"include": 0, "require": 0},
+    "language_form_1": {"include": 0, "require": 0},
+    "language_form_2": {"include": 0, "require": 0},
 }
 
 session_form_str = json.dumps(SESSION_FORM, separators=(',', ':'))
@@ -147,6 +149,8 @@ CUSTOM_FORM_IDENTIFIER_NAME_MAP = {
         "acceptVideoRecording": "Photo & video & text consent",
         "acceptShareDetails": "Partner contact consent",
         "acceptReceiveEmails": "Email consent",
+        "language_form_1": "What is your native language, or what language are you most fluent in?",
+        "language_form_2": "Are you fluent in any other of the following languages?",
         "home_wiki": "What is your home wiki"
     },
 }
