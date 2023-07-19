ef6161565361b601bc307f1f21d8987acbc9d7e1
nnhathung <nnhathung@tma.com.vn>
2023-07-19 23:26:31 +0700

    feature-8958: Store check in kiosk id to mark association fix ci and update latest branch from development

--- app/api/station.py
+++ app/api/station.py
@@ -104,7 +104,7 @@ class StationListPost(ResourceList):
         if not has_access('is_coorganizer', event=data['event']):
             raise ObjectNotFound(
                 {'parameter': 'event'},
-                f"Event: {data['event']} not found",
+                f"Event: {data['event']} not found {args} {kwargs}",
             )
 
         if data.get('microlocation'):
