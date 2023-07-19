f521472b6747c5f8f2255b46dc2f67d1c0a48ed5
Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
2023-07-19 16:36:32 +0700

    updated api with validation
    commit a8efa4dc27bdd79084b8c84aa25fa0c92bdf5b69
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Tue Jul 18 17:35:15 2023 +0700
    
        update import
    
    commit 7721f17140810c5a0b7224f069c88a18e027a12c
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Wed Jul 12 15:53:30 2023 +0700
    
        temp update
    
    commit ed2e60f3688dc4bdf8fef96a47b353ee4d4dda62
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Mon Jul 10 14:14:32 2023 +0700
    
        update
    
    commit 5237e47a4bdeb6ed744717b40e4a213353f4a35b
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Mon Jul 10 10:44:52 2023 +0700
    
        update progress
    
    commit 34854068ca3e5ce3e82211d5a983dedaeaef071c
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Sun Jul 9 20:48:24 2023 -0400
    
        temp2
    
    commit 22c85f426f8dcfb1549eb767f60dd39bf34163c8
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Sun Jul 9 10:58:37 2023 -0400
    
        holy hell
    
    commit 663936d2451df94a23f8572176b3e5edff28ce6a
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Sun Jul 9 01:35:57 2023 -0400
    
        works!
    
    commit 918fdf6231b0bb86f6f65fca11e36e85db0abbe4
    Merge: 7703a0dcd 1dc5d822d
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Sat Jul 8 11:58:15 2023 -0400
    
        Merge branch 'development' into video_trans_temp
    
    commit 7703a0dcd30a0389149a5a11b244101a1a0a43d6
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Sat Jul 8 11:57:46 2023 -0400
    
        temp
    
    commit 916b39238bd226ec5182bcee3bdfac678c3b48f3
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Sat Jul 8 10:13:57 2023 -0400
    
        temp
    
    commit 2517c3c433a133d90c9b7595cd6734040ca81218
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Fri Jul 7 14:24:40 2023 -0400
    
        temp
    
    commit c69ca7e4bf2ecac4b03e033057ea12e20c658e91
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Thu Jul 6 22:09:42 2023 +0700
    
        tmp
    
    commit d48b3269d613e26c0341840d9d7ed1798bd7dc99
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Wed Jul 5 13:08:20 2023 +0700
    
        Recorrect test case
    
    commit 3d08dfe2351805357ced8c9d1f5f61848c814a57
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Tue Jul 4 17:11:00 2023 +0700
    
        Test case logic change due to new return type
    
    commit 50223de8dc355164da930296b3254cbfd2a7cfe2
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Tue Jul 4 16:59:00 2023 +0700
    
        Test case logic change due to new return type
    
    commit e9e3fbed0f2128b7962306a50b469e24bc4bf019
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Tue Jul 4 15:24:07 2023 +0700
    
        Added new parameter for test case
    
    commit 2b2506416d37c32d09ddc7555ff534a68413cdc1
    Author: Mao Nguyen Minh Tam <magnetoCat01@gmail.com>
    Date:   Tue Jul 4 15:03:33 2023 +0700
    
        Rewrite celery export and its helper task with Dictwriter, better validation

--- app/api/schema/translation_channels.py
+++ app/api/schema/translation_channels.py
@@ -1,28 +1,3 @@
-# class TranslationChannelSchema(SoftDeletionSchema):
-#     class Meta:
-#         type_ = 'translation-channels'
-
-#     id = fields.Integer(dump_only=True)
-#     video_stream = fields.Nested(VideoStreamSchema, dump_only=True)
-#     channel = fields.Nested(VideoChannelSchemaPublic, dump_only=True)
-#     url = fields.String()
-
-    # # Relationships
-    # video_stream_id = Relationship(
-    #     related_view= 'v1.video_stream_detail',
-    #     related_view_kwargs={'video_stream_id': '<video_stream.id>'},
-    #     many=False,
-    #     type_='video-stream',
-    #     schema='VideoStreamSchema'
-    # )
-
-    # channel_id = Relationship(
-    #     related_view='/video_channel_detail/{id}',
-    #     related_view_kwargs={'channel_id': '<channel.id>'},
-    #     many=False,
-    #     type_='video-channel',
-    #     schema='VideoChannelSchema'
-    # )
 
 from marshmallow_jsonapi import fields
 from marshmallow_jsonapi.flask import Relationship, Schema
@@ -30,9 +5,7 @@ from app.api.helpers.utilities import dasherize
 
 
 class TranslationChannelSchema(Schema):
-    id = fields.Integer(required=True)
-    # video_stream_id = fields.Integer(required=True, load_only=True)
-    # channel_id = fields.Integer(required=True, load_only=True)
+    id = fields.Integer(dump_only=True)
     name = fields.String(required=True)
     url = fields.String(required=True)
 
@@ -45,7 +18,7 @@ class TranslationChannelSchema(Schema):
         type_="video_stream",
     )
 
-    channel= Relationship(
+    channel = Relationship(
         self_view="v1.translation_channels_channel",
         self_view_kwargs={"id": "<id>"},
         related_view="v1.video_channel_detail",
--- app/api/translation_channels.py
+++ app/api/translation_channels.py
@@ -1,48 +1,78 @@
+from app.api.helpers.utilities import require_relationship
 from app.api.schema.translation_channels import TranslationChannelSchema
 from app.api.schema.video_stream import VideoStreamSchema
 from app.models.translation_channels import TranslationChannel
 from app.models import db
-from flask_rest_jsonapi import ResourceList, ResourceRelationship,ResourceDetail
+from flask_rest_jsonapi import ResourceList, ResourceRelationship, ResourceDetail
+from app.models.video_channel import VideoChannel
+from app.models.video_stream import VideoStream
+from app.api.helpers.permissions import jwt_required
+from flask_rest_jsonapi.exceptions import ObjectNotFound
+from app.api.helpers.db import safe_query, safe_query_by_id, safe_query_kwargs, save_to_db
 
-
+from app.api.bootstrap import api
 
 
 class TranslationChannelsList(ResourceList):
-   
-    
-    def query(self, view_kwargs):
-        def print_out(objs):
-            print(objs)
-            for obj in objs:
-                for column in obj.__table__.columns:
-                    column_name = column.name
-                    column_value = getattr(obj, column_name)
-                    print(f"{column_name}: {column_value}")
 
+    def before_get(self, args, kwargs):
+
+        stream_id = kwargs.get("video_stream_id")
+        if stream_id:
+            vid_stream = safe_query_by_id(VideoStream, stream_id)
+            if not vid_stream:
+
+                raise ObjectNotFound(
+                    {'parameter': f'{stream_id}'},
+                    f"video stream not found for id {stream_id}",
+                )
+
+    def query(self, view_kwargs):
         """
         Query related channels (transaltions) for specific video stream
         """
         if view_kwargs.get("video_stream_id"):
-            print("-----TranslationChannelsList query--------")
             stream_id = view_kwargs.get("video_stream_id")
-            print(stream_id)
-            
-            #Do not use all() as it returns a list of object, needs BaseQuery object
-            records = self.session.query(TranslationChannel).filter_by(video_stream_id=stream_id)
-            print_out(records)
+
+            # Do not use all() as it returns a list of object, needs BaseQuery object
+            records = self.session.query(TranslationChannel).filter_by(
+                video_stream_id=stream_id)
             return records
-            
+
+    methods = ["GET"]
     schema = TranslationChannelSchema
+    decorators = (jwt_required, )
     data_layer = {
         'session': db.session,
         'model': TranslationChannel,
         'methods': {
-            "query": query
+            "query": query,
+            "before_get": before_get
         },
     }
 
+
 class TranslationChannelsListPost(ResourceList):
+
+    def before_post(self, args, kwargs, data):
+        require_relationship(['video_stream', 'channel'], data)
+        video_stream = db.session.query(
+            VideoStream.query.filter_by(id=data['video_stream']).exists()
+        ).scalar()
+
+        channel = db.session.query(
+            VideoChannel.query.filter_by(id=data['channel']).exists()
+        ).scalar()
+        if not video_stream and not channel:
+            raise ObjectNotFound(
+                {'parameter': 'id'}, "Incorrect video_stream and channel data in request body"
+            )
+
     schema = TranslationChannelSchema
+    decorators = (
+        jwt_required,
+        api.has_permission('auth_required', methods="POST", model=TranslationChannel),
+    )
     methods = [
         'POST'
     ]
@@ -50,13 +80,16 @@ class TranslationChannelsListPost(ResourceList):
         'session': db.session,
         'model': TranslationChannel,
         'methods': {
+            "before_post": before_post
         }
-    } 
+    }
+
 
 class TranslationChannelsDetail(ResourceDetail):
     schema = TranslationChannelSchema
+    decorators = (jwt_required, )
     methods = [
-         'GET', 'PATCH', 'DELETE'
+        'GET', 'PATCH', 'DELETE'
     ]
     data_layer = {
         'session': db.session,
@@ -64,7 +97,8 @@ class TranslationChannelsDetail(ResourceDetail):
         'methods': {
         },
     }
-    
+
+
 class TranslationChannelsRelationship(ResourceRelationship):
     schema = TranslationChannelSchema
     data_layer = {
