04fc790bf91120cc450728fdad781d5aa69af196
nnhathung <139526485+nnhathung@users.noreply.github.com>
2023-07-31 04:49:30 +0700

    feature-9004: Implement backend for additional Rocket Chat rooms (#9014)
    * feature-8987 - Implement dedicated Rocket Chat rooms for each video room and make them ready for mobile view
    
    * feature-8987 + feature-9004
    
    * fix python deepsource scan
    
    * fix issue create chat room for disabled location
    
    * fix mutiple migration head

--- app/api/schema/microlocations.py
+++ app/api/schema/microlocations.py
@@ -27,6 +27,9 @@ class MicrolocationSchema(SoftDeletionSchema):
     longitude = fields.Float(validate=lambda n: -180 <= n <= 180, allow_none=True)
     floor = fields.Integer(allow_none=True)
     hidden_in_scheduler = fields.Boolean(default=False)
+    chat_room_name = fields.Str(dump_only=True)
+    is_chat_enabled = fields.Boolean(default=False, allow_none=True)
+    is_global_event_room = fields.Boolean(default=False, allow_none=True)
     position = fields.Integer(allow_none=True, default=0)
     room = fields.Str(allow_none=True)
     sessions = Relationship(
