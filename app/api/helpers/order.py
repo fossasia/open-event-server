d41f157da97646a73dc1376b282d871e101531b0
nnhathung <nnhathung@tma.com.vn>
2023-08-04 22:23:42 +0700

    fix ut

--- app/api/helpers/order.py
+++ app/api/helpers/order.py
@@ -246,7 +246,7 @@ def create_onsite_attendees_for_order(data):
     del data['on_site_tickets']
 
 
-def calculate_order_amount(tickets, verify_discount, discount_code=None):
+def calculate_order_amount(tickets, verify_discount=True, discount_code=None):
     from app.api.helpers.ticketing import (
         is_discount_available,
         validate_discount_code,
@@ -254,8 +254,6 @@ def calculate_order_amount(tickets, verify_discount, discount_code=None):
     )
     from app.models.discount_code import DiscountCode
 
-    if verify_discount is None:
-        verify_discount = True
     ticket_ids = {ticket['id'] for ticket in tickets}
     ticket_map = {int(ticket['id']): ticket for ticket in tickets}
     fetched_tickets = validate_tickets(ticket_ids)
@@ -286,10 +284,11 @@ def calculate_order_amount(tickets, verify_discount, discount_code=None):
         quantity = ticket_info.get('quantity', 1)  # Default to single ticket
         if ticket_info.get('quantity_discount'):
             quantity_discount['numb_discount'] = ticket_info.get('quantity_discount')
-        if quantity_discount['numb_discount'] >= quantity:
-            discount_quantity = quantity
-        else:
-            discount_quantity = quantity_discount['numb_discount']
+        discount_quantity = (
+            quantity
+            if quantity_discount['numb_discount'] >= quantity
+            else quantity_discount['numb_discount']
+        )
         if not event:
             event = ticket.event
 
