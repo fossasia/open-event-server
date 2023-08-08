ab615b0ba47a9fc0591e4b37ed435ae7007c9383
nnhathung <nnhathung@tma.com.vn>
2023-08-09 00:01:11 +0700

    validat mising request params

--- app/api/custom/attendees.py
+++ app/api/custom/attendees.py
@@ -59,6 +59,14 @@ def check_attendee_state():
     """
     from app.models.event import Event
 
+    if not request.args.get('event_id', False):
+        raise NotFoundError(
+            {'parameter': 'event_id'}, "event_id is missing from your request"
+        )
+    if not request.args.get('attendee_id', False):
+        raise NotFoundError(
+            {'parameter': 'attendee_id'}, "attendee_id is missing from your request"
+        )
     event_id = request.args.get('event_id')
     attendee_id = request.args.get('attendee_id')
     if event_id is not None:
