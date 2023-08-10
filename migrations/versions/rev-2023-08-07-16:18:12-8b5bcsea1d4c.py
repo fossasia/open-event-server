4e61a50386f139b12ac7687d344351c977cab199
lthanhhieu <lthanhhieu@tma.com.vn>
2023-08-10 10:55:42 +0700

    feature-9005: Provide option of Youtube integration with Privacy Enhanced Mode

--- migrations/versions/rev-2023-08-07-16:18:12-8b5bcsea1d4c.py
+++ migrations/versions/rev-2023-08-07-16:18:12-8b5bcsea1d4c.py
@@ -1,7 +1,7 @@
 """empty message
 
 Revision ID: 8b5bcsea1d4c
-Revises: 8b5bc48e1d4c
+Revises: 3e8e18c0bebe
 Create Date: 2023-08-03 14:18:12.187180
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = '8b5bcsea1d4c'
-down_revision = '8b5bc48e1d4c'
+down_revision = '3e8e18c0bebe'
 
 
 def upgrade():
