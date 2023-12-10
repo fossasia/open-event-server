e7d73bb00c3f728478b15ad34c8d89bd79141483
Norbert Preining <norbert@preining.info>
2023-12-10 10:23:31 +0900

    Fix slash locations

--- open_event_api/urls.py
+++ open_event_api/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    path("v2/roles", include("roles.urls")),
+    path("v2/roles/", include("roles.urls")),
 ]
--- roles/urls.py
+++ roles/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import RoleDetail, RoleView
 
 urlpatterns = [
-    path("/<int:pk>/", RoleDetail.as_view(), name="role_detail"),
+    path("<int:pk>/", RoleDetail.as_view(), name="role_detail"),
     path("", RoleView.as_view(), name="roles_list"),
 ]
