a22af6d5880cc6908cc9e43c8cf70442a7c21375
thealphadollar <s-a-kumar@mercari.com>
2023-12-16 12:30:12 +0530

    Add pagination and ordering to REST framework settings

--- open_event_api/settings.py
+++ open_event_api/settings.py
@@ -156,6 +156,11 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 100,
+    "DEFAULT_FILTER_BACKENDS": [
+        "rest_framework.filters.OrderingFilter",
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
