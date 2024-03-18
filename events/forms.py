7b84b7266c28803d9559ed3a8c721cd9896ac275
Shivangi-ch <shivangi.ch.936@gmail.com>
2024-03-18 20:29:25 +0530

    added event_id

--- events/forms.py
+++ events/forms.py
@@ -4,4 +4,4 @@ from events.models import Exhibitors
 class ExhibitorForm(forms.ModelForm):
     class Meta:
         model = Exhibitor
-        fields = ['name', 'description', 'url', 'position', 'logo_url', 'banner_url', 'video_url', 'slides_url', 'contact_email', 'contact_link', 'enable_video_room', 'thumbnail_image_url']
+        fields = ['name', 'description', 'url', 'position', 'event_id' 'logo_url', 'banner_url', 'video_url', 'slides_url', 'contact_email', 'contact_link', 'enable_video_room', 'thumbnail_image_url']
